c5fcb9badbf00981920b6ac261d4d6f41268d613
Bost <thebost@gmail.com>
2012-10-15 18:36:02 +0200

    update vim plugings; filefinder.py

--- filefinder.py
+++ filefinder.py
@@ -5,7 +5,6 @@ def find(name, path):
     for root, dirs, files in os.walk(path):
         if name in files:
             return os.path.join(root, name)
-And this will find all matches:
 
 def find_all(name, path):
     result = []
@@ -13,9 +12,9 @@ def find_all(name, path):
         if name in files:
             result.append(os.path.join(root, name))
     return result
-And this will match a pattern:
 
 import os, fnmatch
+
 def find(pattern, path):
     result = []
     for root, dirs, files in os.walk(path):
@@ -24,4 +23,34 @@ def find(pattern, path):
                 result.append(os.path.join(root, name))
     return result
 
-find('*.txt', '/path/to/dir')
+import re, sys, glob
+
+def grep(search_str, files):
+    print 'Current dir: '+os.getcwd()
+    print 'Searching for: '+search_str
+    print ''
+    cntFilesFound = 0
+
+    #for file in filter(os.path.isfile, sys.argv[2:]):
+    for file in files:
+        for lineNr, line in enumerate(open(file, 'r')):
+            if re.search(search_str, line):
+                # comma , after print removes carriage return (line will have one)
+                print file+':'+str(lineNr)+': '+line,
+
+    if cntFilesFound > 0:
+        print 'Files found: '+str(cntFilesFound)
+
+def findJavaCall():
+    dirs = [
+        # TODO put hier dirnames where to search
+        '',
+        ]
+
+    for d in dirs:
+        filesFound = find('*.txt', d)
+        grep('search_str', filesFound)
+
+filesFound = find('*.txt', '/path/to/dir')
+grep('search_str', filesFound)
+
