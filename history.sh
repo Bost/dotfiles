47b54a747cb9067e6bb53630fa758be0aaee84a7
Bost <thebost@gmail.com>
2013-04-13 12:25:42 +0200

    xfce4 plugins

--- history.sh
+++ history.sh
@@ -32,8 +32,10 @@ echo sudo apt-get install chromium-browser ant gparted bitcoind htop traceroute 
 echo sudo apt-get install gnome-system-tools curl openjdk-7-jdk maven ack-grep
      sudo apt-get install gnome-system-tools curl openjdk-7-jdk maven ack-grep
 # in python 3 python-numpy should be already included
-echo sudo apt-get install python-numpy python-matplotlib xfce4
-     sudo apt-get install python-numpy python-matplotlib xfce4
+echo sudo apt-get install python-numpy python-matplotlib
+     sudo apt-get install python-numpy python-matplotlib
+echo xfce4 xfce4-cpugraph-plugin xfce4-clipman-plugin xfce4-netload-plugin xfce4-xkb-plugin xfce4-systemload-plugin
+     xfce4 xfce4-cpugraph-plugin xfce4-clipman-plugin xfce4-netload-plugin xfce4-xkb-plugin xfce4-systemload-plugin
 echo sudo apt-get install bitcoind bitcoin-qt
      sudo apt-get install bitcoind bitcoin-qt
 #sox kdelibs-data thunar-archive-plugin thunar-media-tags-plugin tumbler-plugins-extra xfce4-goodies xfce4-power-manager xfwm4-themes
