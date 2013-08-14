4f1279c58e931184b11b20e6baed9a009876e182
Bost <thebost@gmail.com>
2013-08-15 00:31:41 +0200

    cljs: rhino vs. browser

--- bash/browser-repl.sh
+++ bash/browser-repl.sh
@@ -9,10 +9,13 @@ for next in lib/: lib/*: src/clj: src/cljs: test/cljs; do
   CLJSC_CP=$CLJSC_CP$CLOJURESCRIPT_HOME'/'$next
 done
 
-# echo java -server -cp $CLJSC_CP clojure.main -e \
+
+# TODO use replType=browser
+
+# java -server -cp $CLJSC_CP clojure.main -e \
 # "(require '[cljs.repl :as repl])
-# (require '[cljs.repl.browser :as browser])
-# (def env (browser/repl-env))
+# (require '[cljs.repl.rhino :as rhino])
+# (def env (rhino/repl-env))
 # (repl/repl env)"
 
 java -server -cp $CLJSC_CP clojure.main -e \
