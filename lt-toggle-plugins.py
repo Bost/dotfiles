050e1843fe781b966f2531ed69ebd982863fc0de
Bost <thebost@gmail.com>
2014-06-06 22:07:03 +0200

    lt-toggle-plugins.py: hash table: plugins

--- lt-toggle-plugins.py
+++ lt-toggle-plugins.py
@@ -2,21 +2,29 @@
 
 # TODO auto-detect links/plugins
 
-from fabric.api import cd, env, prefix, run, task, local
+from fabric.api import local
 
 pluginDir     = '~/.config/LightTable/plugins'
 pluginDirDev  = '~/dev/lt-plugins-dev'
 pluginDirOrig = '~/dev/lt-plugins-orig'
 
-idxLinkName   = 0
-idxTargetDev  = 1
-idxTargetOrig = 2
+git='git'
+syntax='syntax'
+dev='dev'
+orig='orig'
 
-plugins = [
-     [pluginDir+'/Git_Status_Bar',    pluginDirDev+'/lt-gitstatusbar',    pluginDirOrig+'/Git_Status_Bar']
-    ,[pluginDir+'/Syntax_Status_Bar', pluginDirDev+'/lt-syntaxstatusbar', pluginDirOrig+'/Syntax_Status_Bar']
-    # ,['/home/bost/dev/my', 'my']
-    ]
+indexes = {dev : 1, orig : 2 } # indexes in the vector-values of pluginHashMap
+idxLink = 0
+
+plugins = {
+    git :    [pluginDir+'/Git_Status_Bar',
+              pluginDirDev+'/lt-gitstatusbar',
+              pluginDirOrig+'/Git_Status_Bar'],
+    syntax : [pluginDir+'/Syntax_Status_Bar',
+              pluginDirDev+'/lt-syntaxstatusbar',
+              pluginDirOrig+'/Syntax_Status_Bar']
+    #, 'my'  : ['/home/bost/dev/my', 'my']
+}
 
 def do(cmd):
     # print(cmd)
@@ -28,19 +36,15 @@ def createLink(target, link):
 def removeLink(link):
     do('rm -f '+link)
 
-def removeLinks():
-    for p in plugins:
-        linkName = p[idxLinkName]
-        removeLink(linkName)
+def activatePlugin(name, version):
+    print 'name: '+name+'; version: '+version
+    idxVersion = indexes[version]
+    plugin   = plugins[name]
+    target   = plugin[idxVersion]
+    linkName = plugin[idxLink]
+    removeLink(linkName)
+    createLink(target, linkName)
 
-def create(idx):
-    removeLinks()
-    for p in plugins:
-        target = p[idx]
-        linkName = p[idxLinkName]
-        createLink(target, linkName)
-
-create(idxTargetOrig)
-print pluginDir
+# activatePlugin(git, dev)
+# activatePlugin(syntax, dev)
 local('ls -lAh --color '+pluginDir)
-
