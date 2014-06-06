107c6cb127603d25399157d8ed3ac188aef5ccc0
Bost <thebost@gmail.com>
2014-06-06 20:16:25 +0200

    lt-toggle-plugins.py: toggle dev / orig

--- lt-toggle-plugins.py
+++ lt-toggle-plugins.py
@@ -4,16 +4,22 @@
 
 from fabric.api import cd, env, prefix, run, task, local
 
-baseDir = '~/dev/lighttable'
-pluginDir = baseDir+'/deploy/plugins'
+pluginDir     = '~/.config/LightTable/plugins'
+pluginDirDev  = '~/dev/lt-plugins-dev'
+pluginDirOrig = '~/dev/lt-plugins-orig'
 
-pairs=[
-     ['/home/bost/dev/lt-gitstatusbar', 'lt-gitstatusbar']
-    ,['/home/bost/dev/lt-syntaxstatusbar', 'lt-syntaxstatusbar']
-    ,['/home/bost/dev/my', 'my']
+idxLinkName   = 0
+idxTargetDev  = 1
+idxTargetOrig = 2
+
+plugins = [
+     [pluginDir+'/Git_Status_Bar',    pluginDirDev+'/lt-gitstatusbar',    pluginDirOrig+'/Git_Status_Bar']
+    ,[pluginDir+'/Syntax_Status_Bar', pluginDirDev+'/lt-syntaxstatusbar', pluginDirOrig+'/Syntax_Status_Bar']
+    # ,['/home/bost/dev/my', 'my']
     ]
 
 def do(cmd):
+    # print(cmd)
     local(cmd)
 
 def createLink(target, link):
@@ -23,18 +29,18 @@ def removeLink(link):
     do('rm -f '+link)
 
 def removeLinks():
-    for tl in pairs:
-        link = pluginDir+'/'+tl[1]
-        removeLink(link)
+    for p in plugins:
+        linkName = p[idxLinkName]
+        removeLink(linkName)
 
+def create(idx):
+    removeLinks()
+    for p in plugins:
+        target = p[idx]
+        linkName = p[idxLinkName]
+        createLink(target, linkName)
 
-def createLinks():
-    for tl in pairs:
-        target = tl[0]
-        link = pluginDir+'/'+tl[1]
-        createLink(target, link)
+create(idxTargetOrig)
+print pluginDir
+local('ls -lAh --color '+pluginDir)
 
-
-# removeLinks()
-# createLinks()
-do('ls -lAh --color '+pluginDir)
