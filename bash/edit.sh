85061c89e5b3c4022a0b1c87838a75892cfeaf34
Bost <thebost@gmail.com>
2014-11-28 11:08:44 +0100

    LinuxVB: compile emacs from source

--- bash/edit.sh
+++ bash/edit.sh
@@ -5,9 +5,7 @@
 # stop on error:
 # set -e
 
-if [ $isLinuxVB -gt 0 ]; then
-    emacsCli=$HOME/emacs-24.4/lib-src/emacsclient
-elif [ $isLinuxNew64 -gt 0 ]; then
+if [ $isLinuxNew64 -gt 0 ] || [ $isLinuxVB -gt 0 ]; then
     emacsCli=$dev/emacs/src/emacsclient
 else
     emacsCli=emacsclient
