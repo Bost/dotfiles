06db8256af5e53a172d7d036921f673b206665b0
Bost <thebost@gmail.com>
2015-01-20 19:34:52 +0100

    evil-numbers-lib.el: use-package evil-numbers

--- elisp/evil-numbers-lib.el
+++ elisp/evil-numbers-lib.el
@@ -1,12 +1,11 @@
-(require 'evil-numbers)
-(global-set-key (kbd "C-c +") 'evil-numbers/inc-at-pt)
-(global-set-key (kbd "C-c -") 'evil-numbers/dec-at-pt)
+(use-package evil-numbers
+  :bind (((kbd "C-c +")  . evil-numbers/inc-at-pt)
+         ((kbd "C-c -")  . evil-numbers/dec-at-pt)
 
-(global-set-key (kbd "s-+") 'evil-numbers/inc-at-pt)
-(global-set-key (kbd "s--") 'evil-numbers/dec-at-pt)
+         ((kbd "s-+")  . evil-numbers/inc-at-pt)
+         ((kbd "s--")  . evil-numbers/dec-at-pt)
 
-(global-set-key (kbd "<C-kp-add>")      'evil-numbers/inc-at-pt)
-(global-set-key (kbd "<C-kp-subtract>") 'evil-numbers/dec-at-pt)
-(global-set-key (kbd "<s-kp-add>")      'evil-numbers/inc-at-pt)
-(global-set-key (kbd "<s-kp-subtract>") 'evil-numbers/dec-at-pt)
-
+         ((kbd "<C-kp-add>")       . evil-numbers/inc-at-pt)
+         ((kbd "<C-kp-subtract>")  . evil-numbers/dec-at-pt)
+         ((kbd "<s-kp-add>")       . evil-numbers/inc-at-pt)
+         ((kbd "<s-kp-subtract>")  . evil-numbers/dec-at-pt)))
