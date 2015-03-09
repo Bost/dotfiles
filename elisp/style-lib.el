132df54bac84ed150ed2f885e705c8b17c23d613
Bost <thebost@gmail.com>
2015-03-09 23:12:12 +0100

    elisp/style-lib.el: switch back to deeper-blue

--- elisp/style-lib.el
+++ elisp/style-lib.el
@@ -1,8 +1,8 @@
 (setq inhibit-splash-screen t)
 ;; (load-theme 'light-blue t)  ;(disable-theme 'light-blue)  (enable-theme 'light-blue)
-;; (load-theme 'deeper-blue t)    ;(disable-theme 'deeper-blue) (enable-theme 'deeper-blue)
+(load-theme 'deeper-blue t)    ;(disable-theme 'deeper-blue) (enable-theme 'deeper-blue)
 ;; (load-theme 'misterioso t)  ;(disable-theme 'misterioso)  (enable-theme 'misterioso)
 ;; (load-theme 'whiteboard t)  ;(disable-theme 'whiteboard)  (enable-theme 'whiteboard)
-(load-theme 'ritchie t) ;(disable-theme 'ritchie) (enable-theme 'ritchie)
+;; (load-theme 'ritchie t) ;(disable-theme 'ritchie) (enable-theme 'ritchie)
 ;; test rainbow parenthesis:
 ;; (((((((())))))))   [[[[[[[[]]]]]]]]   {{{{{{{{}}}}}}}}
