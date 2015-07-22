7241feb453f0dce40c4d84ff6896f2a98563f3c0
Bost <thebost@gmail.com>
2015-07-22 11:14:46 +0200

    .lein/profiles.clj: upgrade

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,2 +1,15 @@
-{:user {:plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]]
-        :dependencies [[org.clojure/tools.nrepl "0.2.7"]]}}
+{:user
+ {:plugins
+  [
+   [refactor-nrepl "1.2.0-SNAPSHOT"]
+   ;; [refactor-nrepl "1.1.0"]
+
+   [cider/cider-nrepl "0.10.0-SNAPSHOT"]
+   ;; [cider/cider-nrepl "0.9.1"]
+
+   [lein-ancient "0.6.7"]
+   ]
+  :dependencies
+  [
+   [org.clojure/tools.nrepl "0.2.10"]
+   ]}}
