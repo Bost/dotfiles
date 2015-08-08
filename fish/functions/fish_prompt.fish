7325151e15538799d216c28f9e1c9029fd5a4337
Bost <thebost@gmail.com>
2015-08-08 13:42:46 +0200

    $ sign in the prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,6 +1,4 @@
-# function fish_prompt
-#     echo (pwd) '$ '
-# end
 function fish_prompt -d "Write out the prompt"
-        printf '%s@%s%s%s%s> ' (whoami) (hostname|cut -d . -f 1) (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
+    # echo (pwd) '$ '
+    printf '%s@%s%s%s%s$ ' (whoami) (hostname|cut -d . -f 1) (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
 end
