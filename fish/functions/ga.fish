d5698db46c61ea1744ba2894fdd4f1de1997751a
Bost <thebost@gmail.com>
2015-08-09 20:02:43 +0200

    fish: function ga -d "GitUpdateAll"

--- fish/functions/ga.fish
+++ fish/functions/ga.fish
@@ -1,3 +1,7 @@
-function ga
-    GitUpdateAll $argv
+function ga -d "GitUpdateAll"
+    cd $dev/cheatsheet
+    git pull --rebase origin # ; and git fetch --tags
+
+    cd $dev/dotfiles
+    git pull --rebase origin # ; and git fetch --tags
 end
