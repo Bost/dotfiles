38eae6e5a0a09c28de5435ba95e423ce626b35fa
Bost <thebost@gmail.com>
2015-08-18 16:27:54 +0200

    fish param expansion (Globbing) causes troubles; youtube-dl --sub-lang 'fr'

--- fish/functions/l.fish
+++ fish/functions/l.fish
@@ -1,5 +1,8 @@
 function l
-    set cmd "ls -lA  --color --time-style='+%d-%m-%Y %H:%M:%S' $argv"
-    echo $cmd
-    eval $cmd
+    # Parameter expansion (Globbing) causes trouble
+    # set cmd "ls -lA  --color --time-style='+%d-%m-%Y %H:%M:%S' $argv"
+    # echo $cmd
+    # eval $cmd
+    echo ls -lA  --color --time-style='+%d-%m-%Y %H:%M:%S' \$argv
+         ls -lA  --color --time-style='+%d-%m-%Y %H:%M:%S'  $argv
 end
--- fish/functions/y.fish
+++ fish/functions/y.fish
@@ -1,5 +1,9 @@
-function y
-    set cmd youtube-dl --write-auto-sub $argv
-    echo $cmd
-    eval $cmd
+function y --description '$argv must be inside double quotes'
+    # Parameter expansion (Globbing) causes trouble
+    # set youtube-dl --write-auto-sub --sub-lang 'fr' $argv
+    # echo $cmd
+    # eval $cmd
+
+    echo youtube-dl --write-auto-sub --sub-lang 'fr' \$argv
+         youtube-dl --write-auto-sub --sub-lang 'fr'  $argv
 end
