45d69e325f0dde796ef9d5fdc6ebf4c9e89a6b3b
Bost <thebost@gmail.com>
2015-09-07 23:11:09 +0200

    u.fish

--- fish/functions/u.fish
+++ fish/functions/u.fish
@@ -1,5 +1,4 @@
-function u -d "UpdateUpgradeClean"
-    set cmd "sudo apt-get update; and sudo apt-get upgrade --yes; and sudo apt-get clean; and sudo checkrestart"
-    echo $cmd
-    eval $cmd
+function u
+    echo sudo apt-get update; and sudo apt-get upgrade --yes; and sudo apt-get clean; and sudo checkrestart
+         sudo apt-get update; and sudo apt-get upgrade --yes; and sudo apt-get clean; and sudo checkrestart
 end
