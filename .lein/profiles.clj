7f0af2ab4189c053bc186f63bbbb97982f683acd
Bost <thebost@gmail.com>
2015-09-13 21:51:21 +0200

    core.typed

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -10,10 +10,12 @@
    [lein-ancient "0.6.7"]
    [lein-localrepo "0.5.3"]
    [lein-kibit "0.1.2"]
+   [lein-typed "0.3.5"]
    ]
   :dependencies
   [
    [org.clojure/tools.nrepl "0.2.10"]
+   [org.clojure/core.typed "0.3.11"]
 
    [alembic "0.3.2"] ; dynamic classpath loader and dependencies resolver
    ;; Usage:
