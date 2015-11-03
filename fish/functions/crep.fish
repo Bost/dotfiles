34d0732984091cef9eceaa3971c07e551f2a766f
Bost <thebost@gmail.com>
2015-11-03 01:15:16 +0100

    Cheatsheet reworked

--- fish/functions/crep.fish
+++ fish/functions/crep.fish
@@ -2,8 +2,12 @@ function crep -d "TODO crep/cr and creph should use the same code basis"
     set -x _pwd (pwd)
     set -x _oldpwd $OLDPWD
     cd $dev/cheatsheet/
-    ack-grep *-commands.js \
-             --nogroup \--break --with-filename --color-filename=grey13 \
+    # --before-context=1 --after-context=1
+    ack-grep git-commands.sh rest-commands.sh \
+             clojure-commands.clj emacs-commands.js \
+             --context=1 \
+             --nogroup \
+             --break --with-filename --color-filename=grey13 \
              --color-lineno=grey13 --color-match="bold blue" \
              --ignore-case --match $argv
     cd $_pwd
