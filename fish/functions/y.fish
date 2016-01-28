c6620456faf8a825abecb3c26c90fa0d8ba415b2
Bost <thebost@gmail.com>
2016-01-28 17:44:40 +0100

    youtube: fixing param quoting in fish

--- fish/functions/y.fish
+++ fish/functions/y.fish
@@ -4,6 +4,7 @@ function y --description '$argv must be inside double quotes'
     # echo $cmd
     # eval $cmd
 
-    echo youtube-dl --write-auto-sub --sub-lang 'fr' $argv
-         youtube-dl --write-auto-sub --sub-lang 'fr' $argv
+    set youtube-dl --write-auto-sub --sub-lang 'fr' \"$argv\"
+    echo $cmd
+    eval $cmd
 end
--- fish/functions/ys.fish
+++ fish/functions/ys.fish
@@ -1,5 +1,5 @@
 function ys
-    set cmd youtube-dl --extract-audio $argv
+    set cmd youtube-dl --extract-audio \"$argv\"
     echo $cmd
     eval $cmd
 end
