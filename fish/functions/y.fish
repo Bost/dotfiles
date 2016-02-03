9bcd0e29c47900c62f37a01f838fcfe2b1b600ce
Bost <thebost@gmail.com>
2016-02-04 00:32:17 +0100

    Bugfix

--- fish/functions/y.fish
+++ fish/functions/y.fish
@@ -1,10 +1,5 @@
 function y --description '$argv must be inside double quotes'
-    # Parameter expansion (Globbing) causes trouble
-    # set youtube-dl --write-auto-sub --sub-lang 'fr' $argv
-    # echo $cmd
-    # eval $cmd
-
-    set youtube-dl --write-auto-sub --sub-lang 'fr' \"$argv\"
+    set cmd youtube-dl --write-auto-sub --sub-lang 'fr' \"$argv\"
     echo $cmd
     eval $cmd
 end
