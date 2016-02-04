7c77138b0f5481a3be9a074a70a8e8fc128de125
Bost <thebost@gmail.com>
2016-02-04 11:03:36 +0100

    refactor-nrepl "1.1.0", cider/cider-nrepl "0.9.1"

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,11 +1,11 @@
 {:user
  {:plugins
   [
-   [refactor-nrepl "2.0.0-SNAPSHOT"]
-   ;; [refactor-nrepl "1.1.0"]  ; otherwise LightTable doesn't connect to REPL
+   ;; [refactor-nrepl "2.0.0-SNAPSHOT"]
+   ;; [cider/cider-nrepl "0.10.2"]
 
-   [cider/cider-nrepl "0.10.2"]
-   ;; [cider/cider-nrepl "0.9.1"]
+   [refactor-nrepl "1.1.0"]  ; otherwise LightTable doesn't connect to REPL
+   [cider/cider-nrepl "0.9.1"]
 
    [lein-ancient "0.6.8"]
    [lein-localrepo "0.5.3"]
