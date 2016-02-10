1eb7c6c0b985d94de453b1406f09507f222adb50
Bost <thebost@gmail.com>
2016-02-11 00:45:34 +0100

    Delete dead commented code

--- emacs/elisp/cider-startup.el
+++ emacs/elisp/cider-startup.el
@@ -168,53 +168,6 @@
     (cider-load-file (buffer-file-name))
     ;; (cider-switch-to-relevant-repl-buffer nil)
     )
-
-  ;; (defun cider-eval-last-expression-in-repl ()
-  ;;   "This doesn't work"
-  ;;   (interactive)
-  ;;   (evil-visual-char)
-  ;;   (evil-jump-item)
-  ;;   ;; (clipboard-kill-ring-save)
-  ;;   ;; (clipboard-kill-region)
-  ;;   ;; (cider-switch-to-relevant-repl-buffer)
-  ;;   ;; (clipboard-yank)
-
-  ;;   ;; (global-set-key [(shift delete)] 'clipboard-kill-region)
-  ;;   ;; (global-set-key [(control insert)] 'clipboard-kill-ring-save)
-  ;;   ;; (global-set-key [(shift insert)] 'clipboard-yank)
-  ;;   )
-
-  ;; (defun cider-mode-keys ()
-  ;;   "Modify keymaps used by `cider-mode'."
-  ;;   (local-set-key (kbd "s-z")
-  ;;                  ;; 'cider-switch-to-repl-buffer
-  ;;                  'cider-switch-to-last-clojure-buffer)
-  ;;   (local-set-key (kbd "s-t") 'cider-test-run-tests)
-  ;;   (local-set-key (kbd "s-.") 'cider-find-var)
-  ;;   (local-set-key (kbd "s-,") 'cider-jump-back)
-  ;;   ;; <menu> key does not work
-  ;;   ;; (local-set-key (kbd "<menu>-c") 'cider-repl-clear-buffer)
-  ;;   )
-  ;; (add-hook 'cider-mode-hook 'cider-mode-keys)
-
-  ;; (defun cider-repl-mode-keys ()
-  ;;   "Modify keymaps used by `cider-repl-mode'."
-  ;;   (local-set-key (kbd "s-z")
-  ;;                  ;; 'cider-switch-to-repl-buffer
-  ;;                  'cider-switch-to-last-clojure-buffer)
-  ;;   (local-set-key (kbd "s-t") 'cider-test-run-tests)
-  ;;   (local-set-key (kbd "s-.") 'cider-find-var)
-  ;;   (local-set-key (kbd "s-,") 'cider-jump-back)
-  ;;   ;; <menu> key does not work
-  ;;   ;; (local-set-key (kbd "<menu>-c") 'cider-repl-clear-buffer)
-  ;;   )
-  ;; (add-hook 'cider-repl-mode-hook 'cider-repl-mode-keys)
-
-  ;; (defun cider-interaction-mode-keys ()
-  ;;   "Modify keymaps used by `cider-interaction-mode'."
-  ;;   ;; (local-set-key (kbd "s-o") 'cider-jump)
-  ;;   )
-  ;; (add-hook 'cider-interaction-mode-hook 'cider-interaction-mode-keys)
   )
 
 (provide 'cider-startup)
