7d2e906c18fe25d239ee69ea3e0c6b7782996992
Bost <thebost@gmail.com>
2016-05-27 17:25:01 +0200

    use apt instead of apt-get

--- fish/functions/u.fish
+++ fish/functions/u.fish
@@ -1,4 +1,7 @@
 function u
-    echo sudo apt-get update; and sudo apt-get upgrade --yes; and sudo apt-get clean; and sudo checkrestart
-         sudo apt-get update; and sudo apt-get upgrade --yes; and sudo apt-get clean; and sudo checkrestart
+    # echo sudo apt-get update; and sudo apt-get upgrade --yes; and sudo apt-get clean; and sudo checkrestart
+    #      sudo apt-get update; and sudo apt-get upgrade --yes; and sudo apt-get clean; and sudo checkrestart
+    echo sudo apt update; and sudo apt upgrade --yes; and sudo checkrestart
+         sudo apt update; and sudo apt upgrade --yes; and sudo checkrestart
+
 end
