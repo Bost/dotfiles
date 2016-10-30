c9c22bc4ff4302c395e9257978130c5a40fe241c
Bost <thebost@gmail.com>
2016-10-30 17:26:08 +0100

    fish: $PATH setup for some spacemacs layers

--- fish/config.fish
+++ fish/config.fish
@@ -2,3 +2,6 @@ set fish_greeting ""
 set -U dev ~/dev
 set -U bin ~/bin
 
+# Some spacemacs layers require certain tools to be available on $PATH
+# see http://spacemacs.org/doc/FAQ.html#setup-path
+# set -x PATH ~/.local/bin $PATH
