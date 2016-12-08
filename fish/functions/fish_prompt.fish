911ffbc0f8e34268994d17c055b7b4ad5d29e84b
Bost <thebost@gmail.com>
2016-12-08 11:12:53 +0100

    fish: git branch in prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,4 +1,50 @@
 function fish_prompt -d "Write out the prompt"
-    # echo (pwd) '$ '
-    printf '%s@%s%s%s%s$ ' (whoami) (hostname|cut -d . -f 1) (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
+  # echo (pwd) '$ '
+  # printf '%s@%s%s%s%s$ ' (whoami) (hostname|cut -d . -f 1) (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
+
+  ############################
+  # set -l git_branch (git branch ^/dev/null | sed -n '/\* /s///p')
+  # echo -n (whoami)'@'(hostname)':'(prompt_pwd)'{'"$git_branch"'} $ '
+
+
+  ############################ see colors in config.fish
+  # set last_status $status
+  # set_color $fish_color_cwd
+  # printf '%s' (prompt_pwd)
+  # set_color normal
+  # printf '%s ' (__fish_git_prompt)
+  # set_color normal
+
+
+  ############################
+  # Just calculate these once, to save a few cycles when displaying the prompt
+  if not set -q __fish_prompt_hostname
+    set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
+  end
+
+  if not set -q __fish_prompt_normal
+    set -g __fish_prompt_normal (set_color normal)
+  end
+
+  if not set -q __git_cb
+    set __git_cb ":"(set_color brown)(git branch ^/dev/null | grep \* | sed 's/* //')(set_color normal)""
+  end
+
+  switch $USER
+    case root
+      if not set -q __fish_prompt_cwd
+        if set -q fish_color_cwd_root
+          set -g __fish_prompt_cwd (set_color $fish_color_cwd_root)
+        else
+          set -g __fish_prompt_cwd (set_color $fish_color_cwd)
+        end
+      end
+      printf '%s@%s:%s%s%s%s# ' $USER $__fish_prompt_hostname "$__fish_prompt_cwd" (prompt_pwd) "$__fish_prompt_normal" $__git_cb
+
+    case '*'
+      if not set -q __fish_prompt_cwd
+        set -g __fish_prompt_cwd (set_color $fish_color_cwd)
+      end
+      printf '%s@%s:%s%s%s%s$ ' $USER $__fish_prompt_hostname "$__fish_prompt_cwd" (prompt_pwd) "$__fish_prompt_normal" $__git_cb
+  end
 end
