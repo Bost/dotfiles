8a26ae36a6779ace4a83a7f5c12ca8dd337f5cb0
Bost <thebost@gmail.com>
2017-01-08 14:43:41 +0100

    git aliases

--- fish/functions/crg.fish
+++ fish/functions/crg.fish
@@ -1,9 +1,10 @@
 function crg
-    ack-grep \
-    $dev/cheatsheet/commands-git.sh \
-    --context=1 \
-    --nogroup \
-    --break --with-filename --color-filename=grey13 \
-    --color-lineno=grey13 --color-match="bold blue" \
-    --ignore-case --match $argv
+  ack-grep \
+  $dev/cheatsheet/commands-git.sh \
+  $dev/dotfiles/.gitconfig \
+  --context=1 \
+  --nogroup \
+  --break --with-filename --color-filename=grey13 \
+  --color-lineno=grey13 --color-match="bold blue" \
+  --ignore-case --match $argv
 end
