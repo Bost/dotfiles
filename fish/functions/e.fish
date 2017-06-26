1babbc63190163e6645e836ee00a3911ec20734c
Bost <thebost@gmail.com>
2017-06-26 18:28:47 +0200

    e.fish: Don't use the --daemon switch

--- fish/functions/e.fish
+++ fish/functions/e.fish
@@ -1,17 +1,18 @@
 function e
-    set cnt (count $argv)
-    if test $cnt = 0
-        set params "./"
-    else
-        set params $argv
-    end
+  set cnt (count $argv)
+  if test $cnt = 0
+    set prms "./"
+  else
+    set prms $argv
+  end
 
-    # DYI violation because variables may not be used as commands
-    if pgrep --exact emacs
-        echo emacsclient $params &
-             emacsclient $params &
-    else
-        echo emacs $params &
-             emacs $params &
-    end
+  # DYI violation because variables may not be used as commands
+  if pgrep --exact emacs
+    echo emacsclient $prms &
+         emacsclient $prms &
+  else
+    # Don't use the --daemon switch. It producess a mess in the shell
+    echo emacs $prms &
+         emacs $prms &
+  end
 end
