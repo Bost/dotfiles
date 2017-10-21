233a75cc003a8991bb3b38f101df7fa6a434a40c
Bost <thebost@gmail.com>
2017-10-21 15:37:31 +0200

    Upgrade profiles: lein-ancient "0.6.14"

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -9,7 +9,7 @@
    ;; [refactor-nrepl "2.0.0-SNAPSHOT"]
    ;; see use-package clj-refactor :pin melpa-stable for newer versions
    [refactor-nrepl "2.3.1"]
-   [lein-ancient "0.6.10"]
+   [lein-ancient "0.6.14"]
    [lein-localrepo "0.5.4" :exclusions [org.clojure/clojure]]
    ;; lein kibit - check style
    [lein-kibit "0.1.5" :exclusions [org.clojure/clojure
