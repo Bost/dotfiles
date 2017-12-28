0f18d65d8142c03d507b2888d5cbb9dcd1f0c9a7
Bost <thebost@gmail.com>
2017-12-28 23:43:09 +0100

    backup

--- lumo/crep.cljs
+++ lumo/crep.cljs
@@ -2,32 +2,57 @@
 ;; -*- clojure -*-
 
 ;; sudo npm install -g lumo-cljs --unsafe-perm
+;; sudo npm install -g colour
+;; lumo crep.cljs sed
 
-(ns crep.core)
+(ns crep.core
+  (:require
+   [clojure.string :as cs]
+   [cljs.nodejs :as node]))
 
+(defonce clr (node/require "colors"))
 (def file "/home/bost/dev/cheatsheet/cmds/linux.sh")
 
 (defn read-file [file cont-fn]
   (.readFile (js/require "fs") file (clj->js {:encoding "utf8"}) cont-fn))
 
-(defn search [s data]
-  (->> (re-find (re-pattern (str "# .*?\n.*" s ".*\n")) data)
-       (.log js/console)))
+(defn decorate-input [data]
+  (as-> (cs/split data #"\n") w
+    #_(take 15 w)
+    (map-indexed (fn [idx line]
+                   (str idx ":" line "\n"))
+                 w)
+    (reduce str w)))
 
-#_(println "*command-line-args*" *command-line-args*)
+(defn decorate-output [ptrn m]
+  (as-> (re-pattern ptrn) v
+    (cs/split m v)
+    (interpose (.-green ptrn) v)
+    (reduce str v)))
 
-#_(def process (js/require "process"))
-#_(def args (.slice (.-argv process) 3))
-#_(println "(.-argv process)" (.-argv process))
+(defn search [ptrn data]
+  (doseq [m (->> data
+                 decorate-input
+                 (re-seq (re-pattern (str "\\d+:# .*?\n.*" ptrn ".*\n")))
+                 #_(map #(decorate-output ptrn %))
+                 #_vector)
+          ]
+    (->
+     (decorate-output ptrn m)
+     println)))
 
-#_(doseq [arg args]
-  (println arg))
+;; (println "*command-line-args*" *command-line-args*)
+;; (def process (js/require "process"))
+;; (def args (.slice (.-argv process) 3))
+;; (println "(.-argv process)" (.-argv process))
+;; (doseq [arg args] (println arg))
 
 (read-file file
            (fn [err data]
              (if err
                (throw (js/Error. err))
                (search
-                (first *command-line-args*)
-                #_(first args)
+                (first *command-line-args*
+                       ;; args
+                       )
                 data))))
