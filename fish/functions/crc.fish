21578c224d1a8d007f00fbd76d8b768278d9b943
Bost <thebost@gmail.com>
2018-02-24 20:34:32 +0100

    crc.fish

--- fish/functions/crc.fish
+++ fish/functions/crc.fish
@@ -1,5 +1,5 @@
 function crc
   # ack-cheat $dev/cheatsheet/cmds/clojure.clj $argv
-  set prm '{:cmt-str ";" :files ["'$dev/cheatsheet/cmds/clojure.clj'"]}'
+  set prm '{:cmt-str ";" :files ["'$dev/cheatsheet/clj/src/clj/core.clj'"]}'
   lumo $dev/dotfiles/lumo/crep.cljs $prm $argv
 end
