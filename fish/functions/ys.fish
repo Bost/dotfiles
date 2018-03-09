eba4798ac45c2042f1203df4fa94d80490185f02
Bost <thebost@gmail.com>
2018-03-09 11:03:59 +0100

    fix ys.fish

--- fish/functions/ys.fish
+++ fish/functions/ys.fish
@@ -1,6 +1,6 @@
 function ys
   # string delims needed - because of the url passed in the $argv
-  set cmd "youtube-dl --extract-audio $argv"
+  set cmd "youtube-dl --extract-audio '$argv'"
   echo $cmd
   eval $cmd
 end
