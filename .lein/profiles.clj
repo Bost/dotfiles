ab3f88a2146a1beb092c87626f87b6071fe3737a
Bost <thebost@gmail.com>
2018-05-11 16:16:31 +0200

    Don't use refactor-nrepl; Upgrade to cider/cider-nrepl "0.17.0"

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -5,9 +5,12 @@
  :user
  {:plugins
   [
-   [cider/cider-nrepl "0.16.0"]
    ;; see use-package clj-refactor :pin melpa-stable for newer versions
-   [refactor-nrepl "2.3.1"]
+   ;; doesn't work with cider/cider-nrepl "0.17.0"; it yields the
+   ;;     Unable to resolve var: refactor-nrepl.middleware/wrap-refactor
+   ;; [refactor-nrepl "2.3.1"]
+   [cider/cider-nrepl "0.17.0"]
+
    [lein-ancient "0.6.14"]
    [lein-localrepo "0.5.4" :exclusions [org.clojure/clojure]]
    ;; lein kibit - check style
