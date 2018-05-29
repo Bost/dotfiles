406cf5ccf2e9e0a328244badc5ba96a31a6fa963
Bost <thebost@gmail.com>
2018-05-29 11:33:42 +0200

    Update .lein/profiles.clj

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -3,20 +3,24 @@
 {
  ;; :repl {:plugins [[cider/cider-nrepl "0.16.0"]]}
  :user
- {:plugins
+ {
+  :plugins
   [
    ;; see use-package clj-refactor :pin melpa-stable for newer versions
    ;; doesn't work with cider/cider-nrepl "0.17.0"; it yields the
    ;;     Unable to resolve var: refactor-nrepl.middleware/wrap-refactor
    ;; [refactor-nrepl "2.3.1"]
-   [cider/cider-nrepl "0.17.0"]
+   ;; collection of nREPL middleware designed to enhance CIDER
+   [cider/cider-nrepl "0.17.0" :exclusions [org.clojure/tools.nrepl]]
 
-   [lein-ancient "0.6.14"]
-   [lein-localrepo "0.5.4" :exclusions [org.clojure/clojure]]
-   ;; lein kibit - check style
-   [lein-kibit "0.1.5" :exclusions [org.clojure/clojure
-                                    org.clojure/tools.cli]]
-   [lein-typed "0.4.2"]
+   ;; check for outdated dependencies and plugins
+   [lein-ancient "0.6.15"]
+   ;;  work with local Maven repository
+   [lein-localrepo "0.5.4"]
+   ;; lein kibit - check style: There's a function for that!
+   [lein-kibit "0.1.6"]
+   ;; type checking for Clojure with Clojure core.typed
+   [lein-typed "0.4.6"]
    ]
   :dependencies
   [
