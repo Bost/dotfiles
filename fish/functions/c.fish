5a97aaf1a04087ecff0e6c5a75fbbbcdb192e49a
Bost <thebost@gmail.com>
2018-07-25 16:43:22 +0200

    c.fish combine cat with crep.cljs

--- fish/functions/c.fish
+++ fish/functions/c.fish
@@ -1,19 +1,25 @@
 function c
-  # ack-cheat $dev/cheatsheet/cmds/rest.sh $argv
-  set f1 $dev/cheatsheet/cmds/linux.sh
-  set f2 $dev/cheatsheet/cmds/rest.sh
-  set f3 $dev/cheatsheet/cmds/findgrep.sh
-  set f4 $dev/cheatsheet/cmds/git.sh
-  set f5 $dev/cheatsheet/cmds/win.bat
-  set prm '{:cmt-str "#" :files ["'$f1'" "'$f2'" "'$f3'" "'$f4'" "'$f5'"]}'
-  lumo $dev/dotfiles/lumo/crep.cljs $prm $argv
+    if test -e $argv
+        set cmd cat $argv
+        # echo $cmd # otherwise c <file> | jq '.' doesn't work
+        eval $cmd
+    else
+        # ack-cheat $dev/cheatsheet/cmds/rest.sh $argv
+        set f1 $dev/cheatsheet/cmds/linux.sh
+        set f2 $dev/cheatsheet/cmds/rest.sh
+        set f3 $dev/cheatsheet/cmds/findgrep.sh
+        set f4 $dev/cheatsheet/cmds/git.sh
+        set f5 $dev/cheatsheet/cmds/win.bat
+        set prm '{:cmt-str "#" :files ["'$f1'" "'$f2'" "'$f3'" "'$f4'" "'$f5'"]}'
+        lumo $dev/dotfiles/lumo/crep.cljs $prm $argv
 
-  set f1 $dev/cheatsheet/clj/src/clj/core.clj
-  set f2 $dev/cheatsheet/cmds/emacs.el
-  set prm '{:cmt-str "#" :files ["'$f1'" "'$f2'"]}'
-  lumo $dev/dotfiles/lumo/crep.cljs $prm $argv
+        set f1 $dev/cheatsheet/clj/src/clj/core.clj
+        set f2 $dev/cheatsheet/cmds/emacs.el
+        set prm '{:cmt-str "#" :files ["'$f1'" "'$f2'"]}'
+        lumo $dev/dotfiles/lumo/crep.cljs $prm $argv
 
-  # set f1 $dev/cheatsheet/cmds/utf8.txt
-  # set prm '{:cmt-str "" :files ["'$f1'"]}'
-  # lumo $dev/dotfiles/lumo/crep.cljs $prm $argv
+        # set f1 $dev/cheatsheet/cmds/utf8.txt
+        # set prm '{:cmt-str "" :files ["'$f1'"]}'
+        # lumo $dev/dotfiles/lumo/crep.cljs $prm $argv
+    end
 end
