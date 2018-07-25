01f46ed60988b1b3728e356fb22305b26dc988e1
Bost <thebost@gmail.com>
2018-07-25 10:31:12 +0200

    git fetch --tags origin $argv; and git rebase origin $argv

--- fish/functions/glo.fish
+++ fish/functions/glo.fish
@@ -1,8 +1,11 @@
 function glo
-    echo git pull --rebase origin $argv
-         git pull --rebase origin $argv
-    echo git fetch --tags
-         git fetch --tags
-         echo $cmd
-         eval $cmd
+    # i.e. git pull --origin $argv; and git fetch --tags
+    # where:
+    #      git fetch --tags
+    # Fetch all tags from the remote (i.e., fetch remote tags refs/tags/* into
+    # local tags with the same name), in addition to whatever else would
+    # otherwise be fetched.
+    set cmd "git fetch --tags origin $argv; and git rebase origin $argv"
+    echo $cmd
+    eval $cmd
 end
