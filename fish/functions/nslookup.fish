bdd7ff080151f2e5071635b843b0a2086cff6609
Bost <thebost@gmail.com>
2018-12-21 19:28:16 +0100

    Using dig instead of lookup

---
+++ fish/functions/nslookup.fish
@@ -0,0 +1,8 @@
+function nslookup
+    set cmd dig (string escape -- $argv)
+    echo $cmd
+    echo "############################################################"
+    echo "### Using dig instead of lookup"
+    echo "############################################################"
+    eval $cmd
+end
