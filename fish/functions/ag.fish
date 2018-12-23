89b7372941bc7edd25ebca8646e124daf4f96572
Bost <thebost@gmail.com>
2018-12-23 12:00:35 +0100

    comments for /usr/bin/ag /usr/bin/du

---
+++ fish/functions/ag.fish
@@ -0,0 +1,7 @@
+function ag
+    set cmd /usr/bin/ag (string escape -- $argv)
+    echo $cmd
+    eval $cmd
+    echo "##########################################"
+    echo "### See also ripgrep, ack, pt, grep, sift"
+end
