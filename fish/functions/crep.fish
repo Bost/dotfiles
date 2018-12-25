7a5054d74e23bc349125977c8d5a6c684a6ffebb
Bost <thebost@gmail.com>
2018-12-25 14:41:51 +0100

    crep.fish: speedup by piping sed and grep(s)

--- fish/functions/crep.fish
+++ fish/functions/crep.fish
@@ -2,6 +2,12 @@ function crep
     set f1 $dev/cheatsheet/cmds/linux.sh
     set f2 $dev/cheatsheet/cmds/systemd.sh
     set f3 $dev/cheatsheet/cmds/rest.sh
-    set prm '{:cmt-str "#" :files ["'$f1'" "'$f2'" "'$f3'"]}'
-    lumo $dev/dotfiles/lumo/crep.cljs $prm $argv
+    # set prm '{:cmt-str "#" :files ["'$f1'" "'$f2'" "'$f3'"]}'
+    # lumo $dev/dotfiles/lumo/crep.cljs $prm $argv
+
+    # -P --perl-regexp
+    # -z --null-data
+    # -o --only-matching
+    # insert separating line   | greate matching groups  | grep for desired pattern
+    sed 's/^$/\n/' $f1 $f2 $f3 | grep -Pzo "\n(\S.*\n)*" | grep -Pz $argv
 end
