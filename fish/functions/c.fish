d3f29849e1c7593189a0b6600dd38ed2c202157d
Bost <thebost@gmail.com>
2018-12-31 09:59:13 +0100

    speedup by piping sed and grep(s)

--- fish/functions/c.fish
+++ fish/functions/c.fish
@@ -13,13 +13,15 @@ function c
         set f3 $dev/cheatsheet/cmds/findgrep.sh
         set f4 $dev/cheatsheet/cmds/git.sh
         set f5 $dev/cheatsheet/cmds/win.bat
-        set prm '{:cmt-str "#" :files ["'$f1'" "'$f2'" "'$f3'" "'$f4'" "'$f5'"]}'
-        lumo $dev/dotfiles/lumo/crep.cljs $prm $argv
+        # set prm '{:cmt-str "#" :files ["'$f1'" "'$f2'" "'$f3'" "'$f4'" "'$f5'"]}'
+        # lumo $dev/dotfiles/lumo/crep.cljs $prm $argv
+        sed 's/^$/\n/' $f1 $f2 $f3 $f4 $f5 | grep -Pzo "\n(\S.*\n)*" | grep -Pz $argv
 
         set f1 $dev/cheatsheet/clj/src/clj/core.clj
         set f2 $dev/cheatsheet/cmds/emacs.el
-        set prm '{:cmt-str "#" :files ["'$f1'" "'$f2'"]}'
-        lumo $dev/dotfiles/lumo/crep.cljs $prm $argv
+        # set prm '{:cmt-str "#" :files ["'$f1'" "'$f2'"]}'
+        # lumo $dev/dotfiles/lumo/crep.cljs $prm $argv
+        sed 's/^$/\n/' $f1 $f2 | grep -Pzo "\n(\S.*\n)*" | grep -Pz $argv
 
         # set f1 $dev/cheatsheet/cmds/utf8.txt
         # set prm '{:cmt-str "" :files ["'$f1'"]}'
--- fish/functions/crg.fish
+++ fish/functions/crg.fish
@@ -1,5 +1,7 @@
 function crg
-  # ack-cheat $dev/cheatsheet/cmds/git.sh $argv
-  set prm '{:cmt-str "#" :files ["'$dev/cheatsheet/cmds/git.sh'"]}'
-  lumo $dev/dotfiles/lumo/crep.cljs $prm $argv
+    # ack-cheat $dev/cheatsheet/cmds/git.sh $argv
+    set f1 $dev/cheatsheet/cmds/git.sh
+    # set prm '{:cmt-str "#" :files ["'$f1'"]}'
+    # lumo $dev/dotfiles/lumo/crep.cljs $prm $argv
+    sed 's/^$/\n/' $f1 | grep -Pzo "\n(\S.*\n)*" | grep -Pz $argv
 end
