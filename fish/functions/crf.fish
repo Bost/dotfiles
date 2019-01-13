a7b7e599e6121dbacef2b74ffd9d6d906f069f2a
Bost <thebost@gmail.com>
2019-01-13 14:14:30 +0100

    Speed up crf.fish

--- fish/functions/crf.fish
+++ fish/functions/crf.fish
@@ -1,5 +1,12 @@
 function crf
-  # ack-cheat $dev/cheatsheet/cmds/findgrep.sh $argv
-  set prm '{:cmt-str "#" :files ["'$dev/cheatsheet/cmds/findgrep.sh'"]}'
-  lumo $dev/dotfiles/lumo/crep.cljs $prm $argv
+    # ack-cheat $dev/cheatsheet/cmds/findgrep.sh $argv
+    set f1 $dev/cheatsheet/cmds/findgrep.sh
+    # set prm '{:cmt-str "#" :files ["'$f1'"]}'
+    # lumo $dev/dotfiles/lumo/crep.cljs $prm $argv
+
+    # -P --perl-regexp
+    # -z --null-data
+    # -o --only-matching
+    # insert separating line   | greate matching groups  | grep for desired pattern
+    sed 's/^$/\n/' $f1 | grep -Pzo "\n(\S.*\n)*" | grep -Pz $argv
 end
