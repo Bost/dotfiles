513ee92dacb7d73bb392e5d789784d78b977958f
Bost <thebost@gmail.com>
2019-01-14 13:19:02 +0100

    Try: tig as gitk alternative

--- fish/functions/gk.fish
+++ fish/functions/gk.fish
@@ -1,5 +1,6 @@
 function gk
-  set cmd gitk --all (string escape -- $argv) \&
-  echo $cmd
-  eval $cmd
+    echo "# Try: tig / tig --all / tig status / tig log / tig grep"
+    set cmd gitk --all (string escape -- $argv) \&
+    echo $cmd
+    eval $cmd
 end
