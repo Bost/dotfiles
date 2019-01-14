edaf3f31f29ccdb3684163c72ce3530b4038f841
Bost <thebost@gmail.com>
2019-01-14 14:15:38 +0100

    Speed up crc.fish

--- fish/functions/crc.fish
+++ fish/functions/crc.fish
@@ -1,5 +1,12 @@
 function crc
-  # ack-cheat $dev/cheatsheet/cmds/clojure.clj $argv
-  set prm '{:cmt-str ";" :files ["'$dev/cheatsheet/clj/src/clj/core.clj'"]}'
-  lumo $dev/dotfiles/lumo/crep.cljs $prm $argv
+    # ack-cheat $dev/cheatsheet/cmds/clojure.clj $argv
+    set f1 $dev/cheatsheet/clj/src/clj/core.clj
+    # set prm '{:cmt-str ";" :files ["'$f1'"]}'
+    # lumo $dev/dotfiles/lumo/crep.cljs $prm $argv
+
+    # -P --perl-regexp
+    # -z --null-data
+    # -o --only-matching
+    # insert separating line   | greate matching groups  | grep for desired pattern
+    sed 's/^$/\n/' $f1 $f2 $f3 | grep -Pzo "\n(\S.*\n)*" | grep -Pz $argv
 end
