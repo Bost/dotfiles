9d377febd35056094b8a5aad1a22b5bdd2dff164
Bost <thebost@gmail.com>
2019-01-18 16:21:59 +0100

    Trying snap

--- fish/functions/inst.fish
+++ fish/functions/inst.fish
@@ -1,5 +1,6 @@
 function inst
-  set cmd sudo apt install --yes (string escape -- $argv)
-  echo $cmd
-  eval $cmd
+    set cmd sudo snap install (string escape -- $argv)
+    # set cmd sudo apt install --yes (string escape -- $argv)
+    echo $cmd
+    eval $cmd
 end
--- fish/functions/rmv.fish
+++ fish/functions/rmv.fish
@@ -1,5 +1,6 @@
 function rmv
-  set cmd sudo apt remove --yes (string escape -- $argv)
+  set cmd sudo snap remove (string escape -- $argv)
+  # set cmd sudo apt remove --yes (string escape -- $argv)
   echo $cmd
   eval $cmd
 end
