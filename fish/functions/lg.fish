95277d4d6e4c691ab2e361714f3fbd4398c7c181
Bost <thebost@gmail.com>
2019-01-21 11:35:48 +0100

    lg.fish: unaliased use of git lg-all-20

--- fish/functions/lg.fish
+++ fish/functions/lg.fish
@@ -1,5 +1,6 @@
 function lg
-  set cmd git lg-20 (string escape -- $argv)
-  echo $cmd
-  eval $cmd
+    # This is in fact: set cmd git lg-all-20 (string escape -- $argv)
+    set cmd git log --graph -20 --color --all "--pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)%an%Creset'" (string escape -- $argv)
+    echo $cmd
+    eval $cmd
 end
