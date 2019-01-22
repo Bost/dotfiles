df9726d5ec6c196ea96cc748c24635fc6aa0808b
Bost <thebost@gmail.com>
2019-01-22 14:57:10 +0100

    lg.fish: don't use the --all param

--- fish/functions/lg.fish
+++ fish/functions/lg.fish
@@ -1,6 +1,7 @@
 function lg
-    # This is in fact: set cmd git lg-all-20 (string escape -- $argv)
-    set cmd git log --graph -20 --color --all "--pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)%an%Creset'" (string escape -- $argv)
+    set cmd git lg-20 (string escape -- $argv)
+    # the '--all' takes too long to execute
+    # set cmd git lg-all-20 (string escape -- $argv)
     echo $cmd
     eval $cmd
 end
