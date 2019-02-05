1550a514ef6463742a41c4ae825629fd29c9dab5
Bost <thebost@gmail.com>
2019-02-05 19:10:05 +0100

    which.fish: type --all ...

---
+++ fish/functions/which.fish
@@ -0,0 +1,8 @@
+function which
+    set cmd type --all (string escape -- $argv)
+    echo $cmd
+    echo "#########################"
+    echo "## see also /bin/which ##"
+    echo "#########################"
+    eval $cmd
+end
