0f5d9d2bfae221bb0443739ef3b8aa8bbd90cf0a
Bost <thebost@gmail.com>
2019-02-05 20:30:29 +0100

    prep.fish improvements

--- fish/functions/prep.fish
+++ fish/functions/prep.fish
@@ -7,19 +7,21 @@ function prep --description "See glances, cputool"
     # set ret (eval $cmd)
     # set pid (string split "/" -- $ret)[0]
 
-    set cmd pidof (string escape -- $argv)
-    echo $cmd
-    set pid (eval $cmd)
-
-    if test $pid
+    set cmd_pidof pidof (string escape -- $argv)
+    set cmd_pgrep pgrep --full (string escape -- $argv)
+    set cmd $cmd_pgrep
+    echo $cmd "   # see also: $cmd_pidof"
+    set pids (eval $cmd)
+    echo "# found" (count $pids) "process ids:" $pids
+    for pid in $pids
+        echo ""
         # Process 'niceness' vs. 'priority' https://askubuntu.com/a/656787
-
         # set cmd ps -o euser,ruser,suser,fuser,f,comm,label -p $pid
         # set cmd ps -o pid,tid,class,rtprio,ni,pri,psr,pcpu,stat,wchan:14,comm -p $pid
         set cmd ps -o ppid,pid,user,nice,%cpu,%mem,command --sort ppid -p $pid
         echo $cmd
         eval $cmd
-    else
-        echo "No PID found: pidof status/retCode $status"
     end
 end
+
+
