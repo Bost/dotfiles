0bb4f74d09625cac1af38fb244050d2d205a1247
Bost <thebost@gmail.com>
2019-02-05 17:01:54 +0100

    cru.fish: speed up

--- fish/functions/cru.fish
+++ fish/functions/cru.fish
@@ -1,5 +1,8 @@
 function cru
   # ack-cheat $dev/cheatsheet/cmds/utf8.txt $argv
-  set prm '{:cmt-str "" :files ["'$dev/cheatsheet/cmds/utf8.txt'"]}'
-  lumo $dev/dotfiles/lumo/crep.cljs $prm $argv
+  # set prm '{:cmt-str "" :files ["'$dev/cheatsheet/cmds/utf8.txt'"]}'
+  # lumo $dev/dotfiles/lumo/crep.cljs $prm $argv
+
+  set f1 $dev/cheatsheet/cmds/utf8.txt
+  grep --ignore-case (string escape -- $argv) $f1
 end
