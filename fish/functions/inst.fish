087236a7f27291e67ac3a214c004b48b51bd05c5
Bost <thebost@gmail.com>
2019-02-08 16:20:00 +0100

    rmv, inst: fallback to apt if snap doesn't have the package

--- fish/functions/inst.fish
+++ fish/functions/inst.fish
@@ -1,6 +1,11 @@
 function inst
     set cmd sudo snap install (string escape -- $argv)
-    # set cmd sudo apt install --yes (string escape -- $argv)
     echo $cmd
     eval $cmd
+
+    if test $status != 0
+        set cmd sudo apt install --yes (string escape -- $argv)
+        echo $cmd
+        eval $cmd
+    end
 end
--- fish/functions/rmv.fish
+++ fish/functions/rmv.fish
@@ -1,6 +1,12 @@
 function rmv
-  set cmd sudo snap remove (string escape -- $argv)
-  # set cmd sudo apt remove --yes (string escape -- $argv)
-  echo $cmd
-  eval $cmd
+    set cmd snap list (string escape -- $argv)
+    echo $cmd
+    eval $cmd
+    if test $status = 0
+        set cmd sudo snap remove (string escape -- $argv)
+    else
+        set cmd sudo apt remove --yes (string escape -- $argv)
+    end
+    echo $cmd
+    eval $cmd
 end
