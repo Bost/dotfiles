2436ffd00fcafca36e47927247212369443aa41c
Bost <thebost@gmail.com>
2019-02-19 13:22:35 +0100

    crc.fish: include cljdocs.clj

--- fish/functions/crc.fish
+++ fish/functions/crc.fish
@@ -1,11 +1,12 @@
 function crc
-    set f1 $dev/cheatsheet/clj/src/clj/cheat.clj
-    # set f1 $dev/cheatsheet/clj/src/clj/s.clj
-    # ack-cheat $f1 $argv
-    # set prm '{:cmt-str ";" :files ["'$f1'"]}'
+    set f1 $dev/cheatsheet/clj/src/clj/cljdocs.clj
+    set f2 $dev/cheatsheet/clj/src/clj/cheat.clj
+    # ack-cheat $f1 $f2 $argv
+    # set prm '{:cmt-str ";" :files ["'$f1'" "'$f2'"]}'
     # lumo $dev/dotfiles/lumo/crep.cljs $prm $argv
     if test $argv = "--files"
         echo $f1
+        echo $f2
         return
     end
     # -P --perl-regexp
@@ -14,7 +15,9 @@ function crc
     # -o --only-matching
     # -e PATTERN, --regepx=PATTERN
     set prms -Pzo
-    # insert separating line | greate matching groups     | grep for desired pattern
-    sed 's/^$/\n/' $f1       | grep -Pzo ";;.+\n(.*\n)*?\n\n" | grep -Pz -e (string escape -- $argv)
+    # insert separating line | greate matching groups         | grep for desired pattern
+    sed 's/^$/\n/' $f1 | grep -Pzo ";;.+\n(.*\n)*?\n" | grep -Pz -e (string escape -- $argv)
+    echo "============================================== $f2"
+    sed 's/^$/\n/' $f2 | grep -Pzo ";;.+\n(.*\n)*?\n" | grep -Pz -e (string escape -- $argv)
 end
 
