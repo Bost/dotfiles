0d382d927af1865de8ce11c99cddd87a0efd6ae8
Bost <thebost@gmail.com>
2019-03-11 23:02:34 +0100

    crc.fish: add ~/.lein/profiles.clj

--- fish/functions/crc.fish
+++ fish/functions/crc.fish
@@ -1,9 +1,10 @@
 function crc
     set f1 $dev/cheatsheet/clj/src/clj/cljdocs.clj
     set f2 $dev/cheatsheet/clj/src/clj/cheat.clj
+    set f3 ~/.lein/profiles.clj
     set files $f1
     cheat-grep $argv $lispline $files
-    set files $f2
+    set files $f2 $f3
     set separator "=============================================="
     echo $separator $files $separator
     cheat-grep $argv $lispline $files
