bf430e4e90a9948760ed805046d76ac7742e3780
Bost <thebost@gmail.com>
2019-03-19 11:56:08 +0100

    --word-regex hint; ignore `compiled` subdir

--- fish/functions/grepc.fish
+++ fish/functions/grepc.fish
@@ -4,15 +4,18 @@ function grepc --description "grep code; see also grepl.fish"
     # -n, --line-number
     # -i, --ignore-case
     # -r, --recursive
+    # -w, --word-regexp
 
     # set opts -nir
     set opts --ignore-case --line-number --recursive
     set incl "--include=\*.{el,clj,cljs,cljc,org,py,md,rst,adoc}"
-    set excl "--exclude-dir={.git,target}"
+    # we're searching recursivelly; out of `resources/public/js/compiled` only
+    # the `compiled` subdir needs to be ignored
+    set excl "--exclude-dir={.git,target,compiled}"
     # wdir is undefined i.e. examine the working directory
     # set wdir ./
     set cmd grep $opts $fdirs $incl $excl (string escape -- $argv) $wdir
     eval $cmd
-    echo "#######################################################"
+    echo "### TODO repeat w/ --word-regex if nothing found, i.e. \$status == 1"
     echo $cmd
 end
