d1890675347b4b62dd6197e84529cba5aa29384b
Bost <thebost@gmail.com>
2019-03-27 13:59:55 +0100

    gtg.fish: Command was ...

--- fish/functions/gtg.fish
+++ fish/functions/gtg.fish
@@ -2,4 +2,6 @@ function gtg
   set cmd git tag --sort version:refname (string escape -- $argv)
   echo $cmd
   eval $cmd
+  # repeat the command in case of too many tags to were printed
+  echo "# Command was:" $cmd
 end
