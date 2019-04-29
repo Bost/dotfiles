d50a231a3b9029313e8951bdf8f5344b4e3ed193
Bost <thebost@gmail.com>
2019-04-29 11:15:56 +0200

    fix xfce4-genmon.sh

--- xfce4-genmon.sh
+++ xfce4-genmon.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
-# xfce4-genmon-plugin
-# only the process name of the process using most of the CPU
-echo "<txt>"$(ps a -o comm --sort=-%cpu | head -n 2 | tail -1)"</txt>"
-# process name, pid and short command of the process using most of the CPU
-echo "<tool>"$(ps a -o %cpu,pid,comm --sort=-%cpu | head -n 2 | tail -1)"</tool>"
+# Script for the xfce4-genmon-plugin
+# obtain the info about the process consuming most of the CPU
+cmd=$(ps ax -o %cpu,pid,comm --sort=-%cpu | head -n 2 | tail -1)
+# echo ${cmd}
+cpu=$(grep --only-matching -e "[-+]\?[0-9]*\.\?[0-9]\+" <<< ${cmd} | head -n 1)
+pid=$(grep --only-matching -e "[-+]\?[0-9]*\.\?[0-9]\+" <<< ${cmd} | tail -n 1)
+comm=$(grep --only-matching -e "\w*" <<< ${cmd} | tail -n 1)
+# echo "<txt>"$(ps a -o comm --sort=-%cpu | head -n 2 | tail -1)"</txt>"
+echo "<txt>"${comm}"</txt>"
+echo "<tool>"${comm}": "${cpu}"% CPU, pid: "${pid}"</tool>"
