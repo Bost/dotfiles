500b63a111e3af2e7ef06041e339b93754add2d7
Bost <thebost@gmail.com>
2019-05-10 17:05:27 +0200

    Formatting

--- fish/functions/e.fish
+++ fish/functions/e.fish
@@ -1,19 +1,19 @@
 function e
-  # TODO systemctl --user enable emacs
-  set cnt (count $argv)
-  if test $cnt = 0
-    set prms "./"
-  else
-    set prms $argv
-  end
+    # TODO systemctl --user enable emacs
+    set cnt (count $argv)
+    if test $cnt = 0
+        set prms "./"
+    else
+        set prms $argv
+    end
 
-  # DYI violation because variables may not be used as commands
-  if pgrep --exact emacs
-    set cmd emacsclient $prms \& disown
-  else
-    # Don't use the --daemon switch. It producess a mess in the shell
-    set cmd emacs $prms \& disown
-  end
-  echo $cmd
-  eval $cmd
+    # DYI violation because variables may not be used as commands
+    if pgrep --exact emacs
+        set cmd emacsclient $prms \& disown
+    else
+        # Don't use the --daemon switch. It producess a mess in the shell
+        set cmd emacs $prms \& disown
+    end
+    echo $cmd
+    eval $cmd
 end
