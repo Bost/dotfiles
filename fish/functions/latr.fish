b9a0447e23399936fb72030749f12a90fc953240
Bost <thebost@gmail.com>
2019-07-23 10:54:00 +0200

    latr.fish improvements

--- fish/functions/latr.fish
+++ fish/functions/latr.fish
@@ -1,6 +1,6 @@
 function latr --description "Sort by modification time, newest first, reversed"
     # set cmd ls -latr (string escape -- $argv)
-    set cmd ls -lt --all --reverse (string escape -- $argv)
+    set cmd ls --sort=time -l --almost-all --reverse (string escape -- $argv)
     eval $cmd
     echo $cmd
 end
