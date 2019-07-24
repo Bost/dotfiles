9e48fdcbb203c285f451eb74519a4ff3aae8e52a
Bost <thebost@gmail.com>
2019-07-24 12:36:32 +0200

    crep.fish: search in vim.vim

--- fish/functions/crep.fish
+++ fish/functions/crep.fish
@@ -1,6 +1,7 @@
 function crep
     set pth $dev/cheatsheet/cmds
     set files $files $pth/emacs.el
+    set files $files $pth/vim.vim
     set files $files $pth/shells.sh
     set files $files $pth/linux.sh
     set files $files $pth/rest.sh
