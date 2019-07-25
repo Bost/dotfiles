8bf9baa2d05329ae073cd60895d6cc1ba19a487a
Bost <thebost@gmail.com>
2019-07-25 11:48:53 +0200

    e.fish: start new emacs server during guile-emacs compilation

--- fish/functions/e.fish
+++ fish/functions/e.fish
@@ -8,13 +8,25 @@ function e
     end
 
     set prms $prms \& disown
-    # DYI violation because variables may not be used as commands
-    if pgrep --exact emacs
-        set cmd emacsclient $prms
-    else
-        # Don't use the --daemon switch. It producess a mess in the shell
-        set cmd emacs $prms
+    # Don't use the --daemon switch. It producess a mess in the shell
+    set emacsCmd emacs
+    set pids (pgrep --exact emacs)
+    # echo "pids:" $pids
+    for pid in $pids
+        set procUser (ps -o user= -p $pid)
+        # echo "pid:" $pid "procUser:" $procUser
+        if test $USER = $procUser
+            set procCmd (ps -o command= -p $pid)
+            # echo "procCmd:" $procCmd
+            if string match -q -- "*defunct*" $procCmd
+                # echo "pid:" $pid "is defunct"
+            else
+                set emacsCmd emacsclient
+                break
+            end
+        end
     end
+    set cmd $emacsCmd $prms
     echo $cmd
     eval $cmd
 end
