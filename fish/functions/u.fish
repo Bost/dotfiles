708e0f2e3506948a35375bf0c5c18250a82422b6
Bost <thebost@gmail.com>
2019-08-07 14:40:14 +0200

    u.fish: TODO do not execute checkrestart when nothing upgraded

--- fish/functions/u.fish
+++ fish/functions/u.fish
@@ -1,6 +1,8 @@
 function u
   # string delims - '; and'
   set cmd "sudo apt update; and sudo apt full-upgrade --yes; and sudo checkrestart"
+  # TODO do not execute checkrestart when nothing upgraded
+  # sudo apt full-upgrade --yes | tail -1 | grep --only-matching "\([0-9]*\?\)"
   echo $cmd
   eval $cmd
   echo "# Try out: sudo apt update; and sudo apt upgrade; and sudo apt dist-upgrade"
