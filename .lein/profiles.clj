8ffabb85ec2ac9e6ad707ff7a1f5aa9345ef5707
Bost <thebost@gmail.com>
2019-08-24 12:06:53 +0200

    cider/cider-nrepl "0.22.0"

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -12,7 +12,7 @@
    ;; nREPL middleware to support editor refactorings
    [refactor-nrepl "2.5.0-SNAPSHOT"]
    ;; collection of nREPL middleware designed to enhance CIDER
-   [cider/cider-nrepl "0.22.0-beta9"]
+   [cider/cider-nrepl "0.22.0"]
 
    ;; network REPL providing Srv, Cli, some common APIs etc.
    [nrepl "0.6.0"]
