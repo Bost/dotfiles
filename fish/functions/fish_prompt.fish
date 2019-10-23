eab8fe13e6afb5a79d57534c4ecd949ac18c7dd5
Bost <thebost@gmail.com>
2019-10-23 13:03:06 +0200

    Noise

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,7 +1,5 @@
 function fish_prompt --description 'Write out the prompt'
-	
     set stat $status
-
     if not set -q __fish_prompt_normal
         set -g __fish_prompt_normal (set_color normal)
     end
@@ -17,9 +15,7 @@ function fish_prompt --description 'Write out the prompt'
     end
 
     switch "$USER"
-
         case root toor
-
             if not set -q __fish_prompt_cwd
                 if set -q fish_color_cwd_root
                     set -g __fish_prompt_cwd (set_color $fish_color_cwd_root)
@@ -27,16 +23,11 @@ function fish_prompt --description 'Write out the prompt'
                     set -g __fish_prompt_cwd (set_color $fish_color_cwd)
                 end
             end
-
             printf '%s@%s %s%s%s# ' $USER (prompt_hostname) "$__fish_prompt_cwd" (prompt_pwd) "$__fish_prompt_normal"
-
         case '*'
-
             if not set -q __fish_prompt_cwd
                 set -g __fish_prompt_cwd (set_color $fish_color_cwd)
             end
-
             printf '[%s] %s%s@%s %s%s %s(%s)%s \f\r> ' (date "+%H:%M:%S") "$__fish_color_blue" $USER (prompt_hostname) "$__fish_prompt_cwd" "$PWD" "$__fish_color_status" "$stat" "$__fish_prompt_normal"
-
     end
 end
