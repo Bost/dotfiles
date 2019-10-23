1293308f4bb35723dbf545b0d3bb309bb8abc3cb
Bost <thebost@gmail.com>
2019-10-23 14:42:30 +0200

    crc.fish: wip

--- fish/functions/script.clj
+++ fish/functions/script.clj
@@ -1,14 +1,36 @@
 (ns script
   (:require
    [cheshire.core :refer :all]
+   [clojure.walk :refer [keywordize-keys]]
    [clojure.pprint :refer [pprint]]
    [clojure.repl :refer [apropos doc] #_:all]))
 
-(let [stream
+#_(println "pwd" (System/getProperty "user.dir"))
+#_(println "home" (System/getProperty "user.home"))
+
+(def vars
+  (->> (str (System/getProperty "user.home")
+            "/dev/dotfiles/fish/functions/"
+            "clojuredocs-export.json")
+       clojure.java.io/reader
+       parse-stream
+       keywordize-keys
+       :vars
+       (take 2)
+       (map (fn [hm]
+              (select-keys hm [:name :examples])))
+       (map (fn [hm]
+              {:name (:name hm)
+               :examples (->> (:examples hm)
+                              (mapv :body))}))))
+
+
+#_(let [stream
       ;; parse a stream (keywords option also supported)
       (parse-stream (clojure.java.io/reader
-                     "$dev/dotfiles/fish/functions/clojuredocs-export.json"))]
-  (println "pwd" (System/getProperty "user.dir") )
+                     (str (System/getProperty "user.home")
+                          "/dev/dotfiles/fish/functions/"
+                          "clojuredocs-export.json")))]
   (println "(count stream)" (count stream))
   (doseq [arg *command-line-args*]
     (printf ";; > (clojure.repl/doc %s)\n" arg)
