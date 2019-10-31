f928a527d7f4af00b80b55877eac35e5c0bd7760
Bost <thebost@gmail.com>
2019-11-01 00:22:51 +0100

    l.fish: exa - modern replacement for ls

--- fish/functions/l.fish
+++ fish/functions/l.fish
@@ -1,5 +1,12 @@
 function l
-    set cmd ls -lA --color "--time-style='+%d-%m-%Y %H:%M:%S'" (string escape -- $argv)
+    set escArgv (string escape -- $argv)
+    if test -s ~/bin/exa
+        # TODO consider custom coloring after `ls --color=never`
+        set cmd exa --long --git --all --time-style=long-iso $escArgv
+        # set cmd exa --long --git --all --extended --time=modified $escArgv
+    else
+        set cmd ls -lA --color "--time-style='+%d-%m-%Y %H:%M:%S'" $escArgv
+    end
     echo $cmd
     eval $cmd
 end
