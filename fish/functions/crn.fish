48f9b11096985e70be7b6ea169c4e891873b5bdb
Bost <thebost@gmail.com>
2019-11-26 16:10:53 +0100

    crn.fish: mv *.{org,md}

--- fish/functions/crn.fish
+++ fish/functions/crn.fish
@@ -1,9 +1,9 @@
 function crn
     set d $dev/notes
-    cheat-grep $argv $d/category-theory.org \
+    cheat-grep $argv $d/category-theory.md \
                      $d/computer-sciences.org \
                      $d/logics.org \
                      $d/math.org \
                      $d/notes.org \
-                     $d/math-structures.org
+                     $d/math-structures.md
 end
