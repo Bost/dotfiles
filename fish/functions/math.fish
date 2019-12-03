f1cdd02f5847bd396f7a55e030b4acd6917c7e20
Bost <thebost@gmail.com>
2019-12-03 15:33:28 +0100

    math.fish: TODO eval with clj only if it has lisp syntax

--- fish/functions/math.fish
+++ fish/functions/math.fish
@@ -1,9 +1,10 @@
-function math
-    set escArgv (string escape -- $argv)
-    echo "escArgv" $escArgv
-    echo "argv" $argv
-    # set cmd clj --eval $escArgv
-    set cmd builtin math $escArgv
-    echo $cmd
-    eval $cmd
-end
+# TODO eval with clj only if it has lisp syntax
+# function math
+#     set escArgv (string escape -- $argv)
+#     echo "escArgv" $escArgv
+#     echo "argv" $argv
+#     # set cmd clj --eval $escArgv
+#     set cmd builtin math $escArgv
+#     echo $cmd
+#     eval $cmd
+# end
