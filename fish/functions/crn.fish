117d34c64e0463eb6aefcb44d7725878ca4241a9
Bost <thebost@gmail.com>
2019-12-04 13:46:34 +0100

    Putting functions to their respective project dirs

--- fish/functions/crn.fish
+++ fish/functions/crn.fish
@@ -1,9 +1,1 @@
-function crn
-    set d $dev/notes
-    cheat-grep $argv $d/category-theory.md \
-                     $d/computer-sciences.org \
-                     $d/logics.org \
-                     $d/math.org \
-                     $d/notes.org \
-                     $d/math-structures.md
-end
+/home/bost/dev/notes/bin/crn.fish
