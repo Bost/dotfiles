f1575553908a4db794338e987bf5caa5d495b9f3
Bost <thebost@gmail.com>
2019-12-05 01:49:53 +0100

    Unify b.fish and c.fish

--- fish/functions/b.fish
+++ fish/functions/b.fish
@@ -1,16 +1,3 @@
 function b
-    set escArgv (string escape -- $argv)
-    if test -z "$escArgv"
-        # echo "Udefined or empty escArgv:" $escArgv
-        l
-    else if test -d $escArgv # is it a directory?
-        l $estArgv
-    else if test -f $estArgv # is it a regular file?
-        set cmd bat $escArgv
-        # echo $cmd # otherwise c <file> | jq '.' doesn't work
-        eval $cmd
-        # is a directory?
-    else
-        crep $escArgv $files
-    end
+    c $argv
 end
--- fish/functions/c.fish
+++ fish/functions/c.fish
@@ -4,11 +4,16 @@ function c
         # echo "Udefined or empty escArgv:" $escArgv
         l
     else if test -d $escArgv # is it a directory?
-        l $estArgv
-    else if test -f $estArgv # is it a regular file?
-        echo "# TODO use `bat`"
-        echo "#####################"
-        set cmd cat $escArgv
+        l $escArgv
+    else if test -f $escArgv # is it a regular file?
+        if test -f /usr/bin/bat
+            set binary bat
+        else
+            echo "# TODO install `bat`"
+            echo "#####################"
+            set binary cat
+        end
+        set cmd $binary $escArgv
         # echo $cmd # otherwise c <file> | jq '.' doesn't work
         eval $cmd
         # is a directory?
