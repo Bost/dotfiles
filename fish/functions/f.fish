9e1f0bfdc64928312e193005ab65cda79302e2b8
Bost <thebost@gmail.com>
2019-12-05 02:14:29 +0100

    testing fd / fd-find / fdfind

--- fish/functions/f.fish
+++ fish/functions/f.fish
@@ -2,7 +2,8 @@ function f
     # example: f '*.clj'
     # set cmd find -name (string escape -- $argv)
     # -iname case insensitive
-    set cmd find -iname (string escape -- $argv)
+    # set cmd find -iname (string escape -- $argv)
+    set cmd fdfind $argv
     eval $cmd
-    echo $cmd
+    echo "#" $cmd
 end
---
+++ fish/functions/find.fish
@@ -0,0 +1,4 @@
+function find
+    /usr/bin/find $argv
+    echo "### try: fd / fdfind" $argv
+end
