ae80bfde458583557a6efa1931a61d38be5d5eb0
Bost <thebost@gmail.com>
2019-12-24 12:26:59 +0100

    goodies.fish: use needrestart

--- fish/functions/goodies.fish
+++ fish/functions/goodies.fish
@@ -1,5 +1,6 @@
 function goodies
-  set cmd sudo checkrestart (string escape -- $argv)
-  echo $cmd
-  eval $cmd
+    # set cmd sudo checkrestart (string escape -- $argv)
+    set cmd sudo needrestart (string escape -- $argv)
+    echo $cmd
+    eval $cmd
 end
