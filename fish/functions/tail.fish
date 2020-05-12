4f3f0b1a9252b9f7f981970878d4867b5503cbed
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2020-05-12 04:14:00 +0200

    tail.fish: don't execute `less +F`, print just a note about it.

--- fish/functions/tail.fish
+++ fish/functions/tail.fish
@@ -1,6 +1,5 @@
 function tail --description "See https://www.brianstorti.com/stop-using-tail/"
-    # `tail -f` is better when watching multiple files at the same time
-    set less +F $argv
-    eval $cmd
-    echo $cmd
+    echo "# Note:"
+    echo "# Instead of `/usr/bin/tail -f ...` try to use `less +F ...`"
+    echo "# However `/usr/bin/tail -f ...` is better when watching multiple files at the same time"
 end
