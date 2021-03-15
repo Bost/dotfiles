f471104337e7334ee3848548ee69a48396271fa7
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2021-03-15 13:51:30 +0100

    fish: emag

---
+++ fish/functions/emag.fish
@@ -0,0 +1,10 @@
+function emag --description="ema && glo"
+    # this gets immediately evaluated: set cmd ema; and glo
+    set cmd cd $dev/emacs
+    echo $cmd
+    eval $cmd
+
+    set cmd glo
+    echo $cmd
+    eval $cmd
+end
