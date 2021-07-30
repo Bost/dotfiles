fc078065cc2dd8e29acab3aefc6a39f50e0e6493
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2021-07-31 00:04:29 +0200

    Rename guix.fish -> guix-os.fish

--- fish/functions/guix-os.fish
+++ fish/functions/guix-os.fish
@@ -1,19 +1,21 @@
-function guix --description "GNU Guix via qemu-system-x86_64"
+function guix-os --description "GNU Guix via qemu-system-x86_64"
 
+set guixFile guix-system-vm-image-1.3.0.x86_64-linux.qcow2
+# 6GB RAM
 qemu-system-x86_64 \
     -nic user,model=virtio-net-pci \
-    -enable-kvm -m 1024 \
+    -enable-kvm -m 6G \
     -device virtio-blk,drive=myhd \
-    -drive if=none,file=guix-system-vm-image-1.2.0.x86_64-linux,id=myhd \
+    -drive if=none,file=$guixFile,id,id=myhd \
     & disown
 
 # share clipboard using virt-viewer with Spice - doesn't work
 # see https://guix.gnu.org/manual/en/html_node/Running-Guix-in-a-VM.html
 # qemu-system-x86_64 \
 #     -nic user,model=virtio-net-pci \
-#     -enable-kvm -m 1024 \
+#     -enable-kvm -m 6G \
 #     -device virtio-blk,drive=myhd \
-#     -drive if=none,file=guix-system-vm-image-1.2.0.x86_64-linux,id=myhd \
+#     -drive if=none,file=$guixFile,id=myhd \
 #     -device virtio-serial-pci,id=virtio-serial0,max_ports=16,bus=pci.0,addr=0x5 \
 #     -chardev spicevmc,name=vdagent,id=vdagent \
 #     -device virtserialport,nr=1,bus=virtio-serial0.0,chardev=vdagent, \
