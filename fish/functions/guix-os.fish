64b73e78aec2ecde03fb7437fcb15ae74690ac01
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2021-08-06 00:49:22 +0200

    Guix in a VM with(!) shared clipboard.
    Thanx to iskarian from the #guix irc channel

--- fish/functions/guix-os.fish
+++ fish/functions/guix-os.fish
@@ -12,7 +12,7 @@ function guix-os --description "GNU Guix via qemu-system-x86_64"
     set guixFile ~/guix/guix-system-vm-image-1.3.0.x86_64-linux.qcow2
     set guixRAM 1G
 
-    # 1. this works, however without shared clipboard:
+    # this works, however without shared clipboard:
     # qemu-system-x86_64 \
     #     -nic user,model=virtio-net-pci \
     #     -enable-kvm -m $guixRAM \
@@ -20,40 +20,18 @@ function guix-os --description "GNU Guix via qemu-system-x86_64"
     #     -drive if=none,file=$guixFile,id=myhd \
     #     & disown
 
-    # 2. running with '-spice port=5930,disable-ticketing'...
+
+    # with shared clipboard:
     qemu-system-x86_64 \
         -nic user,model=virtio-net-pci \
         -enable-kvm -m $guixRAM \
         -device virtio-blk,drive=myhd \
         -drive if=none,file=$guixFile,id=myhd \
+        -device virtio-serial-pci,id=virtio-serial0,max_ports=16,bus=pci.0,addr=0x5 \
+        -chardev spicevmc,name=vdagent,id=vdagent \
+        -device virtserialport,nr=1,bus=virtio-serial0.0,chardev=vdagent,name=com.redhat.spice.0 \
         -spice port=5930,disable-ticketing \
         & disown
-    # ... starts a qemu process in the background. How to connect to it with
-    # virt-viewer is unclear, hmm.
-
-    # 3. trying out the "10.16.2 Using virt-viewer with Spice" instructions...
-    # qemu-system-x86_64 \
-    #     -nic user,model=virtio-net-pci \
-    #     -enable-kvm -m $guixRAM \
-    #     -device virtio-blk,drive=myhd \
-    #     -drive if=none,file=$guixFile,id=myhd \
-    #     -device virtio-serial-pci,id=virtio-serial0,max_ports=16,bus=pci.0,addr=0x5 \
-    #     -chardev spicevmc,name=vdagent,id=vdagent \
-    #     -device virtserialport,nr=1,bus=virtio-serial0.0,chardev=vdagent, \
-    #     name=com.redhat.spice.0 \
-    #     & disown
-    # ... produces an error:
-    # qemu-system-x86_64: name=com.redhat.spice.0: Could not open 'name=com.redhat.spice.0': No such file or directory
-
-    # 4. omitting the 'name=com.redhat.spice.0'...
-    # qemu-system-x86_64 \
-    #     -nic user,model=virtio-net-pci \
-    #     -enable-kvm -m $guixRAM \
-    #     -device virtio-blk,drive=myhd \
-    #     -drive if=none,file=$guixFile,id=myhd \
-    #     -device virtio-serial-pci,id=virtio-serial0,max_ports=16,bus=pci.0,addr=0x5 \
-    #     -chardev spicevmc,name=vdagent,id=vdagent \
-    #     -device virtserialport,nr=1,bus=virtio-serial0.0,chardev=vdagent, \
-    #     & disown
-    # ... starts Guix in the VM, however without shared clipboard.
+    # now open up a new terminal on the (Ubuntu) host and connect with:
+    #     remote-viewer spice://localhost:5930 & disown
 end
