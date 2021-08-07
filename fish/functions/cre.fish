3e3d8c436d8f3092308d7f0b708f8c32ad6b1058
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2021-08-07 10:06:00 +0200

    cre.fish: use cheat-grep

--- fish/functions/cre.fish
+++ fish/functions/cre.fish
@@ -2,10 +2,8 @@ function cre --description "Search through the emacs and elisp cheatsheet"
     # rg --type org $argv ~/.emacs.d
     # echo
     # rg --with-filename $argv $cheat/cmds/emacs.org
+
     set pth $cheat/cmds
     set files $files $pth/emacs.org
-    # set files $cheat-github/cheat/cheatsheets/*
-    # set files $files $pth/win.bat
-    # set files $files $pth/host.org
     cheat-grep --grep-args="$argv" --files="$files"
 end
