076d0caddeab20fe084bab52f09be8207c3b0bb3
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2021-10-31 21:37:42 +0100

    susp.fish: use xfce4-session-logout --suspend

--- fish/functions/susp.fish
+++ fish/functions/susp.fish
@@ -1,6 +1,6 @@
 function susp
-  # set cmd xfce4-session-logout --suspend
-  set cmd systemctl suspend
+  set cmd xfce4-session-logout --suspend
+  # set cmd systemctl suspend
   echo $cmd
   eval $cmd
 end
