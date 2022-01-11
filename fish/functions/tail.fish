38f598f7b77ca45f497891828dba19ae3519e7df
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2022-01-11 15:20:56 +0100

    delete tail.fish

--- fish/functions/tail.fish
+++
@@ -1,5 +0,0 @@
-function tail --description "See https://www.brianstorti.com/stop-using-tail/"
-    echo "# Note:"
-    echo "# Instead of `/usr/bin/tail -f ...` try to use `less +F ...`"
-    echo "# However `/usr/bin/tail -f ...` is better when watching multiple files at the same time"
-end
