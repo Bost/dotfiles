e139b226778d231988858f5c19bc3d6a9590eec7
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2022-01-15 13:32:20 +0100

    Implement cx cw in guile scheme

---
+++ bin/cx.scm
@@ -0,0 +1,23 @@
+
+;; run by `guile cx.scm'
+
+;; On non-Guix OS
+;; #!/usr/bin/guile -s
+;; !#
+
+;; On GuixOS:
+;; #!/run/current-system/profile/bin/guile -s
+;; !#
+
+;; Example:
+;;     chmod --recursive u=rwx,g=rwx,o=rwx /path/to/dir
+
+(apply
+ system*
+ ((compose
+   (lambda (cmd)
+     "(cdr (command-line)) can be an empty list which breaks system*. `append'
+takes care of it"
+     (append cmd (cdr (command-line)))))
+  (list (string-append (getenv "systemBinDir") "/chmod")
+        "+x")))
