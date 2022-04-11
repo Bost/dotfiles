199b305d0865d233d606c7dd079ed91c4ca18f48
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2022-04-11 13:27:45 +0200

    Upgrade .lein/profiles.clj deps

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -7,27 +7,29 @@
   :plugins
   [
    [lein-figwheel "0.5.20"]
+
    ;; see use-package clj-refactor :pin melpa-stable for newer versions
    ;; doesn't work with cider/cider-nrepl "0.17.0"; it yields the
    ;;     Unable to resolve var: refactor-nrepl.middleware/wrap-refactor
    ;; nREPL middleware to support editor refactorings
-   [refactor-nrepl "2.5.1"]
+   [refactor-nrepl "3.5.2"]
+
    ;; collection of nREPL middleware designed to enhance CIDER
-   [cider/cider-nrepl
-    #_"0.24.0" ;; recommended by refactor-nrepl
-    "0.25.5" ;; latest version
-    ;; :exclusions [nrepl]
-    ]
+   [cider/cider-nrepl "0.28.3"]
 
    ;; check for outdated dependencies and plugins
-   [lein-ancient "0.6.15"]
-   ;;  work with local Maven repository
+   [lein-ancient "0.7.0"]
+
+   ;; work with local Maven repository
    [lein-localrepo "0.5.4"]
+
    ;; lein kibit - check style: There's a function for that!
    [lein-kibit "0.1.8"]
+
    ;; auto-compile Garden stylesheets
    [lein-garden "0.3.0"]
-   ;; genere API docu from clj or cljs source
+
+   ;; generate API docu from clj or cljs source
    [lein-codox "0.10.5"]
 
    ;; Drive leiningen project version from git instead of the other way around
@@ -39,13 +41,13 @@
    ;; [lein-virgil "0.1.9"]
 
    ;; National Vulnerability Database dependency-checker
-   [lein-nvd "1.4.1"]
+   [nvd-clojure/nvd-clojure "2.5.0"]
    ]
 
   :dependencies
   [
    ;; network REPL providing Srv, Cli, some common APIs etc.
-   [nrepl "0.8.3"]
+   [nrepl "0.9.0"]
 
    ;; [spyscope "0.1.7-SNAPSHOT"] ;; the SNAPSHOT must be build from the source
    #_[clj-kondo "2020.03.20"]    ;; A linter for Clojure code that sparks joy
