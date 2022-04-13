297071f2ef39c763302598c113526f42068d19d2
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2022-04-13 14:04:46 +0200

    f.fish: detect if fd or fdfind is installed

--- fish/functions/f.fish
+++ fish/functions/f.fish
@@ -3,7 +3,16 @@ function f
     # set cmd find -name (string escape -- $argv)
     # -iname case insensitive
     # set cmd find -iname (string escape -- $argv)
-    set cmd fdfind $argv
-    eval $cmd
-    echo "#" $cmd
+    if test (which fd 2> /dev/null)
+        set fdBin fd
+    else if test (which fdfind 2> /dev/null)
+        set fdBin fdfind
+    end
+    if test $fdBin
+        set cmd $fdBin $argv
+        eval $cmd
+        # echo "#" $cmd
+    else
+        printf "ERR: neither fd nor fdfind installed\n"
+    end
 end
