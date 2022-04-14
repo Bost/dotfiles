ceb8c3c6fa68cfb39d5eb666b3188feb04829dc4
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2022-04-14 09:56:36 +0200

    spag.scm: fix missing --git-dir, cleanup

--- bin/spag.scm
+++ bin/spag.scm
@@ -33,13 +33,15 @@
   prm)
 
 (define* (git #:rest args)
-  (cons* "git" (string-append "--work-tree=" (getenv "HOME") "/.emacs.d")
-         args))
+  (let ((h (getenv "HOME")))
+    (cons* "git"
+           (string-append "--git-dir=" h "/.emacs.d/.git")
+           (string-append "--work-tree=" h "/.emacs.d")
+           args)))
 
 (define (main args)
   (map exec
        (list
-        (git "checkout" "develop")
-        (git "fetch" "--tags" "origin")
-        (git "rebase")
+        (git "fetch" "--tags" "origin" "develop")
+        (git "rebase" "origin/develop" "develop")
         (git "rebase" "develop" "cycle"))))
