ca3b53c5c91f7302b441c6bef8862a26a47202b0
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2022-06-27 20:29:41 +0200

    guix home: manage ghog (push-to-remotes)

--- guix/guix-home/home-configuration.scm
+++ guix/guix-home/home-configuration.scm
@@ -778,9 +778,7 @@
         #:use-module (ice-9 regex)
         #:use-module (ice-9 popen)
         #| #:use-module (guix build utils) ;; invoke - not needed |#
-        #:export (the-answer partial dbg exec))
-
-      (define the-answer 42)
+        #:export (partial dbg exec))
 
       (define (partial fun . args)
         (lambda x (apply fun (append args x))))
@@ -970,12 +968,58 @@
                   "exa" "-abghHliS" "--color=always" "--time-style=long-iso"
                   ;; '--file-type' append indicator (one of /=>@|) to entries
                   ;; TODO consider custom coloring after `ls --color=never`
-                  "ls" "-lA" "--file-type" "--color" "--time-style=+%d-%m-%Y %H:%M:%S"
+                  "ls" "-lA" "--file-type" "--color"
+                       "--time-style=+%d-%m-%Y %H:%M:%S"
                   |#)
                  cdr)
                 (command-line))))))
     (chmod-plus "rw")
-    (chmod-plus "x")))
+    (chmod-plus "x")
+
+    `(,(string-append scm-bin-dirname "/ghog")
+      ,(program-file
+        "git-push-to-remotes"
+        (with-imported-modules
+            ;; TODO clarify is source-module-closure needed only for imports of
+            ;; guix modules?
+            `(((utils) => ,utils))
+          #~(begin
+              (use-modules (ice-9 rdelim)
+                           (ice-9 regex)
+                           (ice-9 popen)
+                           (utils))
+              (let ((args (command-line)))
+                ((compose
+                  (partial
+                   map
+                   (compose
+                    cdr
+                    exec
+                    (lambda (remote)
+                      (append
+                       (list "git" "push" "--follow-tags" "--verbose" remote)
+                       (cdr args)))
+                    car))
+                  (partial filter (lambda (remote-url)
+                                    (not (null? (cdr remote-url)))))
+                  (partial map
+                           (lambda (remote)
+                             (cons remote
+                                   ((compose
+                                     (partial filter
+                                              (lambda (url)
+                                                (string-match "git@" url)))
+                                     cdr
+                                     exec
+                                     (partial list "git" "remote" "get-url"))
+                                    remote))))
+                  (partial filter (lambda (remote)
+                                    (not (string-match "heroku" remote))))
+                  cdr
+                  exec)
+                 (list
+                  "git" "remote")))))))
+    ))
 
    #;
    (simple-service
@@ -985,7 +1029,6 @@
              ,(local-file (string-append (getenv "HOME") "/dev/dotfiles/bin/" f)
                           (string-append "bin-" f))))
          (list "g1" "g1.scm"
-               "ghog" "ghog.scm"
                "glo" "glo.scm"
                "guix-os" "guix-os.scm"
                "spag" "spag.scm"
