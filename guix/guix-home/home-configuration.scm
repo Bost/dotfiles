a866921a4ef29f31ad14c7ea2dbe0793337e2659
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2022-06-27 17:24:56 +0200

    guix home: reuse code using (define-module ...)

--- guix/guix-home/home-configuration.scm
+++ guix/guix-home/home-configuration.scm
@@ -788,7 +788,64 @@ takes care of it"
 ;; fish and bash separate elements of a list with a different separator
 (define bash-list-separator ":")
 (define fish-list-separator " ")
-(define scm-bin-dir "scm-bin")
+(define scm-bin-dirname "scm-bin")
+(define scm-bin-dirpath (string-append "/" scm-bin-dirname))
+
+(define utils
+  (scheme-file
+   "utils.scm"
+   #~(
+      (define-module (utils)
+        #:use-module (ice-9 rdelim)
+        #:use-module (ice-9 regex)
+        #:use-module (ice-9 popen)
+        #| #:use-module (guix build utils) ;; invoke - not needed |#
+        #:export (the-answer partial dbg exec))
+
+      (define the-answer 42)
+
+      (define (partial fun . args)
+        (lambda x (apply fun (append args x))))
+
+      (define (dbg prm)
+        (format #t "\n~a\n" prm)
+        prm)
+
+      (define (read-all port)
+        "Return a list of all lines from the PORT."
+        (let loop ((res '())
+                   (str (read-line port))) ; from (ice-9 popen)
+          (if (and str (not (eof-object? str)))
+              (loop (append res (list str))
+                    (read-line port))
+              res)))
+
+      (define (exec command)
+        "Usage:
+(let* ((ret (exec command)))
+    (if (= 0 (car ret))
+        (let* ((output (cdr ret)))
+          #| process output |#)
+        (format #t \"Command failed\")))"
+        ((compose
+          (lambda (command)
+            (let* ((port (open-input-pipe command)) ; from (ice-9 rdelim)
+                   (str  (read-all port)))
+              (cons
+               (status:exit-val (close-pipe port))
+               str)))
+          (lambda (s)
+            ;; TODO implement pretty-print for bash commands
+            ;; ~a - outputs an argument like display
+            ;; ~s - outputs an argument like write (i.e. print to string)
+            ;; ~% is newline \n
+            (format #t "~a~%" s)
+            s)
+          (lambda (cmd) (if (list? cmd)
+                            (string-join cmd " ")
+                            cmd)))
+         command)))
+   #:splice? #t))
 
 (home-environment
  (packages
@@ -843,7 +900,7 @@ takes care of it"
       `(("PATH" . ,(string-join
                     (list
                      ;; my own scripts take precedence...
-                     (string-append "$HOME/" scm-bin-dir)
+                     (string-append "$HOME" scm-bin-dirpath)
                      ;; TODO create the link
                      ;;     ln -s ~/dev/dotfiles/bin ~/bin
                      ;; using guix home
@@ -897,7 +954,16 @@ takes care of it"
    (simple-service
     'scheme-files home-files-service-type
     (list
-     `(,(string-append scm-bin-dir "/l") ,(program-file "l.scm" (sexp->gexp l-scm)))))
+     `(,(string-append scm-bin-dirname "/l") ,(program-file "l.scm" (sexp->gexp l-scm)))
+     `(,(string-append scm-bin-dirname "/crw")
+       ,(program-file
+         "crw.scm"
+         (with-imported-modules `(((utils) => ,utils))
+           #~(begin
+               (use-modules (utils))
+               ;; Example:
+               ;;     chmod --recursive u=rwx,g=rwx,o=rwx /path/to/dir
+               (exec (cons* "chmod +rw" (cdr (command-line))))))))))
 
    #;
    (simple-service
