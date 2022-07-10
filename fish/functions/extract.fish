06d4513a62b7061d795513eaf463021b4d8a2340
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2022-07-10 14:07:39 +0200

    guix home: wip - manage extract.fish

--- fish/functions/extract.fish
+++ fish/functions/extract.fish
@@ -6,45 +6,30 @@ function extract
 
   # set file $argv[1]
   set file (string escape -- $argv[1])
+  # TODO in Guile use `cond` or `match`
   switch $file
-    case "*.tar.bz2"
-      set cmd tar xjf $file
     case "*.tar.gz"
       set cmd tar xzf $file
       if test $status -eq 2
         echo "WARN: The archive is propably is a tar, not a GZip. Trying alterternative:"
         set cmd tar xvf $file
       end
-    case "*.bz2"
-      set cmd bunzip2 $file
-    case "*.rar"
-      set cmd unrar e $file
-    case "*.gz"
-      set cmd gunzip $file
-    case "*.tar"
-      set cmd tar xf $file
-    case "*.tbz2"
-      set cmd tar xjf $file
-    case "*.tgz"
-      set cmd tar xzf $file
-    case "*.jar"
-      set cmd unzip $file
-    case "*.war"
-      set cmd unzip $file
-    case "*.zip"
-      set cmd unzip $file
-    case "*.Z"
-      set cmd uncompress $file
-    case "*.7z"
-      set cmd 7z x $file
-    case "*.tar.xz"             # LZMA/LZMA2 algorithms
-      set cmd tar xvfJ $file
-    case "*.pax"
-      set cmd pax -r < $file
-    case "*.dmg"                # Apple Mac image
-        # set cmd dmg2img $file "$file.img"
-        set cmd dmg2img $file
-        # set cmd 7z x $file
+    case "*.tar.bz2" set cmd tar xjf $file
+    case "*.bz2" set cmd bunzip2 $file
+    case "*.rar" set cmd unrar e $file
+    case "*.gz" set cmd gunzip $file
+    case "*.tar" set cmd tar xf $file
+    case "*.tbz2" set cmd tar xjf $file
+    case "*.tgz" set cmd tar xzf $file
+    case "*.jar" set cmd unzip $file
+    case "*.war" set cmd unzip $file
+    case "*.zip" set cmd unzip $file
+    case "*.Z" set cmd uncompress $file
+    case "*.7z" set cmd 7z x $file
+    case "*.tar.xz" set cmd tar xvfJ $file # LZMA/LZMA2 algorithms
+    # causes 'Expected end of the statement, but found a redirection'
+    # case "*.pax" set cmd pax -r < $file
+    case "*.dmg" set cmd dmg2img $file     # set cmd 7z x $file # Apple Mac image # set cmd dmg2img $file "$file.img"
     case "\"*\""
        echo "ERROR: double quotes \"...\" shouldn't match the file extension"
     case "'*'"
