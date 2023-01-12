74cd80cfea635a70b62aebcde7a0e8cae0d9e687
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2023-01-12 18:25:01 +0100

    wip: gxh: implement chmod using exec-system*

--- guix/home/scm-bin/chmod.scm
+++ guix/home/scm-bin/chmod.scm
@@ -1,6 +1,6 @@
 (define-module (scm-bin chmod)
   #:use-module (utils)
-  #:export (main))
+  #:export (main chmod))
 
 #|
 
@@ -10,9 +10,27 @@
 
 |#
 
+;; new implementation
+(define* (chmod #:rest args)
+  (apply exec-system*
+         "chmod" (string-append "+" (car args))
+         (cdr args)))
+
+;; (define* (main #:rest args)
+;;   "Usage:
+;; (main \"<ignored>\" \"-f\" \"arg0\")"
+;;   ((compose
+;;     (partial apply chmod)
+;;     (partial apply cdr)
+;;     dbg)
+;;    args))
+
+
+;; old implementation
 (define (main modifier args)
   ((compose
     exec
     (partial cons* (string-append "chmod +" modifier))
     cdr)
    args))
+
