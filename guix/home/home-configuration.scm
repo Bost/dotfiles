c13b38fbf371216704758178659659a1b9b4ae74
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2023-01-15 14:24:58 +0100

    gxh: srvc my=fish

--- guix/home/home-configuration.scm
+++ guix/home/home-configuration.scm
@@ -36,6 +36,7 @@ TODO see https://github.com/daviwil/dotfiles/tree/guix-home
   #:use-module (cfg packages)
   #:use-module (cfg spguimacs-packages)
   #:use-module (cfg mcron)
+  #:use-module (srvc my=fish)
   ;; See service-file -> with-imported-modules
   #:use-module (utils)
   #:use-module (common settings)
@@ -59,12 +60,6 @@ TODO see https://github.com/daviwil/dotfiles/tree/guix-home
   ;; the https://issues.guix.gnu.org/51359 has not been merged yet
   #| home-git-service-type |#
   ;; #:use-module (gnu home services version-control)
-
-  ;; for the my=home-fish-service-type
-  #:use-module (gnu services configuration)
-  #:use-module (guix packages)
-  #:use-module (ice-9 match)
-
   )
 
 (define indent "")
@@ -75,13 +70,13 @@ TODO see https://github.com/daviwil/dotfiles/tree/guix-home
 (define channels-scm-filepath
   (str (basename xdg-config-home) "/guix/channels.scm"))
 
+(define (fix-leading-dot filename)
+  (string-replace filename "dot-" 0 1))
+
 (def* (dotfiles-home #:rest args)
   "Note:
 (format #t \"~a\" \"foo\") doesn't work"
   (apply str home "/dev/dotfiles" args))
-
-(define (fix-leading-dot filename)
-  (string-replace filename "dot-" 0 1))
 
 ;;;      ...                      #:optional (<parameter-name> <default-value>)
 (def* (any-local-file filepath #:optional (filename (basename filepath)))
@@ -123,19 +118,9 @@ TODO see https://github.com/daviwil/dotfiles/tree/guix-home
         (format #t "ERROR: can't read ~a\n" filepath)
         #f))))
 
-(def* (fish-config-base #:rest args)
-  "(fish-config-base) ; => \".config/fish\""
-  (apply str (basename xdg-config-home) "/fish" args))
-
-(def* (fish-config-dotfiles #:rest args)
-  "(fish-config-dotfiles) ; => \"/home/bost/dev/dotfiles/.config/fish\"
-Note:
-(format #t \"~a\" \"foo\") doesn't work"
-  (apply str (dotfiles-home) "/" (fish-config-base) args))
-
 ;; fish and bash separate elements of a list with a different separator
 (define list-separator-bash ":")
-(define list-separator-fish " ")
+#;(define list-separator-fish " ") ;; not needed
 (define bin-dirpath "/bin")
 (define sbin-dirpath "/sbin")
 (define scm-bin-dirname "scm-bin")
@@ -217,10 +202,6 @@ Note:
                                   #;(str home "/.npm-packages"))
                             list-separator))))
 
-;; fish and bash separate elements of a list with a different separator
-(define list-separator-bash ":")
-#;(define list-separator-fish " ") ;; not needed
-
 (def* environment-variables-service
   (simple-service
    'environment-variables-service
@@ -301,13 +282,6 @@ Example:
          #~(begin
              (use-modules (scm-bin #$symb))
              #$main-call))))))
-
-(def* (append-fish-config-dir dir lst)
-  (append
-   `((,(fish-config-base dir)
-      ,(local-file (fish-config-dotfiles dir)
-                   #:recursive? #t)))
-   lst))
 
 ;; xfce4-keyboard: repeat-delay 160 repeat-speed 60
 
@@ -481,9 +455,6 @@ Example:
 (define (bash-config-file name content)
   (shell-config-file "bash" name content))
 
-(define (fish-config-file name content)
-  (shell-config-file "fish" name content))
-
 (def* home-dir-config-service
   ;; TODO add to home-dir-config: notes, rest of the $dotf/.emacs.d directory
   (simple-service
@@ -535,31 +506,42 @@ Example:
                  `(,destination ,(local-file (dotfiles-home "/" dir)
                                              #:recursive? #t)))))))))
 
+(begin
+  (def* (fish-config-base #:rest args)
+    "(fish-config-base) ; => \".config/fish\""
+    (apply str (basename xdg-config-home) "/fish" args))
+
+  (def* (fish-config-dotfiles #:rest args)
+    "(fish-config-dotfiles) ; => \"/home/bost/dev/dotfiles/.config/fish\"
+Note:
+(format #t \"~a\" \"foo\") doesn't work"
+    (apply str (dotfiles-home) "/" (fish-config-base) args))
+
 ;;; TODO The (copy-file ...) is not an atomic operation, i.e. it's not undone
 ;;; when the 'guix home reconfigure ...' fails or is interrupted.
 ;;; Can't use `local-file' or `mixed-text-file' or something similar since the
 ;;; `fish_variables' must be editable
-(let* [(filepath "/fish_variables")
-       (src (fish-config-dotfiles filepath))
-       (dst (user-home "/" (fish-config-base filepath)))]
+  (let* [(filepath "/fish_variables")
+         (src (fish-config-dotfiles filepath))
+         (dst (user-home "/" (fish-config-base filepath)))]
 ;;; TODO is this sexp is not executed because of lazy-evaluation?
-  (let [(indent (str indent indent-inc))]
-    (format #t "~a(copy-file ~a ~a) ... " indent src dst)
-    (let ((retval (copy-file src dst)))
-      (format #t "retval: ~a\n" retval)
-      ;; The value of 'retval' is '#<unspecified>'
-      retval))
+    (let [(indent (str indent indent-inc))]
+      (format #t "~a(copy-file ~a ~a) ... " indent src dst)
+      (let ((retval (copy-file src dst)))
+        (format #t "retval: ~a\n" retval)
+        ;; The value of 'retval' is '#<unspecified>'
+        retval))
 ;;; Just changing ownership and permissions of `fish_variables' doesn't work:
-  #;
-  (begin
-    ;; .rw-r--r-- bost users fish_variables
-    (format #t "(chown ~a ~a ~a)\n" dst (getuid) (getgid))
-    (chown dst (getuid) (getgid))
-    ;; .rw-r--r-- fish_variables
-    (format #t "(chmod ~a ~a)\n" dst #o644)
-    (chmod dst #o644)))
 
-(format #t "~a ... \n" "(define scheme-files-service ...)")
+    ;; (begin
+    ;;   ;; .rw-r--r-- bost users fish_variables
+    ;;   (format #t "(chown ~a ~a ~a)\n" dst (getuid) (getgid))
+    ;;   (chown dst (getuid) (getgid))
+    ;;   ;; .rw-r--r-- fish_variables
+    ;;   (format #t "(chmod ~a ~a)\n" dst #o644)
+    ;;   (chmod dst #o644))
+    ))
+
 (define scheme-files-service
   (simple-service
    'scheme-files-service
@@ -638,224 +620,6 @@ Example:
     (service-file #:program-name "spag"
                   #:desc "spacemacs-git-fetch-rebase"))))
 (format #t "done\n")
-
-
-(define (my=serialize-fish-aliases field-name val)
-  ;; (format #t "[serialize-fish-aliases] field-name: ~a; val: ~a\n" field-name val)
-  #~(string-append
-     #$@(map (match-lambda
-               ((key . value)
-                #~(string-append "alias " #$key " \"" #$value "\"\n"))
-               (_ ""))
-             val)))
-
-(define (my=serialize-fish-abbreviations field-name val)
-  ;; (format #t "[serialize-fish-abbreviations] field-name: ~a; val: ~a\n" field-name val)
-  #~(string-append
-     #$@(map (match-lambda
-               ((key . value)
-                #~(string-append "abbr --add " #$key " " #$value "\n"))
-               (_ ""))
-             val)))
-
-(define (my=serialize-fish-env-vars field-name val)
-  ;; (format #t "[serialize-fish-env-vars] field-name: ~a; val: ~a\n" field-name val)
-  #~(string-append
-     #$@(map (match-lambda
-               ((key . #f)
-                "")
-               ((key . #t)
-                #~(string-append "set -x " #$key "\n"))
-               ((key . value)
-                #~(string-append "set -x " #$key " "  #$value "\n")))
-             val)))
-
-(define-configuration my=home-fish-configuration
-  (package
-    (package fish)
-    "The Fish package to use.")
-  (config
-   (text-config '())
-   "List of file-like objects, which will be added to
-@file{$XDG_CONFIG_HOME/fish/config.fish}.")
-  (environment-variables
-   (alist '())
-   "Association list of environment variables to set in Fish."
-   my=serialize-fish-env-vars)
-  (aliases
-   (alist '())
-   "Association list of aliases for Fish, both the key and the value
-should be a string.  An alias is just a simple function that wraps a
-command, If you want something more akin to @dfn{aliases} in POSIX
-shells, see the @code{abbreviations} field."
-   my=serialize-fish-aliases)
-  (abbreviations
-   (alist '())
-   "Association list of abbreviations for Fish.  These are words that,
-when typed in the shell, will automatically expand to the full text."
-   my=serialize-fish-abbreviations)
-  )
-
-(define (my=fish-packages config)
-  "Defines how is the `home-profile' (i.e. the `home-profile-service-type') extended."
-  (let ((ret (list (my=home-fish-configuration-package config))))
-    (format #t "### [my=fish-packages] ret: \n~a\n\n" ret)
-    ret))
-
-(define-configuration/no-serialization my=home-fish-extension
-  (config
-   (text-config '())
-   "List of file-like objects for extending the Fish initialization file.")
-  (environment-variables
-   (alist '())
-   "Association list of environment variables to set.")
-  (aliases
-   (alist '())
-   "Association list of Fish aliases.")
-  (abbreviations
-   (alist '())
-   "Association list of Fish abbreviations.")
-  )
-
-(define (my=fish-config-files config)
-  "Defines how is the `home-xdg-configuration' (i.e. the `home-xdg-configuration-files-service-type') extended"
-     #|
-     (partial append
-              `((,(fish-config-base)
-;;; TODO modify `local-file' so that can copy files from /gnu/store with
-                 ;; different stats, not only as '.r--r--r-- root root'
-                 ,(local-file (fish-config-dotfiles)
-                              #:recursive? #t
-                              #:select?
-                              (lambda (file stats)
-                                (let* [(ret (or
-;;; `fish_prompt.fish' (among others) changes the content of `fish_variables' so
-;;; this file must be present and editable otherwise all sorts of
-;;; 'Permission denied' problems are to be expected.
-
-                                             (has-suffix? file "/fish_variables")
-;;; `config.fish' is copied by `home-fish-configuration'
-                                             (has-suffix? file "/config.fish")))]
-                                  (when ret
-                                    (format #t "excluding: ~a ~a\n" file stats))
-                                  (not ret)))))))
-     |#
-  (let ((ret `(("fish/completions"
-                ,(local-file (fish-config-dotfiles "/completions")
-                             #:recursive? #t))
-               ("fish/conf.d"
-                ,(local-file (fish-config-dotfiles "/conf.d")
-                             #:recursive? #t))
-               ("fish/functions"
-                ,(local-file (fish-config-dotfiles "/functions")
-                             #:recursive? #t))
-               ("fish/fish_plugins"
-                ;; fish_plugins is just a file, not a directory
-                ,(local-file (fish-config-dotfiles "/fish_plugins")))
-               ("fish/config.fish"
-                ,(mixed-text-file
-                  "fish-config.fish"
-                  #~(string-append "\
-# if we haven't sourced the login config, do it
-status --is-login; and not set -q __fish_login_config_sourced
-and begin
-
-  set --prepend fish_function_path "
-                                   #$fish-foreign-env
-                                   "/share/fish/functions
-  fenv source $HOME/.profile
-  set -e fish_function_path[1]
-
-  set -g __fish_login_config_sourced 1
-
-end\n\n")
-                  (serialize-configuration
-                   config my=home-fish-configuration-fields))))))
-    (format #t "### [my=fish-config-files] ret: \n~a\n\n" ret)
-    ret))
-
-(define (my=home-fish-extensions original-config extension-configs)
-  "`home-fish-extensions' defines how the value of the service is extended with the composition of the extensions"
-  (format #t "### [my=home-fish-extensions] original-config: \n~a\n\n" original-config)
-  (format #t "### [my=home-fish-extensions] extension-configs: \n~a\n\n" extension-configs)
-  (let ((ret (my=home-fish-configuration
-              (inherit original-config)
-              (config
-               (append (my=home-fish-configuration-config original-config)
-                       (append-map
-                        my=home-fish-extension-config extension-configs)))
-              (environment-variables
-               (append (my=home-fish-configuration-environment-variables original-config)
-                       (append-map
-                        my=home-fish-extension-environment-variables extension-configs)))
-              (aliases
-               (append (my=home-fish-configuration-aliases original-config)
-                       (append-map
-                        my=home-fish-extension-aliases extension-configs)))
-              (abbreviations
-               (append (my=home-fish-configuration-abbreviations original-config)
-                       (append-map
-                        my=home-fish-extension-abbreviations extension-configs))))))
-    (format #t "### [my=home-fish-extensions] ret: \n~a\n\n" ret)
-    ret))
-
-(define my=home-fish-service-type
-  (service-type (name 'my=home-fish)
-                (extensions
-                 (list
-                  (service-extension
-                   home-xdg-configuration-files-service-type
-;;; this function returns all the configuration files of the fish-shell from the
-;;; home-xdg-configuration-files-service, i.e. config, environment-variables,
-;;; aliases and abbreviations
-
-;;; TODO what about the content of the completions directory? Should it return
-;;; too?
-                   my=fish-config-files)
-                  (service-extension
-                   home-profile-service-type
-;;; this function returns all the installed fish-shell packages from the
-;;; home-profile-service
-                   my=fish-packages)
-
-                  ))
-
-                (compose identity)
-
-                (extend
-;;; `my=home-fish-extensions' defines how the value of the service is extended
-;;; with the composition of the extensions
-                 my=home-fish-extensions)
-                (default-value (my=home-fish-configuration))
-                (description "my=home-fish-service-type with completions.")))
-
-(define (m2=fish-config-files config)
-  (let* ((m2= `(("fish/completions"
-                 ,(local-file (fish-config-dotfiles "/completions")
-                              #:recursive? #t))
-                ("fish/conf.d"
-                 ,(local-file (fish-config-dotfiles "/conf.d")
-                              #:recursive? #t))
-                ("fish/functions"
-                 ,(local-file (fish-config-dotfiles "/functions")
-                              #:recursive? #t))
-                ("fish/fish_plugins"
-                 ;; fish_plugins is just a file, not a directory
-                 ,(local-file (fish-config-dotfiles "/fish_plugins")))))
-         (ret (append my=home-fish-configuration-fields m2=))
-         )
-    (format #t "### [m2=fish-config-files] ret: \n~a\n\n" ret)
-    ret))
-
-(define m2=home-fish-service-type
-  (service-type (name 'm2=home-fish)
-                (extensions
-                 (list
-                  (service-extension
-                   my=home-fish-service-type
-                   m2=fish-config-files)
-                  ))
-                (description "m2=home-fish-service-type with completions.")))
 
 (define my=services
   (list
@@ -939,25 +703,7 @@ end\n\n")
    ;; https://github.com/search?q=home-fish-service-type&type=code
    ;; see https://github.com/babariviere/brycus/blob/e22cd0c0b75c5b4c95369fc95cce95ed299b63ff/guix/brycus/home-service.scm
 
-   (service
-    my=home-fish-service-type
-    #;home-fish-service-type
-    ;; fish configuration - see ~/dev/guix/gnu/home/services/shells.scm
-    (my=home-fish-configuration
-     ;; Abbreviations are implemented as shell scripts. The TAB key is annoying.
-     ;; 1. Erase all abbreviations in the fish-shell:
-     ;;     abbr --erase (abbr --list)
-     ;; 2. Erase all `_fish_abbr_*' variables from the
-     ;;    '.config/fish/fish_variables'
-
-     ;; aliases for "l" "ll" "ls" may be be overridden - see bashrc aliases
-     #;(aliases '(("l" . "ls -a")))
-
-     (config
-      (list
-       (local-file
-        (fish-config-dotfiles "/config.fish"))))))
-
+   my=fish-service
    environment-variables-service
    home-dir-config-service
    scheme-files-service
@@ -1033,20 +779,7 @@ end\n\n")
 ;;; TODO see [PATCH] services: Add udev-rules-service helper.
 ;;; https://issues.guix.gnu.org/40454
 
-   (services
-    my=services
-    #;
-    (modify-services my=services
-      (home-fish-service-type
-       conf =>
-       (home-fish-configuration
-        (inherit conf)
-        (config
-         (list
-          (local-file
-           (fish-config-dotfiles "/config.fish")))
-    #;
-    (append-fish-config-dir "/completions" '()))))))))
+   (services my=services)))
 
 ;; TODO put home-configuration and system-configuration in one file
 ;; (if (getenv "RUNNING_GUIX_HOME") home system)
