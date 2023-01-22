411320b367bdc95bcf81a66a4e56978f2c6422de
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2023-01-22 12:52:12 +0100

    gxh: wip: define which-emacs and emacs-output-path as functions

--- guix/home/scm-bin/emacs-launcher.scm
+++ guix/home/scm-bin/emacs-launcher.scm
@@ -1,7 +1,11 @@
 (define-module (scm-bin emacs-launcher)
-  ;; for emacs-output-path
-  #:use-module (gnu packages emacs)
-  #:use-module (guix) ;; open-connection
+  #|
+  #:use-module (gnu packages emacs) ; for emacs-output-path
+  #:use-module (guix)               ; for open-connection
+  ;; Activating the two lines above leads to:
+  ;;   In procedure resolve-interface: no code for module (gnu packages emacs)
+  ;;   In procedure resolve-interface: no code for module (guix)
+  |#
 
   #:use-module (utils)
   #:export (main))
@@ -17,7 +21,7 @@
 (format #t "~a ... " "Evaluating emacs-launcher.scm")
 
 ;; returns "/home/bost/.guix-home/profile/bin/emacs"
-(define which-emacs
+(define (which-emacs)
   (let* ((ret (exec "which emacs")))
     (if (= 0 (car ret))
         (let* ((output (cdr ret)))
@@ -26,12 +30,12 @@
         (error-command-failed))))
 
 ;; returns "/gnu/store/c39qm5ql5w9r6lwwnhangxjby57hshws-emacs-28.2/bin/emacs"
-(define emacs-output-path
-  ((compose
-    (partial format #f "~a/bin/emacs")
-    derivation->output-path
-    (partial package-derivation (open-connection)))
-   emacs))
+;; (define (emacs-output-path)
+;;   ((compose
+;;     (partial format #f "~a/bin/emacs")
+;;     derivation->output-path
+;;     (partial package-derivation (open-connection)))
+;;    emacs))
 
 (define (main args)
   ((compose
@@ -43,8 +47,8 @@
 ;;; See `dotspacemacs-server-socket-dir' in the `.spacemacs'.
 ;;; Tilda '~' doesn't work
                    "$HOME/.emacs.d" "/server/server")
-              ;; which-emacs
-              emacs-output-path))
+              ;; (emacs-output-path)
+              (which-emacs)))
     (lambda (prms) (if (null? prms) '("./") prms))
     cdr)
    args))
