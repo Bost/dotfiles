aa934c34b713baac48c11e3862dcc5d4d0c136b1
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2023-02-08 00:34:33 +0100

    [wip] gxh: better gcl - git clone

--- .config/fish/functions/gcl.fish
+++ .config/fish/functions/gcl.fish
@@ -6,13 +6,20 @@ function gcl
     echo $cmd
     eval $cmd
     if test $status = 0
-        if test -d $escArgv
-            set dir $escArgv
-        else
-            set dir (filename (basename $escArgv[(count $escArgv)]))
-        end
-        set cmd cd $dir
+        # get the last command line parameter and call `basename` on it.
+        set cmd cd (basename $escArgv[(count $escArgv)])
         echo $cmd
         eval $cmd
     end
 end
+
+## Test:
+# rm -rf /tmp/foo/ /tmp/bar/
+# mkcd /tmp/foo
+## the '.io' is cut off from the 'ambrevar.gitlab.io':
+# gcl https://gitlab.com/ambrevar/ambrevar.gitlab.io
+#
+# mkcd /tmp/bar
+## clone to the correct directory works; 'cd ambrevar.gitlab.io' fails:
+# gcl /tmp/foo/ambrevar.gitlab.io
+
--- guix/home/scm-bin/gcl.scm
+++ guix/home/scm-bin/gcl.scm
@@ -8,21 +8,6 @@
 -L ./ -e (gcl) -s
 !#
 
-;; Examples:
-(use-module (guix download))
-
-(source
- (origin
-   (method url-fetch)
-   (uri "http://www.example.com/3.2.1.tar.gz")
-   (sha256 %null-sha256)))
-
-(origin
-  (method git-fetch)
-  (uri (git-reference
-        (url "http://www.example.com/x.git")
-        (commit "0")))
-  (sha256 %null-sha256))
 |#
 
 (define* (gcl #:rest args)
@@ -33,15 +18,92 @@
         (gcl \"-f arg0\"))
 ;; > #t
 "
+  ;; git-command implementation see $dev/guix/tests/git.scm
   (apply exec-system*
          "git" "clone"
          args))
 
+(define (show-help)
+  (format #t "Usage: ~a [OPTION] NAME [ARGS ...]
+Clone git repository using the following command:
+
+  git clone [TODO ...]"
+          (car (command-line)))
+  (display "
+
+Usage:
+(main \"<ignored>\" \"-f\" \"arg0\")
+
+Options:
+  -h, --help        display this help and exit")
+  (newline))
+
+
+;; (use-modules (guix gexp))
+;; (define remote "http://example.org/foo.git")
+;; (define path "/tmp/")
+
+;; (let* ((port ((@ (guix build utils) open-pipe-with-stderr)
+;; 		          #$(file-append git "/bin/git") "clone" remote path)))
+;; 	(waitpid WAIT_ANY)
+;; 	(display ((@ (ice-9 rdelim) read-delimited) "" port))
+;; 	(close-port port))
+
+;; (use-modules (git)
+;;              ;; ((guix git) #:select (with-repository))
+
+;; ;;; provides `git-command' which is a dynamically bound variable created by
+;; ;;; make-parameter
+;; ;;; https://www.gnu.org/software/guile/manual/html_node/Parameters.html
+;;              (guix tests git)
+;;              (guix utils)
+;;              (guix build utils))
+
+;; (define directory (format #f "~a/rde-andrew-tropin" (getenv "dev")))
+
+;; (define (git command . args)
+;;   ;; Make sure Git doesn't rely on the user's config.
+;;   (call-with-temporary-directory
+;;    (lambda (home)
+;;      (call-with-output-file
+;;          ;; The behavior of call-with-output-file is unspecified if the file already exists.
+;;          (string-append home "/.gitconfig")
+;;        (lambda (port)
+;;          (display
+;;           (string-append "[user]\n"
+;;                          "  email = charlie@example.org\n"
+;;                          "  name = Charlie Guix\n")
+;;           port)))
+
+;;      (with-environment-variables
+;;          `(
+;;            ;; ("GIT_CONFIG_NOSYSTEM" "1")
+;;            ;; ("GIT_ATTR_NOSYSTEM" "1")
+;;            ("GIT_CONFIG_GLOBAL" ,(string-append home "/.gitconfig"))
+;;            ;; ("HOME" ,home)
+;;            )
+;;        (apply invoke (git-command) "-C" directory
+;;               command args)))))
+;; (git "config" "--global" "--get" "user.name") ;; => Charlie Guix
+;; (git "status" "--short" "--branch")
+
+;; https://gitlab.com/alezost-config/guile/-/blob/master/scripts/profile
 (define* (main #:rest args)
-  "Usage:
-(main \"<ignored>\" \"-f\" \"arg0\")"
-  ((compose
+  (
+   (compose
     (partial apply gcl)
     (partial apply cdr)
     #;dbg)
-   args))
+   #;
+   (compose
+    (lambda (cdr-args)
+      (match (cdr-args)
+        (((or "-h" "--help" "help"))
+         (show-help))
+
+        (#t (gcl cdr-args))
+        ))
+    (partial apply cdr)
+    #;dbg)
+   args)
+  )
