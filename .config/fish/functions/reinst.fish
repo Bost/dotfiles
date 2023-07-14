6d07e591a10b703be874b1480ee66969417824c1
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2023-07-14 16:07:51 +0200

    gxh: improve fish functions

--- .config/fish/functions/reinst.fish
+++ .config/fish/functions/reinst.fish
@@ -1,4 +1,4 @@
-function reinst
+function reinst --description "sudo apt --reinstall install --yes ..."
   set cmd sudo apt --reinstall install --yes (string escape -- $argv)
   echo $cmd
   eval $cmd
--- .config/fish/functions/xreboot.fish
+++ .config/fish/functions/xreboot.fish
@@ -1,4 +1,4 @@
-function reboot
+function xreboot --description "xfce4-session-logout --reboot --fast"
   set cmd xfce4-session-logout --reboot --fast
   echo $cmd
   eval $cmd
