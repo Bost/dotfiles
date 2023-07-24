fc17318c2b2a7f7ba6001a4624d7e2ad27e04f27
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2023-07-24 15:33:11 +0200

    gxs: kernel-version check shouldn't run under sudo

--- guix/systems/syst-geek.scm
+++ guix/systems/syst-geek.scm
@@ -26,7 +26,9 @@ sudo guix system --fallback -L $dotf/guix/systems reconfigure $dotf/guix/systems
   #:use-module (nongnu packages linux)
   #:use-module (nongnu system linux-initrd)
   #:use-module (guix)                  ; for package-version
-  )
+  #:export (
+            syst-config
+            ))
 
 ;; no need to write: #:use-module (gnu services <module>)
 (use-service-modules cups desktop networking ssh xorg)
@@ -39,133 +41,125 @@ sudo guix system --fallback -L $dotf/guix/systems reconfigure $dotf/guix/systems
  shells   ; login shell
  )
 
-(when (is-system-geek)
-  (let* []
-    (define m (module-name-for-logging))
-    ;; (format #t "~a evaluating module ...\n" m)
+(define m (module-name-for-logging))
+;; (format #t "~a evaluating module ...\n" m)
 
-    (define operating-system-configuration
-      (operating-system
-        (kernel linux)
-        (initrd microcode-initrd)
-        (firmware (list linux-firmware))
-        (locale "en_US.utf8")
-        (timezone "Europe/Berlin")
-        (keyboard-layout ; keyboard-layout for the console
-         (keyboard-layout
-          "us,de,sk,fr" "altgr-intl,,qwerty,"
-          #:options '("compose:menu,grp:ctrls_toggle")))
-        (host-name host-geek)
+(define syst-config
+  (operating-system
+    (kernel linux)
+    (initrd microcode-initrd)
+    (firmware (list linux-firmware))
+    (locale "en_US.utf8")
+    (timezone "Europe/Berlin")
+    (keyboard-layout ; keyboard-layout for the console
+     (keyboard-layout
+      "us,de,sk,fr" "altgr-intl,,qwerty,"
+      #:options '("compose:menu,grp:ctrls_toggle")))
+    (host-name host-geek)
 
-        ;; The list of user accounts ('root' is implicit).
-        (users (cons*
-                (user-account
-                 (name "bost")
-                 (comment
-                  (begin
-                    ;; (format #t "~a user-full-name: ~a\n" m user-full-name)
-                    user-full-name))
-                 (group "users")
-                 (home-directory "/home/bost")
+    ;; The list of user accounts ('root' is implicit).
+    (users (cons*
+            (user-account
+             (name "bost")
+             (comment
+              (begin
+                ;; (format #t "~a user-full-name: ~a\n" m user-full-name)
+                user-full-name))
+             (group "users")
+             (home-directory "/home/bost")
 
-                 ;; list of group names that this user-account belongs to
-                 (supplementary-groups
-                  ;; grant access to:
-                  '("wheel"  #| sudo etc.; See polkit-wheel-service for administrative tasks for non-root users |#
-                    "netdev" #| network devices |#
-                    "audio"  #| sound card |#
-                    "video"  #| video devices, e.g. webcams |#
-                    "lp"     #| control bluetooth devices |#
+             ;; list of group names that this user-account belongs to
+             (supplementary-groups
+              ;; grant access to:
+              '("wheel"  #| sudo etc.; See polkit-wheel-service for administrative tasks for non-root users |#
+                "netdev" #| network devices |#
+                "audio"  #| sound card |#
+                "video"  #| video devices, e.g. webcams |#
+                "lp"     #| control bluetooth devices |#
 
-                    ;; "kvm"
-                    ;; "tty"
-                    ;; "input"
-                    ;; "docker"
-                    ;; "realtime"  #| Enable realtime scheduling |#
-                    )))
-                %base-user-accounts))
+                ;; "kvm"
+                ;; "tty"
+                ;; "input"
+                ;; "docker"
+                ;; "realtime"  #| Enable realtime scheduling |#
+                )))
+            %base-user-accounts))
 
 ;;; Packages installed system-wide. Users can also install packages under their
 ;;; own account: use 'guix search KEYWORD' to search for packages and 'guix
 ;;; install PACKAGE' to install a package.
-        (packages
-         (append
-          (map specification->package
-               (list
-                "brightnessctl" #| backlight and LED brightness control |#
-                "git"
-                ;; "gparted"    #| disk partition |#
-                "nss-certs"     #| HTTPS access |#
-                ;; "rsync"      #| 'scp' is preinstalled |#
-                ;; "vim"        #| 'vi' is preinstalled |#
-                ))
-          %base-packages))
+    (packages
+     (append
+      (map specification->package
+           (list
+            "brightnessctl" #| backlight and LED brightness control |#
+            "git"
+            ;; "gparted"    #| disk partition |#
+            "nss-certs"     #| HTTPS access |#
+            ;; "rsync"      #| 'scp' is preinstalled |#
+            ;; "vim"        #| 'vi' is preinstalled |#
+            ))
+      %base-packages))
 
 ;;; Below is the list of system services. To search for available services, run
 ;;; 'guix system search KEYWORD' in a terminal.
-        (services
-         (append (list
-                  (service xfce-desktop-service-type)
+    (services
+     (append (list
+              (service xfce-desktop-service-type)
 
-                  ;; ntp-service-type for system clock sync is in the
-                  ;; %desktop-services by default
+              ;; ntp-service-type for system clock sync is in the
+              ;; %desktop-services by default
 
-                  ;; To configure OpenSSH, pass an 'openssh-configuration'
-                  ;; record as a second argument to 'service' below.
-                  (service openssh-service-type)
+              ;; To configure OpenSSH, pass an 'openssh-configuration'
+              ;; record as a second argument to 'service' below.
+              (service openssh-service-type)
 
-                  (set-xorg-configuration
-                   (xorg-configuration (keyboard-layout keyboard-layout)))
+              (set-xorg-configuration
+               (xorg-configuration (keyboard-layout keyboard-layout)))
 
-                  (udev-rules-service 'mtp libmtp) ;; mtp - Media Transfer Protocol
-                  (udev-rules-service 'android android-udev-rules
-                                      #:groups '("adbusers")))
+              (udev-rules-service 'mtp libmtp) ;; mtp - Media Transfer Protocol
+              (udev-rules-service 'android android-udev-rules
+                                  #:groups '("adbusers")))
 
-                 ;; This is the default list of services we are appending to.
-                 (modify-services %desktop-services
-                   (guix-service-type
-                    config => (guix-configuration
-                               (inherit config)
-                               (substitute-urls
-                                (append (list "https://substitutes.nonguix.org")
-                                        %default-substitute-urls))
-                               (authorized-keys
+             ;; This is the default list of services we are appending to.
+             (modify-services %desktop-services
+               (guix-service-type
+                config => (guix-configuration
+                           (inherit config)
+                           (substitute-urls
+                            (append (list "https://substitutes.nonguix.org")
+                                    %default-substitute-urls))
+                           (authorized-keys
 ;;; The signing-key.pub should be obtained by
 ;;;   wget https://substitutes.nonguix.org/signing-key.pub
-                                (append (list (local-file "./signing-key.pub"))
-                                        %default-authorized-guix-keys)))))))
-        (bootloader
-         (bootloader-configuration
-          (bootloader grub-efi-bootloader)
-          (targets (list "/boot/efi"))
-          (keyboard-layout keyboard-layout)))
-        (mapped-devices (list (mapped-device
-                               (source (uuid
-                                        "fe8beac5-240d-4850-b67c-347b8cf4dc7e"))
-                               (target "encrypted")
-                               (type luks-device-mapping))))
+                            (append (list (local-file "./signing-key.pub"))
+                                    %default-authorized-guix-keys)))))))
+    (bootloader
+     (bootloader-configuration
+      (bootloader grub-efi-bootloader)
+      (targets (list "/boot/efi"))
+      (keyboard-layout keyboard-layout)))
+    (mapped-devices (list (mapped-device
+                           (source (uuid
+                                    "fe8beac5-240d-4850-b67c-347b8cf4dc7e"))
+                           (target "encrypted")
+                           (type luks-device-mapping))))
 
-        ;; The list of file systems that get "mounted".  The unique
-        ;; file system identifiers there ("UUIDs") can be obtained
-        ;; by running 'blkid' in a terminal.
-        (file-systems (cons* (file-system
-                               (mount-point "/")
-                               (device "/dev/mapper/encrypted")
-                               (type "ext4")
-                               (dependencies mapped-devices))
-                             (file-system
-                               (mount-point "/boot/efi")
-                               (device (uuid "FC73-7111"
-                                             'fat32))
-                               (type "vfat")) %base-file-systems))))
+    ;; The list of file systems that get "mounted".  The unique
+    ;; file system identifiers there ("UUIDs") can be obtained
+    ;; by running 'blkid' in a terminal.
+    (file-systems (cons* (file-system
+                           (mount-point "/")
+                           (device "/dev/mapper/encrypted")
+                           (type "ext4")
+                           (dependencies mapped-devices))
+                         (file-system
+                           (mount-point "/boot/efi")
+                           (device (uuid "FC73-7111"
+                                         'fat32))
+                           (type "vfat")) %base-file-systems))))
 
-    ((compose
-      (partial format #t "~a kernel-version: ~a\n" m)
-      package-version
-      operating-system-kernel)
-     operating-system-configuration)
+;; (format #t "~a module evaluated\n" m)
 
-    ;; (format #t "~a module evaluated\n" m)
-
-    ;; operating-system (or image) must be returned
-    operating-system-configuration))
+;; operating-system (or image) must be returned
+syst-config
