91028671e0c1463719a9c3d5937fbe44c28f95b3
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2023-10-25 00:45:43 +0200

    gxh: add gpsf.scm git push --force

---
+++ guix/home/common/scm-bin/gpsf.scm
@@ -0,0 +1,50 @@
+(define-module (scm-bin gpsf)
+;;; All used modules must be present in the module (srvc scheme-files) under:
+;;;   service-file -> with-imported-modules
+  #:use-module (ice-9 rdelim)
+  #:use-module (ice-9 regex)             #| string-match |#
+  #:use-module (ice-9 popen)
+  #:use-module (utils)
+  #:export (main))
+
+#|
+
+#!/usr/bin/env -S guile \\
+-L ./ -e (gpsf) -s
+!#
+
+|#
+
+(define (main args)
+  ((compose
+    (partial
+     map
+     (compose
+      cdr
+      exec
+      (lambda (remote)
+        (append
+         (list "git" "push" "--force" "--follow-tags"
+               ;; "--verbose"
+               remote)
+         (cdr args)))
+      car))
+    (partial filter (lambda (remote-url)
+                      (not (null? (cdr remote-url)))))
+    (partial map
+             (lambda (remote)
+               (cons remote
+                     ((compose
+                       (partial filter
+                                (lambda (url)
+                                  (string-match "git@" url)))
+                       cdr
+                       exec
+                       (partial list "git" "remote" "get-url"))
+                      remote))))
+    (partial filter (lambda (remote)
+                      (not (string-match "heroku" remote))))
+    cdr
+    exec)
+   (list
+    "git" "remote")))
