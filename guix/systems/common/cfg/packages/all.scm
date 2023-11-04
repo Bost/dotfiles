b1cd063ed1ef21464c9e59b114a854869d3c2c44
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2023-11-04 17:06:44 +0100

    gxs: install gnupg / gpg system-wide

--- guix/systems/common/cfg/packages/all.scm
+++ guix/systems/common/cfg/packages/all.scm
@@ -7,6 +7,7 @@
   (list
 ;;; Install git & rsync system-wide to be able to git-clone / rsync the dotfiles
    "git"
+   "gnupg"      #| gpg |#
    "gparted"    #| disk partition |#
    "mtools"     #| used by gparted |#
    "nss-certs"  #| HTTPS access |#
