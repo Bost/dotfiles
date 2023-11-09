7edeae8a47e1df746f09011e874581f66e985d68
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2023-11-09 16:17:29 +0100

    gx: use define-public instead of export 4.

--- guix/common/memo.scm
+++ guix/common/memo.scm
@@ -15,14 +15,7 @@
   #:use-module (srfi srfi-1)
   ;; #:use-module (guix build utils) ;; invoke - not needed
   #:use-module (ice-9 pretty-print)
-  #:export (
-            hostname-memoized
-
-            is-system-lukas
-            is-system-ecke
-            is-system-edge
-            is-system-geek
-            ))
+)
 
 ;;; Use function-implementations so that the code below is not evaluated every
 ;;; time some of the scm-bin CLI utility requiring this module is executed.
@@ -37,16 +30,16 @@
           *unspecified*))))
 ;; (format #t "[utils] hostname: ~a\n" hostname)
 
-(define hostname-memoized (memoize hostname))
+(define-public hostname-memoized (memoize hostname))
 
-(define (is-system-lukas) (equal? (hostname-memoized) host-lukas))
+(define-public (is-system-lukas) (equal? (hostname-memoized) host-lukas))
 (testsymb 'is-system-lukas)
 
-(define (is-system-ecke) (equal? (hostname-memoized) host-ecke))
+(define-public (is-system-ecke) (equal? (hostname-memoized) host-ecke))
 (testsymb 'is-system-ecke)
 
-(define (is-system-edge) (equal? (hostname-memoized) host-edge))
+(define-public (is-system-edge) (equal? (hostname-memoized) host-edge))
 (testsymb 'is-system-edge)
 
-(define (is-system-geek) (equal? (hostname-memoized) host-geek))
+(define-public (is-system-geek) (equal? (hostname-memoized) host-geek))
 (testsymb 'is-system-geek)
