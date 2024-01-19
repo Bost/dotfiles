9993130bd0ad70dab6b8361da5d62308ecb1f528
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2024-01-19 14:08:32 +0100

    gxh: fish: fix descrition -> description

--- .config/fish/functions/gri.fish
+++ .config/fish/functions/gri.fish
@@ -1,4 +1,4 @@
-function gri --descrition "git rebase --interactive"
+function gri --description "git rebase --interactive"
   set cmd git rebase --interactive (string escape -- $argv)
   echo $cmd
   eval $cmd
