35e956e33e0f34beadb87ff43f2978e0e43d5711
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2024-02-08 01:00:28 +0100

    gxh: rewrite susp in guile

---
+++ guix/home/common/scm-bin/susp.scm
@@ -0,0 +1,36 @@
+(define-module (scm-bin susp)
+;;; All used modules must be present in the module (srvc scheme-files) under:
+;;;   service-file -> with-imported-modules
+  #:use-module (utils)
+  #:export (main susp))
+
+#|
+
+#!/usr/bin/env -S guile \\
+-L ./guix/common -L ./guix/home/common -e (scm-bin\ susp) -s
+!#
+
+cd $dotf
+./guix/home/common/scm-bin/susp.scm
+
+|#
+
+(evaluating-module)
+
+(define* (susp #:rest args)
+  "Usage: "
+  (apply exec-system* (append
+                       (list "xfce4-session-logout" "--suspend")
+                       args)))
+(testsymb 'susp)
+
+(define* (main #:rest args)
+  "Usage: "
+  ((compose
+    (partial apply susp)
+    (partial apply cdr)
+    #;dbg)
+   args))
+(testsymb 'main)
+
+(module-evaluated)
