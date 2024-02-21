7b4a649fac5a25bdd04096f02fd61d194c0262c9
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2024-02-21 12:48:43 +0100

    gxh: add crafted-launcher for crafted-emacs

---
+++ guix/home/common/scm-bin/crafted-launcher.scm
@@ -0,0 +1,34 @@
+(define-module (scm-bin crafted-launcher)
+;;; All used modules must be present in the module (srvc scheme-files) under:
+;;;   service-file -> with-imported-modules
+  #|
+  #:use-module (gnu packages emacs) ; for emacs-output-path
+  #:use-module (guix)               ; for open-connection
+  ;; Activating the two lines above leads to:
+  ;;   In procedure resolve-interface: no code for module (gnu packages emacs)
+  ;;   In procedure resolve-interface: no code for module (guix)
+  |#
+
+  #:use-module (utils) ;; partial
+  #:use-module (scm-bin emacs-launcher)
+  #:export (main))
+
+#|
+
+#!/usr/bin/env -S guile \\
+-L ./guix/common -L ./guix/home/common -e (scm-bin\ crafted-launcher) -s
+!#
+
+cd $dotf
+./guix/home/common/scm-bin/crafted-launcher.scm rest args
+./guix/home/common/scm-bin/crafted-launcher.scm --profile=my-profile rest args
+
+|#
+
+(evaluating-module)
+
+(define (main args)
+  (apply (partial emacs-launcher #:profile "crafted")
+         (cdr args)))
+
+(module-evaluated)
