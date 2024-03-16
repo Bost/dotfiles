0e8ae235e50581afb4d3d54ce385e5398c50adc7
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2024-03-16 22:50:59 +0100

    gxh: cleanup

--- guix/home/common/srvc/home-dir-cfg.scm
+++ guix/home/common/srvc/home-dir-cfg.scm
@@ -76,152 +76,154 @@
 See also (@(fs-utils) local-dotfile)
 "
   (let [(hn (hostname-memoized))
-        (files (cond
-                [(is-system-ecke)
-                 (list
-                  "panel/launcher-17/17100751821.desktop"
-                  "panel/launcher-18/17100751822.desktop"
-                  "panel/launcher-19/17100751823.desktop"
-                  "panel/launcher-20/17100751824.desktop"
-                  "panel/xfce4-clipman-actions.xml"
-                  "xfconf/xfce-perchannel-xml/displays.xml"
-                  "xfconf/xfce-perchannel-xml/keyboards.xml"
-                  "xfconf/xfce-perchannel-xml/thunar.xml"
-                  "xfconf/xfce-perchannel-xml/xfce4-appfinder.xml"
-                  "xfconf/xfce-perchannel-xml/xfce4-desktop.xml"
-                  "xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml"
-                  "xfconf/xfce-perchannel-xml/xfce4-notifyd.xml"
-                  "xfconf/xfce-perchannel-xml/xfce4-panel.xml"
-                  "xfconf/xfce-perchannel-xml/xfce4-power-manager.xml"
-                  "xfconf/xfce-perchannel-xml/xfce4-screensaver.xml"
-                  "xfconf/xfce-perchannel-xml/xfce4-session.xml"
-                  "xfconf/xfce-perchannel-xml/xfce4-terminal.xml"
-                  "xfconf/xfce-perchannel-xml/xfwm4.xml"
-                  )]
-                [(is-system-edge)
-                 (list
-                  "xfce4-screenshooter"
-                  "xfconf/xfce-perchannel-xml/displays.xml"
-                  "xfconf/xfce-perchannel-xml/keyboards.xml"
-                  "xfconf/xfce-perchannel-xml/thunar.xml"
-                  "xfconf/xfce-perchannel-xml/xfce4-desktop.xml"
-                  "xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml"
-                  "xfconf/xfce-perchannel-xml/xfce4-notifyd.xml"
-                  "xfconf/xfce-perchannel-xml/xfce4-panel.xml"
-                  "xfconf/xfce-perchannel-xml/xfce4-power-manager.xml"
-                  "xfconf/xfce-perchannel-xml/xfce4-screensaver.xml"
-                  "xfconf/xfce-perchannel-xml/xfce4-session.xml"
-                  "xfconf/xfce-perchannel-xml/xfce4-terminal.xml"
-                  "xfconf/xfce-perchannel-xml/xfwm4.xml"
-                  )]
-                [#t (list)]))]
+        (path "/")]
     ((comp
       (partial remove unspecified-or-empty-or-false?)
       (partial
        map
        (lambda (config-file)
          (let* [(destination (str ".config/xfce4/" config-file))
-                (path "/")
                 (filename (str ".config/xfce4." hn "/" config-file))
                 (filepath (user-dotf path filename))]
            (if (access? filepath R_OK)
-               (list destination
-                     (let [(base-filename (basename filename))]
-                       ;; 'local-file' is a macro and cannot be used by 'apply'
-                       (if (equal? "." (substring base-filename 0 1))
-                           ;; base-filename of the local-file can't start with '.'
-                           ;; use the `(...) forms for debugging
-                           ;; `(local-file ,filepath ,(fix-leading-dot base-filename))
-                           ;; `(local-file ,filepath)
-                           (local-file filepath (fix-leading-dot base-filename))
-                           (local-file filepath))))
+               (list
+                destination
+                (let [(base-filename (basename filename))]
+                  ;; 'local-file' is a macro and cannot be used by 'apply'
+                  (if (equal? "." (substring base-filename 0 1))
+                      ;; base-filename of the local-file can't start with '.'
+                      ;; use the `(...) forms for debugging
+                      ;;`(local-file ,filepath ,(fix-leading-dot base-filename))
+                      ;;`(local-file ,filepath)
+                      (local-file filepath (fix-leading-dot base-filename))
+                      (local-file filepath))))
                (begin
                  (format #t "E ~a Can't read ~a\n" m filepath)
                  #f)))))
-      #;(lambda (lst) (take lst 2)))
-     files)))
+      #;(lambda (lst) (take lst 2))
+      (partial remove unspecified-or-empty-or-false?)
+      (partial append
+               (when (or (is-system-edge) (is-system-ecke))
+                 (list
+                  "xfconf/xfce-perchannel-xml/displays.xml"
+                  "xfconf/xfce-perchannel-xml/keyboards.xml"
+                  "xfconf/xfce-perchannel-xml/thunar.xml"
+                  "xfconf/xfce-perchannel-xml/xfce4-desktop.xml"
+                  "xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml"
+                  "xfconf/xfce-perchannel-xml/xfce4-notifyd.xml"
+                  "xfconf/xfce-perchannel-xml/xfce4-panel.xml"
+                  "xfconf/xfce-perchannel-xml/xfce4-power-manager.xml"
+                  "xfconf/xfce-perchannel-xml/xfce4-screensaver.xml"
+                  "xfconf/xfce-perchannel-xml/xfce4-session.xml"
+                  "xfconf/xfce-perchannel-xml/xfce4-terminal.xml"
+                  "xfconf/xfce-perchannel-xml/xfwm4.xml"))))
+     (cond
+      [(is-system-ecke)
+       (list
+        "panel/launcher-17/17100751821.desktop"
+        "panel/launcher-18/17100751822.desktop"
+        "panel/launcher-19/17100751823.desktop"
+        "panel/launcher-20/17100751824.desktop"
+        "panel/xfce4-clipman-actions.xml"
+        "xfconf/xfce-perchannel-xml/xfce4-appfinder.xml")]
+      [(is-system-edge)
+       (list
+        "xfce4-screenshooter"
+        )]
+      [#t (list)]))))
 
 (define (home-dir-cfg-srvc-files)
   ((comp
     ;; (lambda (p) (format #t "######## 3.p:\n~a\n" (pretty-print->string p)) p)
-    (partial append
-             ((comp
-               ;; (partial append (list (local-dotfile
-               ;;                        "/"
-               ;;                        (str (basename xdg-config-home)
-               ;;                             "/guix-gaming-channels/games.scm"))))
-               list create-channels-scm
-               (lambda (lst)
-                 (if (or (is-system-ecke) (is-system-edge))
-                     (append
-                      lst
-                      `(
-                        ;; Dynamic tiling Wayland compositor configurable in Guile Scheme
-                        ;; (channel (name 'home-service-dwl-guile)
-                        ;;          (url "https://github.com/engstrand-config/home-service-dwl-guile")
-                        ;;          (branch "main")
-                        ;;          (introduction
-                        ;;           (make-channel-introduction
-                        ;;            "314453a87634d67e914cfdf51d357638902dd9fe"
-                        ;;            (openpgp-fingerprint
-                        ;;             "C9BE B8A0 4458 FDDF 1268 1B39 029D 8EB7 7E18 D68C"))))
+    (partial
+     append
+     ((comp
+       ;; (partial append (list (local-dotfile
+       ;;                        "/"
+       ;;                        (str (basename xdg-config-home)
+       ;;                             "/guix-gaming-channels/games.scm"))))
+       list create-channels-scm
+       (partial remove unspecified-or-empty-or-false?)
+       (lambda (lst)
+         (append
+          lst
+          (when (or (is-system-ecke) (is-system-edge))
+            `(
+;;; Dynamic tiling Wayland compositor configurable in Guile Scheme
+              #;
+              (channel
+               (name 'home-service-dwl-guile)
+               (url
+                "https://github.com/engstrand-config/home-service-dwl-guile")
+               (branch "main")
+               (introduction
+                (make-channel-introduction
+                 "314453a87634d67e914cfdf51d357638902dd9fe"
+                 (openpgp-fingerprint
+                  "C9BE B8A0 4458 FDDF 1268 1B39 029D 8EB7 7E18 D68C"))))
 
-                        (channel (name 'flat)
-                                 (url "https://github.com/flatwhatson/guix-channel.git")
-                                 (introduction
-                                  (make-channel-introduction
-                                   "33f86a4b48205c0dc19d7c036c85393f0766f806"
-                                   (openpgp-fingerprint
-                                    "736A C00E 1254 378B A982  7AF6 9DBE 8265 81B6 4490"))))
+;;; flatwhatson contains emacs-native-comp, however it doesn't compile
+              #;
+              (channel
+               (name 'flat)
+               (url "https://github.com/flatwhatson/guix-channel.git")
+               (introduction
+                (make-channel-introduction
+                 "33f86a4b48205c0dc19d7c036c85393f0766f806"
+                 (openpgp-fingerprint
+                  "736A C00E 1254 378B A982  7AF6 9DBE 8265 81B6 4490"))))
 
-                        (channel (name 'haskell-and-clojure)
-                                 (url
-                                  #;"https://github.com/Tass0sm/guix-develop/tassos-guix"
-                                  "https://github.com/Bost/haskell-guix"
-                                  #;,(format #f "file://~a/dev/haskell-guix" home)))
+              (channel
+               (name 'haskell-and-clojure)
+               (url
+                #;"https://github.com/Tass0sm/guix-develop"
+                "https://github.com/Bost/haskell-guix"
+                #;,(format #f "file://~a/dev/haskell-guix" home)))
 
 ;;; Andrew Tropin's tools for managing reproducible development environments
-                        #;
-                        (channel (name 'rde)
-                                 (url
-                                  "https://git.sr.ht/~abcdw/rde"
-                                  #;,(format #f "file://~a/dev/andrew-rde" home))
-                                 (introduction
-                                  (make-channel-introduction
-                                   "257cebd587b66e4d865b3537a9a88cccd7107c95"
-                                   (openpgp-fingerprint
-                                    "2841 9AC6 5038 7440 C7E9  2FFA 2208 D209 58C1 DEB0"))))
-                        #|
+              #;
+              (channel
+               (name 'rde)
+               (url
+                "https://git.sr.ht/~abcdw/rde"
+                #;,(format #f "file://~a/dev/andrew-rde" home))
+               (introduction
+                (make-channel-introduction
+                 "257cebd587b66e4d865b3537a9a88cccd7107c95"
+                 (openpgp-fingerprint
+                  "2841 9AC6 5038 7440 C7E9  2FFA 2208 D209 58C1 DEB0"))))
+
 ;;; https://raw.githubusercontent.com/wube/factorio-data/master/changelog.txt
 ;;; Use:
 ;;;     guix package --load-path=./ --install=factorio
 ;;; '--keep-failed' doesn't keep the binary in the /gnu/store when the sha256 is
 ;;; wrong
-                        ;; requires the guix-gaming-channels/games.scm - see above
-                        (channel (name 'games)
-                                 (url
-                                  #;"https://gitlab.com/rostislav.svoboda/games"
-                                  #;,(format #f "file://%s/dev/games" home)
-                                  "https://gitlab.com/guix-gaming-channels/games.git")
-                                 ;; Enable signature verification:
-                                 (introduction
-                                  (make-channel-introduction
-                                   ;; The 1st commit from this repository which can be trusted is:
-                                   "c23d64f1b8cc086659f8781b27ab6c7314c5cca5"
-                                   (openpgp-fingerprint
-                                    ;; ... as it was made by some with OpenPGP fingerprint:
-                                    "50F3 3E2E 5B0C 3D90 0424  ABE8 9BDC F497 A4BB CC7F"))))
-                        |#
-                        ))
-                     lst)))
-              `(
-                ;; provides:
-                ;; - (bost gnu packages emacs-xyz) module
-                ;; - clojure, babashka, postgres 13.3, openjdk18 etc.
-                (channel (name 'bost)
-                         (url
-                          "https://github.com/Bost/guix-packages"
-                          #;,(format #f "file://~a/dev/guix-packages" home))))))
+;;;
+;;; The games channel requires the guix-gaming-channels/games.scm - see above
+              #;
+              (channel
+               (name 'games)
+               (url
+                #;"https://gitlab.com/rostislav.svoboda/games"
+                #;,(format #f "file://%s/dev/games" home)
+                "https://gitlab.com/guix-gaming-channels/games.git")
+               ;; Enable signature verification:
+               (introduction
+                (make-channel-introduction
+                 ;; 1st commit from this repository which can be trusted is:
+                 "c23d64f1b8cc086659f8781b27ab6c7314c5cca5"
+                 (openpgp-fingerprint
+                  ;; ... as it was made by some with OpenPGP fingerprint:
+                  "50F3 3E2E 5B0C 3D90 0424  ABE8 9BDC F497 A4BB CC7F"))))))
+          lst)))
+      `(
+        ;; provides:
+        ;; - (bost gnu packages emacs-xyz) module
+        ;; - clojure, babashka, postgres 13.3, openjdk18 etc.
+        (channel (name 'bost)
+                 (url
+                  "https://github.com/Bost/guix-packages"
+                  #;,(format #f "file://~a/dev/guix-packages" home))))))
     ;; (lambda (p) (format #t "######## 3.\n~a\n" p) p)
     (partial append (host-specific-config))
     ;; (lambda (p) (format #t "######## 2.\n~a\n" p) p)
@@ -230,12 +232,13 @@ See also (@(fs-utils) local-dotfile)
                (partial remove unspecified-or-empty-or-false?)
                (partial map (partial local-dotfile "/")))
               (list
-               #|
-;;; TODO make sure that .gnupg and its content have the right ownership and permissions:
-chown -R $(whoami) ~/.gnupg
-find ~/.gnupg -type f -exec chmod u=rwx,g=---,o=--- {} \; # i.e. 600 for files
-find ~/.gnupg -type d -exec chmod u=rwx,g=---,o=--- {} \; # i.e. 700 for directories
-               |#
+;;; TODO make sure that .gnupg and its content have the right ownership and
+;;; permissions:
+;;; chown -R $(whoami) ~/.gnupg
+;;; # i.e. 600 for files:
+;;; find ~/.gnupg -type f -exec chmod u=rwx,g=---,o=--- {} \;
+;;; # i.e. 700 for directories:
+;;; find ~/.gnupg -type d -exec chmod u=rwx,g=---,o=--- {} \;
                ".config/sway/config"
                ".config/tmux/tmux.conf"
 
