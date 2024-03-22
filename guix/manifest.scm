834206dfaca85881fe78a21c2ec32302e95017c4
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2024-03-22 17:35:11 +0100

    gxh: add gxpr: populate default profile
    The manifest.scm is located outside of load-path(s) so that it's recomputed only
    when needed

--- guix/manifest.scm
+++ guix/manifest.scm
@@ -1,11 +1,15 @@
 (define-module (manifest)
- #:use-module (guix profiles)
- #:use-module (cfg packages all))
+  #:use-module (bost utils)
+  #:use-module (guix profiles)
+  #:use-module (cfg packages all))
 
 (define (partial fun . args)
   (lambda x (apply fun (append args x))))
 
 (define-public (manifest-content)
+"Leads to creation of a 540K large file:
+$ ls --human-readable --size /home/bost/.guix-profile/manifest
+540K /home/bost/.guix-profile/manifest"
   ((compose
     manifest
     (partial map
@@ -16,14 +20,7 @@
     #;(lambda (p) (format #t "~a\n" ((@(ice-9 pretty-print) pretty-print) p)) p)
     #;(lambda (p) ((@(srfi srfi-1) take) p 2)))
    (packages-to-install)))
-
-#|
-guix package \
-  -L /home/bost/dev/dotfiles/guix/common \
-  -L /home/bost/dev/dotfiles/guix/home/common \
-  --profile=/home/bost/.guix-profile \
-  --manifest=/home/bost/dev/dotfiles/guix/home/common/manifest.scm
+(testsymb 'manifest-content)
 
 (manifest-content)
-|#
 
