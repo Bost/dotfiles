d59477bbe2bd4dec86f036c1dd20bcfc56b4043b
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2024-03-24 18:34:44 +0100

    gxh: make the lock command available the default shell

--- .config/fish/functions/lock.fish
+++
@@ -1,8 +0,0 @@
-function lock
-  # TODO check if xfce4-screensaver is installed. Otherwise
-  # sudo apt install xfce4-screensaver
-  # gnome-screensaver-command -l
-  set cmd xflock4
-  echo $cmd
-  eval $cmd
-end
