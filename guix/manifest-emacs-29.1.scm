cffddde3d9c7dfa429cde2fdd1ff14b44e2dd0cc
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2024-04-09 15:14:27 +0200

    gxh: [wip] $GUIX_EXTRA_PROFILES/emacs-29.1
    1. Can't use modules:
    (guix packages)
    (guix inferior)
    (guix channels)
    (guix profiles)
    
    2. Can't be implement inferior-package-in-guix-channel in the guix/common/utils.scm

--- guix/manifest-emacs-29.1.scm
+++ guix/manifest-emacs-29.1.scm
@@ -1,7 +1,30 @@
-(define-module (manifest)
+#|
+# Create profile: run in /bin/sh
+mkdir $GUIX_EXTRA_PROFILES
+source /etc/profile
+guix package --cores=24 \
+    -L /home/bost/dev/dotfiles/guix/common \
+    -L /home/bost/dev/dotfiles/guix/home/common \
+    --profile=$GUIX_EXTRA_PROFILES/emacs-29.1 \
+    --manifest=$dotf/guix/manifest-emacs-29.1.scm
+
+# Activate the profile:
+GUIX_PROFILE="/home/bost/.guix-extra-profiles/emacs-29.1"
+. "$GUIX_PROFILE/etc/profile"
+|#
+(define-module (manifest-emacs-29.1)
+  #:use-module (srfi srfi-1) ;; remove
+  #:use-module (guix packages)
   #:use-module (utils) ;; partial
   #:use-module (guix profiles)
   #:use-module (cfg packages all) ;; packages-to-install
+
+  ;; for inferior-package-in-guix-channel : beg
+  #:use-module (guix packages)
+  #:use-module (guix inferior)
+  #:use-module (guix channels)
+  ;; #:use-module (guix profiles) ;; probably not needed
+  ;; for inferior-package-in-guix-channel : end
   )
 
 (define m (module-name-for-logging))
@@ -70,16 +93,55 @@
    [else
     (my-package->manifest-entry package)]))
 
+(define-public (inferior-package-in-guix-channel package commit)
+  "Returns an inferior representing the `commit' (predecessor-sha1) revision.
+Can't be in the guix/common/utils.scm. Therefore duplicated.
+See guix/manifest-emacs-29.1.scm, guix/home/common/cfg/packages/all.scm"
+  (first
+   (lookup-inferior-packages
+    (inferior-for-channels
+     (list (channel
+            (name 'guix)
+            (url "https://git.savannah.gnu.org/git/guix.git")
+            (commit commit))))
+    package)))
+(testsymb 'inferior-package-in-guix-channel)
+
+(define (inferior-pkgs pkgs)
+  "The original, i.e. non-inferior packages must not be present in the PKGS."
+  ((comp
+    (partial append pkgs)
+    ;; (lambda (pkgs) (format #t "~a\ninferior-pkgs: ~a\n" m pkgs) pkgs)
+    (partial map (partial apply inferior-package-in-guix-channel)))
+   (list
+    ;; emacs 29.1
+    (list "emacs" "ff1ec930e5baa00b483f1ce43fa8bec18c797c03")
+    ;; emacs 28.2
+    ;; (list "emacs" "772eaa69f31457aa19ca4dc4ce755c791d722054")
+    )))
+
 (define-public (manifest-content)
   "Leads to creation of a rather large file:
-$ ls --human-readable --size /home/bost/.guix-profile/manifest
-540K /home/bost/.guix-profile/manifest"
+ls --human-readable --size $GUIX_EXTRA_PROFILES/emacs-29.1/manifest
+564K /home/bost/.guix-extra-profiles/emacs-29.1/manifest"
   ((compose
     manifest
     (partial map to-manifest-entry)
     #;(lambda (p) (format #t "~a\n" ((@(ice-9 pretty-print) pretty-print) p)) p)
     #;(lambda (p) ((@(srfi srfi-1) take) p 2))
-    (partial dbg-packages-to-install #:search-space '() #:pkgs))
+    (partial dbg-packages-to-install #:search-space '("emacs") #:pkgs)
+    inferior-pkgs
+    (partial dbg-packages-to-install #:search-space '("emacs") #:pkgs)
+    (partial remove (lambda (p)
+                      ;; (and (package? p) (equal? (package-name p) "emacs"))
+                      (if (package? p)
+                        (if (equal? (package-name p) "emacs")
+                            (begin
+                              (format #t "D ~a Removing ~a\n" m p)
+                              #t)
+                            #f)
+                        #f)))
+    (partial dbg-packages-to-install #:search-space '("emacs") #:pkgs))
    (packages-to-install)))
 (testsymb 'manifest-content)
 
