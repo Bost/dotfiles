775c1afd411bb19bf15a2ba6a140b97c666ebf1e
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2024-06-04 20:10:14 +0200

    gxs: adjust packages installed system-wide

--- guix/systems/common/cfg/packages/all.scm
+++ guix/systems/common/cfg/packages/all.scm
@@ -11,6 +11,9 @@
  mtools          ;; used by gparted
  rsync
  vim
+ wget            ;; wget
+ linux           ;; iptables (IP packet filtering rules)
+ ssh             ;; openssh
  )
 
 (define m (module-name-for-logging))
@@ -19,7 +22,18 @@
 (define-public (packages-to-install)
   (list
 ;;; Install git & rsync system-wide to be able to git-clone / rsync the dotfiles
-   git
+   ;; From the comment in gnu/packages/version-control.scm
+   ;; The size of the closure of 'git-minimal' is two thirds that of 'git'.
+   ;; Its test suite runs slightly faster and most importantly it doesn't
+   ;; depend on packages that are expensive to build such as Subversion.
+   git-minimal
+   ;; git
+
+   wget
+   iptables   ;; Programs to configure Linux IP packet filtering rules
+   openssh
+   strace
+
    gnupg
    gparted    ;; disk partition
    mtools     ;; used by gparted
