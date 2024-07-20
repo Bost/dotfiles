f10f03d321fac44c6d992518140f6dda079039eb
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2024-07-20 17:01:08 +0200

    gxh: emacs launcher cleanup: parameter '--version'

--- guix/home/common/emacs-editable-config.scm
+++ guix/home/common/emacs-editable-config.scm
@@ -43,7 +43,6 @@ Usage:
         (copy-file src dst)))))
 (testsymb 'set-config-editable)
 
-;; TODO --version is not needed
 (define* (handle-cli #:key utility-name fun #:rest args)
   "All the options, except rest-args, must be specified for the option-spec so that
 the options-parser doesn't complain about e.g. 'no such option: -p'."
@@ -56,24 +55,24 @@ the options-parser doesn't complain about e.g. 'no such option: -p'."
     (let* [(option-spec
             '[
               (help       (single-char #\h))
-              (version    (single-char #\v))
+              #;(version    (single-char #\v))
               ])]
       ;; TODO isn't the #:stop-at-first-non-option swapped?
       (let* [(options (getopt-long args option-spec #:stop-at-first-non-option #t))
              ;; #f means that the expected value wasn't specified
              (val-help       (option-ref options 'help    #f))
-             (val-version    (option-ref options 'version #f))]
+             #;(val-version    (option-ref options 'version #f))]
         (when dbg
           (format #t "~a option-spec   : ~a\n" m option-spec)
           (format #t "~a val-help      : ~a\n" m val-help)
-          (format #t "~a val-version   : ~a\n" m val-version))
+          #;(format #t "~a val-version   : ~a\n" m val-version))
         (cond
          [val-help
-          (format #t "~a [options]
-    -v, --version    Display version
-    -h, --help       Display this help
-" utility-name)]
-         [val-version
+          (format #t "~a [options] ~a\n~a\n\n"
+                  utility-name
+                  #;"    -v, --version    Display version"
+                  "    -h, --help       Display this help")]
+         #;[val-version
           (format #t "~a version 1.23
 " utility-name)]
          [#t
