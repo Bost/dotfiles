3559eb5929bc70198502cb55aeae91ba0b3477cd
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2024-07-30 14:10:59 +0200

    python: update common.py
    Code saved code from bost@martin:/home/bost.inactive/dev/do_exec.py

--- python/common.py
+++ python/common.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python
 
 from subprocess import Popen, PIPE
+import operator
 import os
 import re
+import datetime
+import time
+from functools import reduce
 from pathlib import Path
+
+def map_f_reduce_add(f, xs):
+    return reduce(operator.add, map(f, xs), []) # only map and reduce!
+
+def tstp():
+    """ See also:
+    import logging
+    logging.info("Foo")
+    """
+    return datetime.datetime.fromtimestamp(time.time()).strftime('%Y%m%d_%H%M%S')
 
 # return code index in the list returned by do_exec
 idx_rc = 1
@@ -28,3 +42,21 @@ def do_exec(cmd):
 # os.environ['...'] doesn't work
 dev = str(Path.home())+"/dev"
 dec = str(Path.home())+"/dec"
+
+files = [
+    "git.sh",
+    "linux.sh",
+    "win.bat",
+    "clojure.clj",
+    "emacs.el",
+    "utf8.txt",
+]
+
+def links(f):
+    target_dir = str(Path.home()) + "/dev/cheatsheet/cmds/"
+    linkname_dir = target_dir+"crep/"
+    cmd = ["ln", "--symbolic", target_dir+f, linkname_dir+f]
+    do_exec(cmd)
+
+# w/o the map(...) into list(...) I get only # <map object at 0x7fa54f2b6048>
+list(map(links, files))
