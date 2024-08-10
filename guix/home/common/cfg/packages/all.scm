9b2e74f77a74d9f407baa31b539bf245adabfe75
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2024-08-10 10:28:52 +0200

    gxh: comments (icedove ~ thunderbird)

--- guix/home/common/cfg/packages/all.scm
+++ guix/home/common/cfg/packages/all.scm
@@ -174,7 +174,7 @@ when called from the Emacs Geiser REPL by ,use or ,load"
    ;; Rebranded Mozilla Thunderbird email client. Optionally install also
    ;; libotr the Off-the-Record (OTR) Messaging Library and Toolkit. See
    ;; Thunderbird console Ctrl-Shift-j
-   icedove  ;; 48.8MiB
+   icedove  ;; 48.8MiB ; ie. thunderbird
 
    ;; Vector graphics editor
    inkscape ;; ~93MiB
