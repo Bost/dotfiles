d84c68e49c37b2ecc3f17541728a87e0f9960a59
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2024-09-07 21:11:30 +0200

    gxh: Manage Emacs packages

--- guix/home/common/cfg/packages/spguimacs/all.scm
+++ guix/home/common/cfg/packages/spguimacs/all.scm
@@ -314,9 +314,13 @@
         bst:emacs-vdiff-magit
 
         bst:emacs-chatgpt-shell
+        bst:emacs-nyan-mode
+        bst:emacs-sly-named-readtables
         ))
       (partial map (comp list specification->package+output))
 
+      (partial remove (partial string= "emacs-sly-named-readtables"))
+      (partial remove (partial string= "emacs-nyan-mode"))
       (partial remove (partial string= "emacs-chatgpt-shell"))
 
       (partial remove (partial string= "emacs-use-package-chords"))
