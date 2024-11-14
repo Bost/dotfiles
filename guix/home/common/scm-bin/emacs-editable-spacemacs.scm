f8e6c6ffe93af246627b602914c67a34957fbac3
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2024-11-14 14:06:08 +0100

    gxh: create emacs-common (DRY)

--- guix/home/common/scm-bin/emacs-editable-spacemacs.scm
+++ guix/home/common/scm-bin/emacs-editable-spacemacs.scm
@@ -2,8 +2,7 @@
 ;;; All used modules must be present in the module (srvc scheme-files) under:
 ;;;   service-file -> with-imported-modules
   #:use-module (utils)
-  #:use-module (emacs-editable-config)
-  #:use-module (ice-9 getopt-long) ;; command-line arguments handling
+  #:use-module (emacs-common)
   #:export (main))
 
 #|
@@ -15,6 +14,7 @@
 cd $dotf
 ./guix/home/common/scm-bin/emacs-editable-spacemacs.scm --version
 ./guix/home/common/scm-bin/emacs-editable-spacemacs.scm --gx-dry-run
+./guix/home/common/scm-bin/emacs-editable-spacemacs.scm --version --gx-dry-run
 ./guix/home/common/scm-bin/emacs-editable-spacemacs.scm
 
 |#
@@ -22,37 +22,7 @@ cd $dotf
 (define m (module-name-for-logging))
 (evaluating-module)
 
-(define dbg #f)
 (define utility-name (last (module-name (current-module))))
-
-(define (fun args)
-  "All the options, including(!) rest-args, must be specified for the option-spec
-so that the options-parser doesn't complain about e.g. 'no such option: -p'."
-  (let* [(option-spec
-          ;; (value #t): a given option expects accept a value
-          `[
-            (help       (single-char #\h) (value #f))
-            (version    (single-char #\v) (value #f))
-            (gx-dry-run (single-char #\d) (value #f))
-            (rest-args                    (value #f))
-            ])]
-    ;; (format #t "~a option-spec : ~a\n" m option-spec)
-    (let* [(options (getopt-long args option-spec))
-           ;; #f means that the expected value wasn't specified
-           (val-gx-dry-run (option-ref options 'gx-dry-run #f))
-           (val-rest-args  (option-ref options '()         #f))
-           ]
-      (when dbg
-        (format #t "~a options        : ~a\n" m options)
-        (format #t "~a val-gx-dry-run : ~a\n" m val-gx-dry-run)
-        (format #t "~a val-rest-args  : ~a\n" m val-rest-args))
-      (begin
-        (apply
-         (partial set-config-editable
-                  #:gx-dry-run val-gx-dry-run
-                  #:profile spacemacs)
-         val-rest-args)))))
-(testsymb 'fun)
 
 (define (main args)
   "Usage:
@@ -60,7 +30,10 @@ so that the options-parser doesn't complain about e.g. 'no such option: -p'."
 (main (list \"<ignored>\" \"rest\" \"args\"))
 (main (list \"<ignored>\" \"--gx-dry-run\" \"rest\" \"args\"))
 "
-  (handle-cli #:utility-name utility-name #:fun fun args))
+  (handle-cli #:utility-name utility-name
+              #:fun set-config-editable
+              #:profile spacemacs
+              args))
 (testsymb 'main)
 
 (module-evaluated)
