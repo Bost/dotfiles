0b1d4b4fe278223f8ae965f4a7f464a8816d531c
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2025-04-12 14:39:04 +0200

    [wip] gxh: scheme-files: emacs-cli-utils 8.

--- guix/home/common/emacs-common.scm
+++ guix/home/common/emacs-common.scm
@@ -11,6 +11,9 @@
             handle-cli
             pkill-server
             set-config-editable
+
+            set-editable-profiles
+            handle-cli-profiles
             ))
 
 #|
@@ -183,6 +186,97 @@ Usage:
             (copy-file src dst))))
     ))
 (testsymb 'set-config-editable)
+
+(define* (set-editable-profiles #:key (verbose #f) utility-name gx-dry-run #:rest args)
+  "
+Usage:
+(set-editable #:gx-dry-run #t \"rest\" \"args\")
+(set-editable                 \"rest\" \"args\")
+(set-editable                 \"rest\" \"args\")
+"
+  (let* [(f "[set-editable-profiles]")]
+    (when verbose
+      (format #t "~a ~a utility-name : ~a\n" m f utility-name)
+      (format #t "~a ~a gx-dry-run   : ~a\n" m f gx-dry-run)
+      (format #t "~a ~a args         : ~a\n" m f args)
+      )
+
+    (let* [(args (remove-kw-from-args #:verbose args))
+           (args (remove-kw-from-args #:utility-name args))
+           (args (remove-kw-from-args #:gx-dry-run args))
+           (monad (if gx-dry-run
+                      compose-commands-guix-shell-dry-run
+                      compose-commands-guix-shell))]
+      (if gx-dry-run
+          (begin
+            (format #t "~a ~a monad: ~a\n" m f monad)
+            (format #t "~a ~a TODO implement --gx-dry-run\n" m f))
+          (let* [(file ".emacs-profiles.el")
+                 (dst (str home "/" file))
+                 (src (str (getenv "dotf") "/" file))]
+            (when verbose
+              (format #t "~a ~a src: ~a\n" m f src)
+              (format #t "~a ~a dst: ~a\n" m f dst))
+            (with-monad monad
+              (>>=
+               (return (list dst))
+               mdelete-file
+               `(override-mv ,src ,dst)
+               mcopy-file))
+            (copy-file src dst))))))
+(testsymb 'set-editable-profiles)
+
+(define* (handle-cli-profiles #:key (verbose #f) utility-name fun #:rest args)
+  "All the options, except rest-args, must be specified for the option-spec so
+ that the options-parser doesn't complain about e.g. 'no such option: -p'."
+  ;; (format #t "~a args: ~a\n" m args)
+  (let* [(f "[handle-cli-profiles]")]
+    (when verbose
+      (format #t "~a ~a utility-name : ~a\n" m f utility-name)
+      (format #t "~a ~a fun          : ~a\n" m f fun)
+      (format #t "~a ~a args         : ~a\n" m f args)
+      )
+    (let* [(args (remove-kw-from-args #:verbose args))
+           (args (remove-kw-from-args #:utility-name args))
+           (args (remove-kw-from-args #:fun args))
+           (args (car args))
+
+           ;; (value #t): a given option expects accept a value
+           (option-spec `[
+                          (help       (single-char #\h) (value #f))
+                          (version    (single-char #\v) (value #f))
+                          (gx-dry-run (single-char #\d) (value #f))
+                          (rest-args                    (value #f))])
+
+           ;; TODO isn't the #:stop-at-first-non-option swapped?
+           (options (getopt-long args option-spec #:stop-at-first-non-option #t))
+           ;; #f means that the expected value wasn't specified
+           (val-help       (option-ref options 'help       #f))
+           (val-version    (option-ref options 'version    #f))
+           (val-gx-dry-run (option-ref options 'gx-dry-run #f))
+           (val-rest-args  (option-ref options '()         #f))
+           ]
+      (when verbose
+        (format #t "~a ~a option-spec    : ~a\n" m f option-spec)
+        (format #t "~a ~a options        : ~a\n" m f options)
+        (format #t "~a ~a val-help       : ~a\n" m f val-help)
+        (format #t "~a ~a val-version    : ~a\n" m f val-version)
+        (format #t "~a ~a val-gx-dry-run : ~a\n" m f val-gx-dry-run)
+        (format #t "~a ~a val-rest-args  : ~a\n" m f val-rest-args))
+      (cond
+       [val-help
+        (format #t "~a [options]\n~a\n~a\n\n"
+                utility-name
+                "    -v, --version    Display version"
+                "    -h, --help       Display this help")]
+       [val-version
+        (format #t "~a version <...>\n" utility-name)]
+       [#t
+        (apply (partial fun
+                        #:utility-name utility-name
+                        #:gx-dry-run val-gx-dry-run)
+               val-rest-args)]))))
+(testsymb 'handle-cli-profiles)
 
 (define* (handle-cli #:key (verbose #f) utility-name fun profile #:rest args)
   "All the options, except rest-args, must be specified for the option-spec so
--- guix/home/common/srvc/scheme-files.scm
+++ guix/home/common/srvc/scheme-files.scm
@@ -6,7 +6,8 @@
   #:use-module (memo)
   #:use-module (fs-utils)
   #:use-module (srvc emacs-cli-utils)
-  ;; See service-file -> with-imported-modules
+  #:use-module (emacs-common)  ;; handle-cli, create-emacs-launcher, etc.
+  ;; See service-file-general -> with-imported-modules
   #:use-module (scm-bin gcl)
   #:use-module (gnu home)
   #:use-module (gnu packages)
@@ -93,10 +94,11 @@
 (define emacs-utils-lst (append launcher-lst editable-lst pkill-lst))
 (testsymb 'emacs-utils-lst)
 
-(define* (service-file #:key
-                       program-name desc scheme-file module-name
-                       chmod-params files
-                       (other-files (list)))
+(define* (service-file-general
+          #:key
+          program-name desc scheme-file module-name
+          chmod-params files
+          (other-files (list)))
   "The priority is 1. module-name, 2. scheme-file, 3. program-name
 
 TODO The `search-notes' program should read a `search-space-file' containing
@@ -105,7 +107,7 @@ a list of files to search through.
 Example:
     chmod --recursive u=rwx,g=rwx,o=rwx /path/to/dir
 "
-  (let* [(m (format #f "~a [service-file]" m))]
+  (let* [(m (format #f "~a [service-file-general]" m))]
     ;; (format #t "~a Starting ...\n" m)
     ;; (format #t "~a program-name : ~s\n" m program-name)
     ;; (format #t "~a desc         : ~s\n" m desc)
@@ -116,143 +118,133 @@ Example:
     ;; (format #t "~a other-files  : ~s\n" m other-files)
     ;; (format #t "\n")
     ;; (format #t "~a member : ~s\n" m (member scheme-file emacs-utils-lst))
+    (list
+     (str scm-bin-dirname "/" program-name)
 
-    ;; TODO for emacs-utils I must return a pair
-    ;;   (<...> (gexp->file <name> <exp>))
-    ;; instead of
-    ;;   (<...> (program-file <name> <exp>))
-    (cond
-     [(equal? scheme-file pkill-crafted)
-      (begin
-        ;; (format #t "$$$$ ~a (str scm-bin-dirname \"/\" program-name): ~s\n" m (str scm-bin-dirname "/" program-name))
-        (let* [(prg-file
-                (program-file
-                 scheme-file ;; 1st param: name
-                 (let* [(symb-string (or scheme-file program-name))
-                        (symb (or module-name
-                                  (string->symbol symb-string)))]
-                   (with-imported-modules
-                       `((guix monads)
-                         (utils)
-                         (settings)
-                         (emacs-common))
-                     #~(begin
-                         (use-modules (ice-9 getopt-long)
-                                      (ice-9 regex)
-                                      (guix monads)
-                                      (utils)
-                                      (settings)
-                                      (emacs-common))
+     (program-file
+      ;; 1st param: name
+      (cond
+       [(equal? scheme-file "chmod")
+        (str "chmod-plus-" chmod-params)]
+       [(equal? scheme-file "search-notes")
+        (str "search-notes-" program-name)]
+       [#t
+        desc])
 
-                         (handle-cli #:utility-name #$scheme-file
-                                     #:fun pkill-server ;; see emacs-utils
-                                     #:profile crafted ;; see emacs-profiles
-                                     (command-line)))
+      ;; 2nd param: exp
+      ;; TODO clarify if source-module-closure is needed only for imports of
+      ;; guix modules?
+      (let* [(symb-string (or scheme-file program-name))
+             (symb (or module-name
+                       (string->symbol symb-string)))
+             (main-call ((comp (partial remove unspecified?))
+                         (list
+                          'main
+                          (cond
+                           [(equal? scheme-file "chmod")
+                            `(let [(cmd-line (command-line))]
+                               (append (list (car cmd-line)
+                                             ,chmod-params)
+                                       (cdr cmd-line)))]
 
-                     ))))]
-          (format #t "$$$$ ~a prg-file : ~s\n" m prg-file)
-          (list (str scm-bin-dirname "/" program-name) prg-file)))]
-     [(member scheme-file emacs-utils-lst)
-      (begin
-        ;; (format #t "$$$$ ~a (str scm-bin-dirname \"/\" program-name): ~s\n" m (str scm-bin-dirname "/" program-name))
-        (let* [(prg-file
-                (program-file
-                 scheme-file ;; 1st param: name
+                           [(equal? scheme-file "search-notes")
+                            `(append
+                              (command-line)
+                              (list ,@(append other-files
+                                              (full-filepaths files))))]
 
-                 ;; 2nd param: exp
-                 ;; TODO clarify if source-module-closure is needed only for imports of
-                 ;; guix modules?
-                 (let* [(symb-string (or scheme-file program-name))
-                        (symb (or module-name
-                                  (string->symbol symb-string)))]
-                   (with-imported-modules
-                       `((guix monads)
-                         (utils)
-                         (settings)
-                         (emacs-common)
-                         (scm-bin ,symb))
+                           [#t `(command-line)]))))]
+        (with-imported-modules
+            ((comp
+              (partial remove unspecified?)
+              (lambda (lst)
+                (if (equal? scheme-file "search-notes")
+                    (append lst `(
+                                  (guix profiling)
+                                  (guix memoization)
+                                  (guix colors)
+                                  ;; (ice-9 getopt-long)
+                                  ))
+                    lst)))
+             `((guix monads)
+               (utils)
+               (settings)
+               ;; following three modules don't need to be everywhere
+               (scm-bin gre)
+               (scm-bin gps)
 
-                     #~(begin
-                         (use-modules (scm-bin #$symb))
-                         (main (command-line)))))))]
-          ;; (format #t "$$$$ ~a prg-file : ~s\n" m prg-file)
-          (list (str scm-bin-dirname "/" program-name) prg-file)))]
-     [#t
-      (begin
-        ;; (format #t "%%%% ~a (str scm-bin-dirname \"/\" program-name): ~s\n" m (str scm-bin-dirname "/" program-name))
-        (list
-         (str scm-bin-dirname "/" program-name)
-         (program-file
+               ;; module-search-notes
+               ;; 'ls' is needed only for 'lf.scm'
+               ,(cond
+                 [(equal? symb-string "lf")           `(scm-bin ls)]
+                 [(equal? scheme-file "chmod")        `(scm-bin ,symb)]
+                 [(equal? scheme-file "search-notes") `(scm-bin ,symb)]
+                 [#t                                  `(scm-bin ,symb)])))
 
-          ;; 1st param: name
-          (cond
-           [(equal? scheme-file "chmod")
-            (str "chmod-plus-" chmod-params)]
-           [(equal? scheme-file "search-notes")
-            (str "search-notes-" program-name)]
-           [#t
-            desc])
+          #~(begin
+              (use-modules (scm-bin #$symb))
+              #$main-call)))))))
+(testsymb 'service-file-general)
 
-          ;; 2nd param: exp
-          ;; TODO clarify if source-module-closure is needed only for imports of
-          ;; guix modules?
-          (let* [(symb-string (or scheme-file program-name))
-                 (symb (or module-name
-                           (string->symbol symb-string)))
-                 (main-call ((comp (partial remove unspecified?))
-                             (list
-                              'main
-                              (cond
-                               [(equal? scheme-file "chmod")
-                                `(let [(cmd-line (command-line))]
-                                   (append (list (car cmd-line)
-                                                 ,chmod-params)
-                                           (cdr cmd-line)))]
+(define* (service-file-emacs-utils
+          #:key program-name scheme-file (verbose #f) utility-name fun profile)
+  " TODO The `search-notes' program should read a `search-space-file' containing
+a list of files to search through.
 
-                               [(equal? scheme-file "search-notes")
-                                `(append
-                                  (command-line)
-                                  (list ,@(append other-files
-                                                  (full-filepaths files))))]
+Example:
+    chmod --recursive u=rwx,g=rwx,o=rwx /path/to/dir
+"
+  (let* [(m (format #f "~a [service-file-emacs-utils]" m))]
+    ;; (format #t "~a Starting ...\n" m)
+    ;; (format #t "~a program-name : ~s\n" m program-name)
+    ;; (format #t "~a scheme-file  : ~s\n" m scheme-file)
+    ;; (format #t "~a fun          : ~s\n" m fun)
+    ;; (format #t "~a profile      : ~s\n" m profile)
+    ;; (format #t "\n")
+    ;; (format #t "~a member : ~s\n" m (member scheme-file emacs-utils-lst))
 
-                               [#t `(command-line)]))))]
-            (with-imported-modules
-                ((comp
-                  (partial remove unspecified?)
-                  (lambda (lst)
-                    (if (equal? scheme-file "search-notes")
-                        (append lst `(
-                                      (guix profiling)
-                                      (guix memoization)
-                                      (guix colors)
-                                      ;; (ice-9 getopt-long)
-                                      ))
-                        lst)))
-                 `((guix monads)
-                   (utils)
-                   (settings)
-                   ;; following three modules don't need to be everywhere
-                   (scm-bin gre)
-                   (scm-bin gps)
+    (list
+     (str scm-bin-dirname "/" program-name)
+     (let* [(prg-file
+             (program-file
+              scheme-file ;; 1st param: name
+              (let* [(symb-string (or scheme-file program-name))
+                     (symb (or module-name
+                               (string->symbol symb-string)))
+                     (sexp (if (equal? fun 'set-editable-profiles)
+                               `(handle-cli-profiles
+                                 #:fun ,fun
+                                 (command-line))
+                               `(handle-cli
+                                 #:fun ,fun
+                                 #:profile ,profile
+                                 (command-line))))
+                     ]
+                ;; (format #t "$$$$ ~a sexp :\n~s\n\n" m sexp)
+                (with-imported-modules
+                    `((guix monads)
+                      (utils)
+                      (settings)
+                      (emacs-common))
+                  #~(begin
+                      (use-modules (ice-9 getopt-long)
+                                   (ice-9 regex)
+                                   (guix monads)
+                                   (utils)
+                                   (settings)
+                                   (emacs-common))
 
-                   ;; module-search-notes
-                   ;; 'ls' is needed only for 'lf.scm'
-                   ,(cond
-                     [(equal? symb-string "lf")           `(scm-bin ls)]
-                     [(equal? scheme-file "chmod")        `(scm-bin ,symb)]
-                     [(equal? scheme-file "search-notes") `(scm-bin ,symb)]
-                     [#t                                  `(scm-bin ,symb)])))
-
-              #~(begin
-                  (use-modules (scm-bin #$symb))
-                  #$main-call))))))])))
-(testsymb 'service-file)
+                      #$sexp)
+                  ))))]
+       ;; (format #t "$$$$ ~a prg-file :\n~s\n\n" m prg-file)
+       prg-file))))
+(testsymb 'service-file-emacs-utils)
 
 (define search-notes-service-files
   (let* [(m (format #f "~a [search-notes-service-files]" m))]
     ;; (format #t "~a Starting ...\n" m)
     (map
-     (partial apply service-file)
+     (partial apply service-file-general)
      (list
       (list #:program-name "crc"
             #:files (list "lisp/clojure")
@@ -357,6 +349,7 @@ Example:
             #:scheme-file "search-notes")))))
 (testsymb 'search-notes-service-files)
 
+
 (define-public (scheme-files-service)
   (let* [(m (format #f "~a [scheme-files-service]" m))]
     ;; (format #t "~a Starting ...\n" m)
@@ -370,25 +363,23 @@ Example:
        (if (or (is-system-ecke) (is-system-edge))
            ((comp
              (partial append search-notes-service-files)
-             (partial map (partial apply service-file))
+             (partial map (partial apply service-file-emacs-utils))
              )
             (list
-             (list #:program-name "ep" #:scheme-file editable-profiles)
-
-             (list #:program-name  "d" #:scheme-file launcher-develop)
-             (list #:program-name "ed" #:scheme-file editable-develop)
-             (list #:program-name "kd" #:scheme-file pkill-develop)
-
-             (list #:program-name  "g" #:scheme-file launcher-spguimacs)
-             (list #:program-name "eg" #:scheme-file editable-spguimacs)
-             (list #:program-name "kg" #:scheme-file pkill-spguimacs)
-
-             (list #:program-name  "r" #:scheme-file launcher-crafted)
-             (list #:program-name "er" #:scheme-file editable-crafted)
-             (list #:program-name "kr" #:scheme-file pkill-crafted)
+             ;; TODO editable-profiles
+             (list #:program-name "ep" #:scheme-file editable-profiles  #:fun 'set-editable-profiles)
+             (list #:program-name  "d" #:scheme-file launcher-develop   #:fun 'create-emacs-launcher #:profile develop)
+             (list #:program-name "ed" #:scheme-file editable-develop   #:fun 'set-config-editable   #:profile develop)
+             (list #:program-name "kd" #:scheme-file pkill-develop      #:fun 'pkill-server          #:profile develop)
+             (list #:program-name  "g" #:scheme-file launcher-spguimacs #:fun 'create-emacs-launcher #:profile spguimacs)
+             (list #:program-name "eg" #:scheme-file editable-spguimacs #:fun 'set-config-editable   #:profile spguimacs)
+             (list #:program-name "kg" #:scheme-file pkill-spguimacs    #:fun 'pkill-server          #:profile spguimacs)
+             (list #:program-name  "r" #:scheme-file launcher-crafted   #:fun 'create-emacs-launcher #:profile crafted)
+             (list #:program-name "er" #:scheme-file editable-crafted   #:fun 'set-config-editable   #:profile crafted)
+             (list #:program-name "kr" #:scheme-file pkill-crafted      #:fun 'pkill-server          #:profile crafted)
              ))
            (list)))
-      (partial map (partial apply service-file)))
+      (partial map (partial apply service-file-general)))
      (list
       ;; pwr and prw do the same
       (list #:program-name "pwr" #:chmod-params "rw" #:scheme-file "chmod")
