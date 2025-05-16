d83c0148c77611f804c05f93d141dbf1dea22624
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2025-05-16 19:15:42 +0200

    gxh: hrep: show search results in reversed order; add description

--- .config/fish/functions/hrep.fish
+++ .config/fish/functions/hrep.fish
@@ -1,5 +1,5 @@
-function hrep
-  set cmd history --show-time="[%Y-%m-%d %H:%M:%S]\ " --search --contains (string escape -- $argv)
-  echo $cmd
-  eval $cmd
+function hrep --description "Search fish-shell history"
+    set cmd history --show-time="[%Y-%m-%d %H:%M:%S]\ " --reverse --search --contains (string escape -- $argv)
+    echo $cmd
+    eval $cmd
 end
