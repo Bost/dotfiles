7083a6d91bdd7d49387815e4a72f438fb1c62696
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2025-06-09 20:10:43 +0200

    gxh: search-notes: fix line numbering

--- guix/home/common/scm-bin/search-notes.scm
+++ guix/home/common/scm-bin/search-notes.scm
@@ -63,7 +63,7 @@ cd $dotf
 ;;         "Y" "[YÝ]"
 ;;         "Z" "[ZŽ]"
 ;;         "ß" "ß")) ; German sharp S
-;; 
+;;
 ;; (define (string-normalize s)
 ;;   ;; Normalization Form C, Canonical Decomposition followed by Canonical
 ;;   ;; Composition:
@@ -73,14 +73,14 @@ cd $dotf
 ;;   ;; Use this when you want to normalize characters to their composed forms
 ;;   ;; while still respecting canonical equivalence.
 ;;   (string-normalize-nfc s))
-;; 
+;;
 ;; (define (regexp-normalize-match* regex target-str)
 ;;   ;; (printf "[regexp-normalize-match*] regex: ~a\n" regex)
 ;;   ;; (printf "[regexp-normalize-match*] target-str : ~a\n" target-str)
 ;;   (let* ((normalized-target (string-normalize target-str)))
 ;;     ;; (printf "[regexp-normalize-match*] normalized-target: ~a\n" normalized-target)
 ;;     (regexp-match* regex normalized-target)))
-;; 
+;;
 ;; (define (regexp-normalize-split regex target-str)
 ;;   ;; (printf "[regexp-normalize-split] regex: ~a\n" regex)
 ;;   ;; (printf "[regexp-normalize-split] target-str : ~a\n" target-str)
@@ -88,38 +88,48 @@ cd $dotf
 ;;     ;; (printf "[regexp-normalize-split] normalized-target: ~a\n" normalized-target)
 ;;     (regexp-split regex normalized-target)))
 
-(define* (search-file #:key ptrn file)
+(define (search-file pattern file)
   "
-(search-file
- #:ptrn \"pattern\"
- #:file \"/home/bost/dev/notes/notes/bric_a_brac.scrbl\")
+(search-file \"pattern\"
+             \"/home/bost/dev/notes/notes/bric_a_brac.scrbl\")
 "
-  (let* [(cmd1
-          ;; 't' means: number only nonempty lines
-          (format #f "nl --body-numbering=t '~a'" file))
+  ;; Red: \033[31m
+  ;; Bright Red: \033[1;31m
+  ;; Green: \033[32m
+  ;; Yellow: \033[33m
+  ;; Blue: \033[34m
+  ;; Magenta: \033[35m
+  ;; Cyan: \033[36m
+  ;; White: \033[37m
+  ;; Reset: \033[0m
+  (let* [
+         ;; If the line is empty or contains only whitespace chars then don't
+         ;; prefix it. Else prefix it with '<the-line-number>:' and
+         ;; <the-line-number> is displayed Green
+         (cmd1
+          (format #f
+                  "awk '
+/^[[:space:]]*$/ {
+    print; next
+}
+{
+    printf \"\\033[32m%d\\033[0m:  %s\\n\", NR, $0
+}
+' '~a'" file))
          (cmd2 "sed 's/^\\s*$//'")
+
          (cmd3
-          ;; Red: \033[31m
-          ;; Bright Red: \033[1;31m
-          ;; Green: \033[32m
-          ;; Yellow: \033[33m
-          ;; Blue: \033[34m
-          ;; Magenta: \033[35m
-          ;; Cyan: \033[36m
-          ;; White: \033[37m
-          ;; Reset: \033[0m
           (format
            #f
            ;; -v var=val		--assign=var=val
            "awk -v RS='' '/~a/ {gsub(/~a/, \"\\033[1;31m&\\033[0m\"); print $0 \"\\n\"}'"
-           ptrn ptrn))
+           pattern pattern))
          (cmd (format #f "~a | ~a | ~a" cmd1 cmd2 cmd3))
          (ret (exec cmd #:verbose #f))]
     (if (= 0 (car ret))
         (let* [(output (cdr ret))]
-          #| process output |#
           (unless (null? output)
-            (format #t "~a\n" (colorize-string file (color GREEN))) ; text in green
+            (format #t "~a\n" (colorize-string file (color MAGENTA))) ; file in magenta
             (map (partial format #t "~a\n") output)))
         (error-command-failed m))))
 (testsymb 'search-file)
@@ -133,12 +143,12 @@ cd $dotf
 "
   ;; (format #t "args: '~a'\n" args)
   (let* [(arg-lst (car args))
-         (ptrn ((comp car cdr) arg-lst))]
+         (pattern ((comp car cdr) arg-lst))]
     ;; (format #t "arg-lst: '~a'\n" arg-lst)
-    ;; (format #t "ptrn: '~a'\n" ptrn)
+    ;; (format #t "ptrn: '~a'\n" pattern)
     (let* [(files ((comp cdr cdr) arg-lst))]
       ;; (format #t "files: '~a'\n" files)
-      (map (partial search-file #:ptrn ptrn #:file) files))))
+      (map (partial search-file pattern) files))))
 
 (define main search-notes)
 
