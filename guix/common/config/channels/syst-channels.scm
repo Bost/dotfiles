38706215aca3596e2a188e44c5a12172e9c39ecc
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2025-08-11 12:44:16 +0200

    gx: Channel pinning

--- guix/common/config/channels/syst-channels.scm
+++ guix/common/config/channels/syst-channels.scm
@@ -3,4 +3,7 @@
 ;; guile -c '(use-modules (guix channels)) (format #t "~a\n" %default-channels)'
 
 (define-module (config channels syst-channels))
-((@(config channels channel-defs) syst-channels))
+((@(config channels channel-defs) syst-channels
+  ;; #:guix-commit    "f73e6468c93bf353b1187357fbf10044482dda88"
+  ;; #:nonguix-commit "43699ceddb6f5f7aa60a646daa44a75e269a3856"
+  ))
