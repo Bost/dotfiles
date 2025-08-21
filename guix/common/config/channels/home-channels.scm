120285db6e66997c25e649f134bb7d81e528e43c
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2025-08-21 17:17:24 +0200

    gx: Update channels

--- guix/common/config/channels/home-channels.scm
+++ guix/common/config/channels/home-channels.scm
@@ -37,8 +37,8 @@
            lst)
           lst)))
    (syst-channels
-    ;; #:guix-commit    "f73e6468c93bf353b1187357fbf10044482dda88"
-    ;; #:nonguix-commit "43699ceddb6f5f7aa60a646daa44a75e269a3856"
+    ;; #:guix-commit    "64ef6db09b1e14a12fc0d97a5a0615394d6d6c7a"
+    ;; #:nonguix-commit "9bf3d1394f595413c51d621e61f231789c74891e"
     )))
 (testsymb 'home-channels)
 
