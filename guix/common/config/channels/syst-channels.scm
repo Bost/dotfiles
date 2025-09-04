e8c969677a5bf741d0a7b3f4336dfa0334df6023
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2025-09-04 14:16:12 +0200

    gxs: Update syst-channels

--- guix/common/config/channels/syst-channels.scm
+++ guix/common/config/channels/syst-channels.scm
@@ -3,7 +3,11 @@
 ;; guile -c '(use-modules (guix channels)) (format #t "~a\n" %default-channels)'
 
 (define-module (config channels syst-channels))
-((@(config channels channel-defs) syst-channels
-  ;; #:guix-commit    "f73e6468c93bf353b1187357fbf10044482dda88"
-  ;; #:nonguix-commit "43699ceddb6f5f7aa60a646daa44a75e269a3856"
-  ))
+((@(config channels channel-defs) syst-channels)
+ ;; #:guix-commit    "f73e6468c93bf353b1187357fbf10044482dda88"
+ ;; #:nonguix-commit "43699ceddb6f5f7aa60a646daa44a75e269a3856"
+
+ ;; 03 sept. 2025 14:06:29
+ #:nonguix-commit "477f283914ca771a8622e16b73d845b87c63335d"
+ #:guix-commit "b377ec079d9ffe8f0f372c43735ad012ea889b6f"
+ )
