01e298f4d4c6e785c3378f963d1d069bf5e1b8d3
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2025-09-11 12:39:44 +0200

    gxs: Update syst-channels

--- guix/common/config/channels/syst-channels.scm
+++ guix/common/config/channels/syst-channels.scm
@@ -2,12 +2,18 @@
 ;; `~/.config/guix/current/manifest`. See
 ;; guile -c '(use-modules (guix channels)) (format #t "~a\n" %default-channels)'
 
+;;; TODO it seems like the channels are evaluated twice. Once from syst-channels and from home-channels, too.
+
 (define-module (config channels syst-channels))
 ((@(config channels channel-defs) syst-channels)
  ;; #:guix-commit    "f73e6468c93bf353b1187357fbf10044482dda88"
  ;; #:nonguix-commit "43699ceddb6f5f7aa60a646daa44a75e269a3856"
 
  ;; 03 sept. 2025 14:06:29
- #:nonguix-commit "477f283914ca771a8622e16b73d845b87c63335d"
- #:guix-commit "b377ec079d9ffe8f0f372c43735ad012ea889b6f"
+ ;; #:nonguix-commit "477f283914ca771a8622e16b73d845b87c63335d"
+ ;; #:guix-commit "b377ec079d9ffe8f0f372c43735ad012ea889b6f"
+
+ ;; 11 sept. 2025 12:33:23
+ ;; #:guix-commit "fe2ed12e66097ee2befc55d7ae88e2a7c19f9e72"
+ ;; #:nonguix-commit "d096df03564783372b315fe6c179655c2c337d5a"
  )
