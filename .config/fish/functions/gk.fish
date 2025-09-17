088bdf2a787b3e65d3689ef4e800f8366db62564
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2025-09-17 12:22:57 +0200

    gxh: cli-utils: Refactor

--- .config/fish/functions/gk.fish
+++
@@ -1,6 +0,0 @@
-function gk --description "gitk --all … &"
-    echo "# Try: tig / tig --all / tig status / tig log / tig grep"
-    set cmd gitk --all (string escape -- $argv) \&
-    echo $cmd
-    eval $cmd
-end
