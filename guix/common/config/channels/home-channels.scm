2ff8632942bb808e9ddd5af32bc6ef51af4f346a
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2025-09-25 19:39:49 +0200

    gxh: Update home-channels

--- guix/common/config/channels/home-channels.scm
+++ guix/common/config/channels/home-channels.scm
@@ -28,13 +28,13 @@
             (channel-guix-android
              ;; 16 sept. 2025 13:50 (roughly)
              ;; 18 sept. 2025 10:11:24
-             #:commit "e5f52bd57275e404db74bf03b541bb62f7d73d58"
+             ;; #:commit "e5f52bd57275e404db74bf03b541bb62f7d73d58"
              )
             ;; (channel-guix-past) ;; pulled-in via channel-games; not needed directly.
             (channel-guixrus ;; whereiseveryone
              ;; 16 sept. 2025 12:33:23
              ;; 18 sept. 2025 10:11:24
-             #:commit "83321ce5d23109f143413490aa5acf04361c8076"
+             ;; #:commit "83321ce5d23109f143413490aa5acf04361c8076"
              )
             (channel-hask-clj
              ;; 16 sept. 2025 12:33:23
@@ -90,7 +90,11 @@
     ;; #:nonguix-commit "de297a2a28577651cbe27ba58f8b9ea8912392b0"
 
     ;; 24 2025 22:07:31
-    #:guix-commit "aabe0d2c30a2c8143ff70e3f5be1189ff6d494cd"
+    ;; #:guix-commit "aabe0d2c30a2c8143ff70e3f5be1189ff6d494cd"
+    ;; #:nonguix-commit "25d7a8091c2c9678a8694f073d846a7001165169"
+
+    ;; 25 sept. 2025 18:59:29
+    #:guix-commit "ace78713a9ee7abfdd19e2f009c3fb606b797160"
     #:nonguix-commit "25d7a8091c2c9678a8694f073d846a7001165169"
     )))
 (testsymb 'home-channels)
