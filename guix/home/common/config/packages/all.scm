ca94d49c3c85566bf60333575de613de1dd804c0
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2025-10-17 12:20:17 +0200

    gxh: Add OCR packages to the profile

--- guix/home/common/config/packages/all.scm
+++ guix/home/common/config/packages/all.scm
@@ -46,7 +46,7 @@
  virtualization racket readline mp3 texinfo freedesktop cdrom lua emacs-xyz
  elixir tree-sitter agda idris emacs text-editors patchutils java glib maven
  mail messaging irc commencement gcc clojure machine-learning cups scanner
- file-systems librewolf libcanberra security-token firmware
+ file-systems librewolf libcanberra security-token firmware ocr
  )
 
 (define (email-in-emacs-packages)
@@ -665,9 +665,9 @@ TODO implement: Show warning & don't compile if substitutes are not present."
    youtube-viewer  ; Search & play YT videos in a native player
    vlc             ; Audio and video player and framework
 
-   ;; tesseract-ocr               ; OCR Optical character recognition engine
-   ;; tesseract-ocr-tessdata-fast ; Fast versions of trained LSTM models
-   ;; gimagereader                ; Qt front-end to tesseract-ocr
+   tesseract-ocr               ; OCR Optical character recognition engine
+   tesseract-ocr-tessdata-fast ; Fast versions of trained LSTM models
+   gimagereader                ; Qt front-end to tesseract-ocr
 
    surfraw       ; Unix command line interface to the www
    w3m           ; Text-mode web browser
