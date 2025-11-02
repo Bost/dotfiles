79174332b92aae9e5d531c52c1a32a0ef9978ce6
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2025-11-02 01:48:55 +0100

    gxh: cli-common: Implement --dx-dry-run

--- guix/home/common/cli-common.scm
+++ guix/home/common/cli-common.scm
@@ -55,26 +55,27 @@ Examples:
     (format #t "~a #:exec-fun      ~a ; ~a\n" f (pr-str-with-quote exec-fun)      (test-type exec-fun))
     (format #t "~a #:ignore-errors ~a ; ~a\n" f (pr-str-with-quote ignore-errors) (test-type ignore-errors))
     (format #t "~a   args          ~a ; ~a\n" f (pr-str-with-quote args)          (test-type args)))
-  (if gx-dry-run
-      (begin
-        ;; <stdin>:1494:40: warning: possibly unbound variable `monad'
-        ;; (format #t "~a monad: ~a\n" f monad)
-        (format #t "~a TODO implement --gx-dry-run\n" f))
-      (begin
-        ((comp
-          ;; (lambda (p) (format #t "~a done\n" f) p)
-          (lambda (command) (exec-fun command
-                                      #:trace trace
-                                      #:verbose verbose
-                                      #:ignore-errors ignore-errors
-                                      ))
-          ;; (lambda (p) (format #t "~a 2. ~a\n" f p) p)
-          (partial append (list params))
-          ;; (lambda (p) (format #t "~a 1. ~a\n" f p) p)
-          (partial map (partial format #f "~s"))
-          ;; (lambda (p) (format #t "~a 0. ~a\n" f p) p)
-          )
-         (list (last args))))))
+  ((comp
+    ;; (lambda (p) (format #t "~a done\n" f) p)
+    (lambda (command)
+      (if gx-dry-run
+          (begin
+            ;; <stdin>:1494:40: warning: possibly unbound variable `monad'
+            ;; (format #t "~a monad: ~a\n" f monad)
+            (format #t "~a --gx-dry-run : ~a\n" f gx-dry-run)
+            (format #t "~a ~a\n" f command))
+          (exec-fun command
+                    #:trace trace
+                    #:verbose verbose
+                    #:ignore-errors ignore-errors
+                    )))
+    ;; (lambda (p) (format #t "~a 2. ~a\n" f p) p)
+    (partial append (list params))
+    ;; (lambda (p) (format #t "~a 1. ~a\n" f p) p)
+    (partial map (partial format #f "~s"))
+    ;; (lambda (p) (format #t "~a 0. ~a\n" f p) p)
+    )
+   (list (last args))))
 
 (define-public (main . args)
   (format #t "~a\n" args)
