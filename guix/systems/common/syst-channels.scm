8d5559c3e16f3571ea2879d9aed31796d70b41f8
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2025-11-03 12:53:35 +0100

    gxs: Update syst-channels

--- guix/systems/common/syst-channels.scm
+++ guix/systems/common/syst-channels.scm
@@ -12,21 +12,6 @@
 
 (define (syst-channels)
   (common-channels
-   ;; #:guix-commit    "f73e6468c93bf353b1187357fbf10044482dda88"
-   ;; #:nonguix-commit "43699ceddb6f5f7aa60a646daa44a75e269a3856"
-
-   ;; 03 sept. 2025 14:06:29
-   ;; #:nonguix-commit "477f283914ca771a8622e16b73d845b87c63335d"
-   ;; #:guix-commit "b377ec079d9ffe8f0f372c43735ad012ea889b6f"
-
-   ;; 11 sept. 2025 14:29:25
-   ;; #:guix-commit "fe2ed12e66097ee2befc55d7ae88e2a7c19f9e72"
-   ;; #:nonguix-commit "d096df03564783372b315fe6c179655c2c337d5a"
-
-   ;; 22 sept. 2025 17:56:58
-   ;; #:guix-commit "f662d534068736d0770f9988a29e268db2db2a51"
-   ;; #:nonguix-commit "bc2a73d7a8252b14a2a3705f8df791ab314f5a5c"
-
    ;; 25 sept. 2025 19:54:02
    ;; #:guix-commit "ace78713a9ee7abfdd19e2f009c3fb606b797160"
    ;; #:nonguix-commit "25d7a8091c2c9678a8694f073d846a7001165169"
@@ -44,8 +29,12 @@
    ;; #:nonguix-commit "3f4a1907cae9b0def09d1549c491bb4e307b9097"
 
    ;; 22 oct. 2025 16:43:15
-   #:guix-commit "f01920e7f4962896452126928ea45a3b9e31832b"
-   #:nonguix-commit "a345ef84fbdf3b2491acb2c2b6665a4eb97bd4aa"
+   ;; #:guix-commit "f01920e7f4962896452126928ea45a3b9e31832b"
+   ;; #:nonguix-commit "a345ef84fbdf3b2491acb2c2b6665a4eb97bd4aa"
+
+ 	 ;; 03 nov. 2025 00:09:24
+   #:guix-commit "4fd32c00abb59b0f275a93c3fef35d5193adbe7a"
+   #:nonguix-commit "971bc275cf8a6554003ca924c65142ab7d430cad"
    ))
 
 (syst-channels)
