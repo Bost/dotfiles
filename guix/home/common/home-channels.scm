e26e6f0ca43c641aa4f8727c8b99d6780ff4fe14
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2025-11-04 13:43:35 +0100

    gxh: Update home-channels

--- guix/home/common/home-channels.scm
+++ guix/home/common/home-channels.scm
@@ -29,33 +29,23 @@
           (append
            (list
             (channel-guix-android
-             ;; 16 sept. 2025 13:50 (roughly)
-             ;; 18 sept. 2025 10:11:24
-             ;; #:commit "e5f52bd57275e404db74bf03b541bb62f7d73d58"
-
              ;; 07 oct. 2025 15:33:59
-             ;; #:commit "e5f52bd57275e404db74bf03b541bb62f7d73d58"
-
              ;; 23 oct. 2025 23:06:15
+	           ;; 04 nov. 2025 13:20:55
              ;; #:commit "e5f52bd57275e404db74bf03b541bb62f7d73d58"
              )
             ;; (channel-guix-past) ;; pulled-in via channel-games; not needed directly.
             (channel-guixrus ;; whereiseveryone
-             ;; 16 sept. 2025 12:33:23
-             ;; 18 sept. 2025 10:11:24
-             ;; #:commit "83321ce5d23109f143413490aa5acf04361c8076"
-
              ;; 07 oct. 2025 15:33:59
              ;; #:commit "83321ce5d23109f143413490aa5acf04361c8076"
 
              ;; 23 oct. 2025 23:06:15
+             ;; 04 nov. 2025 13:20:55
              ;; #:commit "40f83be862e31832f4571b826ff6a5f5372e905c"
              )
             (channel-hask-clj
-             ;; 16 sept. 2025 12:33:23
-             ;; #:commit "a8b30a606f91caabec3cc8dc4b1255a69836554e"
-
              ;; 23 oct. 2025 23:06:15
+             ;; 04 nov. 2025 13:20:55
              ;; #:commit "a8b30a606f91caabec3cc8dc4b1255a69836554e"
              )
             ;; (channel-games)
@@ -63,49 +53,29 @@
             ;; (channel-flat)
             ;; (channel-rde)
             (channel-bost
-             ;; 16 sept. 2025 12:33:23
-             ;; #:commit "8eacfc4fece97e9fbff4ecaf7bb3cb181b0c12ca"
-
-             ;; 18 sept. 2025 10:11:24
-             ;; #:commit "96d8f9cbc39780a90935d0f49edb372b77702875"
-
              ;; 07 oct. 2025 15:33:59
              ;; #:commit "ead5b77819456c577d162e047f8ec6eed9252520"
 
              ;; 23 oct. 2025 23:06:15
              ;; #:commit "f63e660d12ca11a99052859ebce08d39404b1001"
+
+	           ;; 04 nov. 2025 13:20:55	(actuelle)
+             ;; #:commit "c8ca36f01a616f17eeb6a581ac9de76b57cd9af3"
              ))
            lst)
           lst)))
    (common-channels
-    ;; 18 sept. 2025 10:11:24
-    ;; #:guix-commit "f62206ecd8605538b1fb18d038d3db33707a8535"
-    ;; #:nonguix-commit "df4e6ed9fe917f004357d931e210e328e348bb38"
-
-    ;; 20 sept. 2025 17:50:37
-    ;; #:guix-commit "f5b3b375e5d8cae24b02d00f213d09659e60358d"
-    ;; #:nonguix-commit "de297a2a28577651cbe27ba58f8b9ea8912392b0"
-
-    ;; 24 2025 22:07:31
-    ;; #:guix-commit "aabe0d2c30a2c8143ff70e3f5be1189ff6d494cd"
-    ;; #:nonguix-commit "25d7a8091c2c9678a8694f073d846a7001165169"
-
-    ;; 25 sept. 2025 18:59:29
-    ;; #:guix-commit "ace78713a9ee7abfdd19e2f009c3fb606b797160"
-    ;; #:nonguix-commit "25d7a8091c2c9678a8694f073d846a7001165169"
-
-    ;; 26 sept. 2025 13:03:02
-    ;; librewolf not present on substitutes yet
-    ;; #:guix-commit "3c96d9fb1e1ee8fe4efd44ba6e8f3886b43668be"
-    ;; #:nonguix-commit "25d7a8091c2c9678a8694f073d846a7001165169"
-
     ;; 07 oct. 2025 15:33:59
     ;; #:guix-commit "11ab410217fa094f040128917e90ea89ee586ffc"
     ;; #:nonguix-commit "a5d216cd7c2d67eb95e58871bb805f22c160c57b"
 
     ;; 23 oct. 2025 23:06:15
-    #:guix-commit "a38e1990eaded3f35c6898aa8fe817a1601702d7"
-    #:nonguix-commit "a345ef84fbdf3b2491acb2c2b6665a4eb97bd4aa"
+    ;; #:guix-commit "a38e1990eaded3f35c6898aa8fe817a1601702d7"
+    ;; #:nonguix-commit "a345ef84fbdf3b2491acb2c2b6665a4eb97bd4aa"
+
+	  ;; 04 nov. 2025 13:20:55	(actuelle)
+    #:guix-commit "873fc541a17f3a41d72c6b375754e035686de3fe"
+    #:nonguix-commit "74b20a74f9b37944f9532f5b649e962b50068faf"
     )))
 (testsymb 'home-channels)
 
