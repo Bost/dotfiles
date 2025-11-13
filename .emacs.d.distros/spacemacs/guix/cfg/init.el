4efc66ebdfed756fae523c2485129ae9c2691f74
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2025-11-13 17:39:42 +0100

    spacemacs: Add (global-undo-tree-mode)

--- .emacs.d.distros/spacemacs/guix/cfg/init.el
+++ .emacs.d.distros/spacemacs/guix/cfg/init.el
@@ -1314,6 +1314,8 @@ configuration.
 Put your configuration code here, except for variables that should be set
 before packages are loaded."
 
+  (global-undo-tree-mode)
+
   ;; (spacemacs/toggle-display-fill-column-indicator) ;; toggle with ~SCP t f~
 
   ;; (debug) ;; stops the execution. What I need is the stack frame as a string
