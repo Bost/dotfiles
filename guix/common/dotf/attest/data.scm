7e90b85f546c0a30cdc46aa4308bb286357653e4
Rostislav Svoboda <Rostislav.Svoboda@posteo.net>
2026-01-20 17:34:33 +0100

    gx: On building trust: From Authentication to Attestation 11

--- guix/common/dotf/attest/data.scm
+++ guix/common/dotf/attest/data.scm
@@ -2,6 +2,7 @@
   #:use-module (srfi srfi-1)   ; list-processing procedures
   #:use-module (srfi srfi-19)  ; current-date
   #:use-module (srfi srfi-88)  ; provides keyword objects
+  #:use-module (guix channels) ; %default-guix-channel %default-channels
   #:export
   (
    ksha
@@ -33,6 +34,11 @@
 (define kname :n)
 (define kcommitter :c)
 (define kfinger-print :f)
+
+(define kurl :u)
+(define ktype :t)
+(define kplatform :p)
+
 
 (define (person name fp) (list kname name kfinger-print fp))
 
@@ -86,3 +92,175 @@
    (cons* kattested "2026-01-10_09-58-07" (list-ref new-commits 5))
    (cons* kattested "2026-01-10_09-58-07" (list-ref new-commits 6))
    ))
+
+;;; ----------------------------------------------------------------------------
+(define (parse-ls-remote-line line)
+  "Parse: 'commit-hash\trefs/tags/tag-name'"
+  (match (string-split line #\tab)
+    ((hash ref)
+     (let ((tag-name (string-drop ref (string-length "refs/tags/"))))
+       (and (string-prefix? "running-" tag-name)
+            (make-tag-info tag-name hash))))
+    ;; (_ #f)
+    ))
+
+(define (fetch-git-ls-remote-tags url)
+  "Use git ls-remote to get tags without clone.
+
+git ls-remote --tags <url>
+git ls-remote --heads <url>
+
+git ls-remote --heads https://git.sr.ht/~abcdw/rde
+git ls-remote --heads https://github.com/Bost/guix.git
+git ls-remote --heads https://gitlab.com/rostislav.svoboda/guix.git
+
+#!/bin/bash
+
+REPO_URL=\"https://github.com/Bost/guix.git\"
+git ls-remote --heads $REPO_URL | awk '{print $2}' | grep --extended-regexp '^refs/tags/' | cut --delimiter='/' -f3
+
+"
+  (let* ((output (with-output-to-string
+                  (lambda ()
+                    (system* "git" "ls-remote" "--tags" url))))
+         (lines (string-split output #\newline)))
+    (filter-map parse-ls-remote-line lines)))
+
+(define (fetch-github-tags url)
+  "Use GitHub API for efficiency.
+
+curl --silent https://api.github.com/repos/<owner>/<repo>/tags
+
+curl --silent https://api.github.com/repos/Bost/guix/tags
+"
+  (let* ((repo-path (extract-github-repo url))
+         (api-url (string-append
+                   "https://api.github.com/repos/"
+                   repo-path "/tags"))
+         (json (http-get-json api-url)))
+    (filter-map
+     (lambda (tag-obj)
+       (let ((name (assoc-ref tag-obj "name")))
+         (and (string-prefix? "running-" name)
+              (make-tag-info
+               name
+               (assoc-ref (assoc-ref tag-obj "commit") "sha")))))
+     json)))
+
+(define (fetch-gitlab-tags url)
+  "Use GitLab API.
+
+curl --silent https://gitlab.com/api/v4/projects/<project-id>/
+(project-id is under the three vertical dots ⋮ on the upper right side)
+
+curl --silent https://gitlab.com/api/v4/projects/67989111/
+"
+  (let* ((project-id (extract-gitlab-project-id url))
+         (api-url (string-append
+                   "https://gitlab.com/api/v4/projects/"
+                   (uri-encode project-id)
+                   "/repository/tags"))
+         (json (http-get-json api-url)))
+    (filter-map
+     (lambda (tag-obj)
+       (let ((name (assoc-ref tag-obj "name")))
+         (and (string-prefix? "running-" name)
+              (make-tag-info
+               name
+               (assoc-ref tag-obj "commit" "id")))))
+     json)))
+
+(define (fetch-remote-tags url)
+  "Fetch tags from remote git repo without cloning."
+  (cond
+   ((string-contains url "github.com")   (fetch-github-tags url))
+   ((string-contains url "gitlab.com")   (fetch-gitlab-tags url))
+   ;; Codeberg (Gitea API)
+   ((string-contains url "codeberg.org") (fetch-gitea-tags url))
+   ;; Generic git (any hosting)
+   (else                                 (fetch-git-ls-remote-tags url))))
+
+(define %default-attestation-sources
+  (list
+   ;; Guix-based distributions
+   (list kname "RDE (Andrew Tropin)" kurl "https://git.sr.ht/~abcdw/rde" ktype 'channel)
+   (list kname "Guix-HPC"            kurl "https://gitlab.inria.fr/guix-hpc/guix-hpc" ktype 'channel)
+   (list kname "Efraim's channel"    kurl "https://git.sr.ht/~efraim/my-guix" ktype 'channel)
+   ))
+
+(define last-week-non-codeberg-forks
+  '(
+    https://github.com/Millak/guix                  ; Efraim Flashner
+    https://gitlab.com/debdistutils/guix/mirror     ; probably automatic
+    https://https.git.savannah.gnu.org/git/guix.git ; automatic
+    ))
+
+;;; forks with an update done 'last week' of later; checked on:
+;;; 2025-12-26 ; roughly; it was checked by the end of December 2025
+;;; 2026-01-19 ; i.e. after ~3 weeks
+;;; 25 out of ~348 forks only on Codeberg
+(define last-week-codeberg-forks
+  '(
+    https://codeberg.org/lechner/guix-mirror
+    https://codeberg.org/group/guix
+    https://codeberg.org/Yelninei/guix
+    https://codeberg.org/cmargiotta/guix
+    https://codeberg.org/museoa/guix
+    https://codeberg.org/hebasto/guix
+    https://codeberg.org/TohsakaTypeclass/guix
+    https://codeberg.org/sikmir/guix
+    https://codeberg.org/gabber/guix
+    https://codeberg.org/jnms/guix
+    https://codeberg.org/fanquake/guix
+    https://codeberg.org/r0man/guix
+    https://codeberg.org/FuncProgLinux/guix
+    https://codeberg.org/Doom4535/guix
+    https://codeberg.org/ajad/guix
+    https://codeberg.org/aquohn/guix
+    https://codeberg.org/mst/guix
+    https://codeberg.org/rodion-goritskov/guix
+    https://codeberg.org/kinote/guix
+    https://codeberg.org/ravenjoad/guix
+    https://codeberg.org/PatrickNorton/guix
+    https://codeberg.org/foster-hangdaan/guix
+    https://codeberg.org/frankie/guix
+    https://codeberg.org/yqshao/guix
+    https://codeberg.org/retropikzel/guix
+    ))
+
+;; See /home/bost/dev/whereiseveryone-toys/channels.scm
+;; Check the project stars
+
+
+;;; forks with an update done 'last week' of later; checked on:
+;;; 2025-12-26 ; roughly; it was checked by the end of December 2025
+;;; 2026-01-19 ; i.e. after ~3 weeks
+;;; 25 out of ~348 forks only on Codeberg
+(define last-week-codeberg-forks
+  '(
+    https://codeberg.org/lechner/guix-mirror
+    https://codeberg.org/group/guix
+    https://codeberg.org/Yelninei/guix
+    https://codeberg.org/cmargiotta/guix
+    https://codeberg.org/museoa/guix
+    https://codeberg.org/hebasto/guix
+    https://codeberg.org/TohsakaTypeclass/guix
+    https://codeberg.org/sikmir/guix
+    https://codeberg.org/gabber/guix
+    https://codeberg.org/jnms/guix
+    https://codeberg.org/fanquake/guix
+    https://codeberg.org/r0man/guix
+    https://codeberg.org/FuncProgLinux/guix
+    https://codeberg.org/Doom4535/guix
+    https://codeberg.org/ajad/guix
+    https://codeberg.org/aquohn/guix
+    https://codeberg.org/mst/guix
+    https://codeberg.org/rodion-goritskov/guix
+    https://codeberg.org/kinote/guix
+    https://codeberg.org/ravenjoad/guix
+    https://codeberg.org/PatrickNorton/guix
+    https://codeberg.org/foster-hangdaan/guix
+    https://codeberg.org/frankie/guix
+    https://codeberg.org/yqshao/guix
+    https://codeberg.org/retropikzel/guix
+    ))
