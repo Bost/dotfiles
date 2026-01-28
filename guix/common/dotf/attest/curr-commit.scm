28b59d3554174e655ad9d10b4c9eb9164550e34b
Rostislav Svoboda <Rostislav.Svoboda@posteo.net>
2026-01-28 12:07:16 +0100

    gx: On building trust: From Authentication to Attestation 12

--- guix/common/dotf/attest/curr-commit.scm
+++ guix/common/dotf/attest/curr-commit.scm
@@ -5,14 +5,9 @@
   #:use-module (ice-9 popen)
   #:use-module (ice-9 rdelim)
   #:use-module (srfi srfi-1)
-  #:use-module (srfi srfi-13)
   #:use-module (ice-9 pretty-print)
   #:export
   (
-   ;; "API" for the currently active (guix describe ...) channels.
-   current-channels-expr
-   guix-channel-commit
-   guix-channel-url
    curr-commit
    ))
 
@@ -21,7 +16,7 @@
     (let ((x (read port)))
       (if (eof-object? x) (reverse acc) (loop (cons x acc))))))
 
-(define (current-channels-expr)
+(define (current-channels)
   ;; returns whatever `(guix describe -f channels)` prints, as Scheme data
   (let* ((p (open-pipe* OPEN_READ "guix" "describe" "--format=channels"))
          (xs (read-all p)))
@@ -29,8 +24,14 @@
     ;; typically exactly one top-level expr, but keep it robust:
     (if (= (length xs) 1) (car xs) `(begin ,@xs))))
 
-(define* (guix-channel-commit #:optional (channels-expr (current-channels-expr)))
-  ;; channels-expr is usually:
+(define* (guix-channel-commit #:key
+                              (name 'guix)
+                              (channels (current-channels)))
+  "Return the commit of the (name NAME) channel from CHANNELS.
+Examples:
+(guix-channel-commit #:name 'guix #:channels (current-channels))
+"
+  ;; channels is usually:
   ;;   (list (channel (name 'guix) (url ...) (commit "...") ...) ...)
   ;; Do simple s-expression walk to find (name 'guix) and then (commit "...")
   ;; fields is ( (name ...) (url ...) (commit ...) ...)
@@ -45,31 +46,38 @@
 
   (let* ((channels
           (cond
-            ((and (pair? channels-expr) (eq? (car channels-expr) 'list))
-             (cdr channels-expr))
+            ((and (pair? channels) (eq? (car channels) 'list))
+             (cdr channels))
             (else
-             (error "Unexpected format" channels-expr))))
+             (error "Unexpected format" channels))))
          (guix-chan
           (find (lambda (ch)
 
                   (and (pair? ch) (eq? (car ch) 'channel)
                        (let* ((fs (channel-fields ch))
                               (nm (assoc-kw 'name fs)))
-                         (and nm (equal? (cadr (field-value nm)) 'guix)))))
+                         (and nm (equal? (cadr (field-value nm))
+                                         name)))))
                 channels)))
     (unless guix-chan
       (error
        ;; ~y pretty-print
-       (format #f "Could not find (name 'guix) channel among:\n~y"
-               channels-expr)))
+       (format #f "Could not find (name ~s) channel among:\n~y"
+               name
+               channels)))
     (let* ((fs (channel-fields guix-chan))
            (cm (assoc-kw 'commit fs)))
       (unless cm
         (error "Guix channel has no commit field?" guix-chan))
       (field-value cm))))
 
-(define* (guix-channel-url #:optional (channels-expr (current-channels-expr)))
-  "Return the URL of the (name 'guix) channel from CHANNELS-EXPR."
+(define* (guix-channel-url #:key
+                           (name 'guix)
+                           (channels (current-channels)))
+  "Return the URL of the (name NAME) channel from CHANNELS.
+Examples:
+(guix-channel-url #:name 'guix #:channels (current-channels))
+"
   (define (assoc-kw key fields)
     (find (lambda (f) (and (pair? f) (eq? (car f) key))) fields))
 
@@ -80,21 +88,23 @@
 
   (let* ((channels
           (cond
-            ((and (pair? channels-expr) (eq? (car channels-expr) 'list))
-             (cdr channels-expr))
+            ((and (pair? channels) (eq? (car channels) 'list))
+             (cdr channels))
             (else
-             (error "Unexpected format" channels-expr))))
+             (error "Unexpected format" channels))))
          (guix-chan
           (find (lambda (ch)
                   (and (pair? ch) (eq? (car ch) 'channel)
                        (let* ((fs (channel-fields ch))
                               (nm (assoc-kw 'name fs)))
-                         (and nm (equal? (cadr (field-value nm)) 'guix)))))
+                         (and nm (equal? (cadr (field-value nm))
+                                         name)))))
                 channels)))
     (unless guix-chan
       (error
-       (format #f "Could not find (name 'guix) channel among:\n~y"
-               channels-expr)))
+       (format #f "Could not find (name ~s) channel among:\n~y"
+               name
+               channels)))
     (let* ((fs (channel-fields guix-chan))
            (url (assoc-kw 'url fs)))
       (unless url
@@ -102,5 +112,8 @@
       (field-value url))))
 
 ;; Usage:
-(define (curr-commit) (guix-channel-commit (current-channels-expr)))
+(define (curr-commit) (guix-channel-commit
+                       #:name 'guix
+                       #:channels (current-channels)))
 ;; (curr-commit)  ;; => "abcd1234..."
+
