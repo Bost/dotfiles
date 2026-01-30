1efdcf753485ab5f85ec3e01023b2da4e41c302f
Rostislav Svoboda <Rostislav.Svoboda@posteo.net>
2026-01-30 01:19:31 +0100

    gxh: Update home-channels

--- guix/home/common/home-channels.scm
+++ guix/home/common/home-channels.scm
@@ -78,15 +78,25 @@
     #:nonguix-commit nonguix-commit)))
 
 (home-channels
- ;; 12 jan. 2026 12:30:03
+ ;; ;; 12 jan. 2026 12:30:03
+ ;; #:guix-android-commit "e5f52bd57275e404db74bf03b541bb62f7d73d58"
+ ;; ;; #:guixrus-commit from file:///home/bost/dev/guixrus
+ ;; #:guixrus-commit      "9f5a0a357ffafbe9b31fcdc3916dfee57baf5859"
+ ;; #:hask-clj-commit     "a8b30a606f91caabec3cc8dc4b1255a69836554e"
+ ;; #:games-commit        "acc252d2f7fed939ace5a5a98d7750197696dac3"
+ ;; #:bost-commit         "09700d780a7f706c910221d5d1de9378dc56ab12"
+ ;; #:guix-commit         "62c28bc6d8c54800b16341d1c93a1d7833fc3328"
+ ;; #:nonguix-commit      "3ceef556e1ce8e52180b959a942b16000ed4e61c"
+
+ ;; 30 jan. 2026 01:29:12
  #:guix-android-commit "e5f52bd57275e404db74bf03b541bb62f7d73d58"
  ;; #:guixrus-commit from file:///home/bost/dev/guixrus
  #:guixrus-commit      "9f5a0a357ffafbe9b31fcdc3916dfee57baf5859"
  #:hask-clj-commit     "a8b30a606f91caabec3cc8dc4b1255a69836554e"
  #:games-commit        "acc252d2f7fed939ace5a5a98d7750197696dac3"
- #:bost-commit         "09700d780a7f706c910221d5d1de9378dc56ab12"
- #:guix-commit         "62c28bc6d8c54800b16341d1c93a1d7833fc3328"
- #:nonguix-commit      "3ceef556e1ce8e52180b959a942b16000ed4e61c"
+ #:bost-commit         "0d2c6cf3cdcaecaf06c63c7d111ddf615ecc240d"
+ #:guix-commit         "f2a0cf7633b65f906a437180f54517e7d5ac23d1"
+ #:nonguix-commit      "216cf961b56a8b2679239ba102089ab94c259e89"
  )
 (testsymb 'home-channels)
 
