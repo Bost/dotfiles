4f3b1654c95636fe7c42af37b57dc7f1d682e721
Rostislav Svoboda <Rostislav.Svoboda@posteo.net>
2026-03-15 12:49:53 +0100

    gxh: Update home- and syst-channels

--- guix/systems/common/syst-channels.scm
+++ guix/systems/common/syst-channels.scm
@@ -31,67 +31,6 @@
 (module-evaluated)
 
 (syst-channels
- ;; 25 sept. 2025 19:54:02
- ;; #:guix-commit "ace78713a9ee7abfdd19e2f009c3fb606b797160"
- ;; #:nonguix-commit "25d7a8091c2c9678a8694f073d846a7001165169"
-
- ;; 26 sept. 2025 13:15:27
- ;; #:guix-commit "3c96d9fb1e1ee8fe4efd44ba6e8f3886b43668be"
- ;; #:nonguix-commit "25d7a8091c2c9678a8694f073d846a7001165169"
-
- ;; 07 oct. 2025 16:38:18
- ;; #:guix-commit "2793cb63025047fe2a9c7b3f05007475d4a1c6ab"
- ;; #:nonguix-commit "a5d216cd7c2d67eb95e58871bb805f22c160c57b"
-
- ;; 13 oct. 2025 16:29:31
- ;; #:guix-commit "c17d65c728c571668b84f82ad7cb8649d34019b5"
- ;; #:nonguix-commit "3f4a1907cae9b0def09d1549c491bb4e307b9097"
-
- ;; 22 oct. 2025 16:43:15
- ;; #:guix-commit "f01920e7f4962896452126928ea45a3b9e31832b"
- ;; #:nonguix-commit "a345ef84fbdf3b2491acb2c2b6665a4eb97bd4aa"
-
- ;; 03 nov. 2025 00:09:24
- ;; #:guix-commit "4fd32c00abb59b0f275a93c3fef35d5193adbe7a"
- ;; #:nonguix-commit "971bc275cf8a6554003ca924c65142ab7d430cad"
-
- ;; 14 nov. 2025 14:24:12
- ;; #:guix-commit "7e286874b81dc1d4f60153ae80348e493eda35c1"
- ;; #:nonguix-commit "0f68c1684169cbef8824fb246dfefa3e6832225b"
-
- ;; 19 nov. 2025 14:37:33
- ;; #:guix-commit "7e286874b81dc1d4f60153ae80348e493eda35c1"
- ;; #:nonguix-commit "0f68c1684169cbef8824fb246dfefa3e6832225b"
-
- ;; 02 déc. 2025 19:01:47
- ;; #:guix-commit "5f7cc5c2c6497ebaf7039cdb30ceba025602d698"
- ;; #:nonguix-commit "82be0b7adaaaa7a98d47382d7f72dd2e31d8e6d8"
-
- ;; 10 déc. 2025 22:25:49
- ;; #:guix-commit "ec959892550ecbfcd4be47e8464df953af6529b3"
- ;; #:nonguix-commit "cb35c71a028eb4a7c950c6a9637c0efad413ef35"
-
- ;; 29 déc. 2025 17:05:40
- ;; xfce is broken
- ;; #:guix-commit "d88ecb34b4ec6ce43e34def8e54b2f2522706721"
- ;; #:nonguix-commit "ba91bc437dba367f98608658bb4a19fb0880ad24"
-
- ;; 05 jan. 2026 12:29:08
- ;; #:guix-commit "b670a1ed28d8e9e31a8f698066eda39200ad5f5d"
- ;; #:nonguix-commit "cb2d0277635eaf4c573aeb9ca7563605fac7601c"
-
- ;; 12 jav. 2026 12:04:45
- ;; #:guix-commit "62c28bc6d8c54800b16341d1c93a1d7833fc3328"
- ;; #:nonguix-commit "3ceef556e1ce8e52180b959a942b16000ed4e61c"
-
- ;; 30 jan. 2026 02:11:55
- ;; #:guix-commit "930ec113149f343c160349eb78044dab57d6fea5"
- ;; #:nonguix-commit "216cf961b56a8b2679239ba102089ab94c259e89"
-
- ;; 30 jav. 2026 19:40:42
- ;; #:guix-commit "a6dddbb062ebc9ed20a51dbec0f1f2e9b6dba77c"
- ;; #:nonguix-commit "216cf961b56a8b2679239ba102089ab94c259e89"
-
  ;; 05 feb. 2026 14:35:03
  ;; #:nonguix-commit "6c0ea215e0bd089bf3b2097e5c59dd726fbbe304"
  ;; #:guix-commit "2c710762f5c80b1a151b57c6f369dfa1812d1f97"
@@ -113,6 +52,10 @@
  ;; #:guix-commit "c07690f0d673d8415066160617455313afa1d544"
 
  ;; 04 mar. 2026 20:53:35
- #:nonguix-commit "816b5c6f45ed56cd81e2a6338ed5b710e4e66e98"
+ ;; #:nonguix-commit "816b5c6f45ed56cd81e2a6338ed5b710e4e66e98"
+ ;; #:guix-commit "a375202ad3443e4c3fb492160e32aa6abcc965bd"
+
+ ;; 15 mar. 2026 12:05:35
+ #:nonguix-commit "7d14e819fc7b2681240023b9e4c001fc3f5cba04"
  #:guix-commit "a375202ad3443e4c3fb492160e32aa6abcc965bd"
  )
