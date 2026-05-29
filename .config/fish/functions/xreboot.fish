bfd9c11e246970c80888a2d665e16e4f0ccd5d31
Rostislav Svoboda <Rostislav.Svoboda@posteo.net>
2026-05-29 15:04:15 +0200

    gxs: gxh: Cleanup fish functions

--- .config/fish/functions/xreboot.fish
+++ .config/fish/functions/xreboot.fish
@@ -1,5 +1,7 @@
 function xreboot --description "xfce4-session-logout --reboot --fast"
-    set cmd xfce4-session-logout --reboot --fast
+    # -r, --reboot             Reboot without displaying the logout dialog
+    # -f, --fast               Log out quickly; don't save the session
+    set cmd xfce4-session-logout --reboot --fast $argv
     echo $cmd
     eval $cmd
 end
