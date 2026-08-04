81b66cb7dfdd912b6af92a28e8d3289380dc0e22
Rostislav Svoboda <Rostislav.Svoboda@posteo.net>
2026-08-04 18:42:03 +0200

    Unify utils 4

--- guix/home/common/home-channels.scm
+++ guix/home/common/home-channels.scm
@@ -191,10 +191,10 @@
  ;; #:bost-commit    "35282eedb5d4f59f09273d0c44e69c9918eb735a"
  ;; #:guix-commit    "dcddca760b8252474498e549b6e6ef34fc393f21"
 
- ;; 4 août 2026 16:20:22
+ ;; 4 août 2026 18:11:58
  #:nonguix-commit "7b7b2c47f9c205ad89ddf54293e7756e797f8980"
- #:bost-commit    "55dc35853c3a0429d37fd367a6c6e556b0ef3b5c"
- #:guix-commit    "86813d5779253bb50002d79ab791eeda5a8b4729"
+ #:bost-commit    "82b0707493422da2c9eab085e2df6bc1ce5baee9"
+ #:guix-commit    "d3acc7d021c51e292fe7572d6eef3d2d8d86b1c2"
 
  #:use-local-checkout #f)
 
